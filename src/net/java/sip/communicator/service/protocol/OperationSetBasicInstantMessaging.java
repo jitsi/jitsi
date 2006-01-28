be3ee2eaf76f6f8707b2e877ffe129bb8b372263
Emil Ivov <emcho@jitsi.org>
2006-01-28 14:31:12 +0000

    started support for the basic instant messaging

--- src/net/java/sip/communicator/service/protocol/OperationSetBasicInstantMessaging.java
+++ src/net/java/sip/communicator/service/protocol/OperationSetBasicInstantMessaging.java
@@ -6,17 +6,43 @@
  */
 package net.java.sip.communicator.service.protocol;
 
+import net.java.sip.communicator.service.protocol.event.MessageListener;
+
 /**
+ * Provides basic functionality for sending and receiving InstantMessages.
  *
  * @author Emil Ivov
  */
 public interface OperationSetBasicInstantMessaging
     extends OperationSet
 {
-    public void createMessageFactory();
+    /**
+     * Create a Message instance for sending arbitrary MIME-encoding content.
+     *
+     * @param content content value
+     * @param contentType MIME-type
+     * @param contentEncoding encoding used for the MIME-type
+     * @return the newly created message.
+     */
+    Message 	createMessage(byte[] content,
+                              String contentType,
+                              String contentEncoding);
+    /**
+     * Create a Message instance for sending a simple text messages with default
+     * (text/plain) content type and encoding.
+     *
+     * @param messageText the string content of the message.
+     * @return Message the newly created message
+     */
+    Message 	createMessage(String messageText);
 
-    public void sendInstantMessage();
+    public void sendInstantMessage(Contact to, Message message);
 
-    public void addMessageListener();
+    /**
+     * Registeres a MessageListener with this operation set.
+     *
+     * @param listener the message listener to register.
+     */
+    public void addMessageListener(MessageListener listener);
 
 }
