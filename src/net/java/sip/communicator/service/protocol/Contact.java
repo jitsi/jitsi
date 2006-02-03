07283274269606426faae97565dacfc285a23d60
Emil Ivov <emcho@jitsi.org>
2006-02-03 16:28:33 +0000

    Added the possibility of retrieving a reference to the parent protocol provider service

--- src/net/java/sip/communicator/service/protocol/Contact.java
+++ src/net/java/sip/communicator/service/protocol/Contact.java
@@ -12,23 +12,47 @@ import java.util.*;
  * This class represents the notion of a Contact or Buddy, that is widely used
  * in instant messaging today. From a protocol point of view, a contact is
  * generally considered to be another user of the service that proposes the
- * protocol
+ * protocol. Instances of Contact could be used for delivery of presence
+ * notifications or when addressing instant messages.
  *
  *
  * @author Emil Ivov
  */
 public interface Contact
 {
-    //address
+    /**
+     * Returns a String that can be used for identifying the contact. The
+     * exact contents of the string depends on the protocol. In the case of
+     * SIP, for example, that would be the SIP uri (e.g. sip:alice@biloxi.com)
+     * in the case of icq - a UIN (12345653) and for AIM a screenname (mysname).
+     * Jabber (and hence Google) would be having e-mail like addresses.
+     * @return a String id representing and uniquely identifying the contact.
+     */
     public String getAddress();
 
-    //image
+    /**
+     * Returns a String that could be used by any user interacting modules for
+     * referring to this contact. An alias is not necessarily unique but is
+     * often more human readable than an address (or id).
+     * @return a String that can be used for referring to this contact when
+     * interacting with the user.
+     */
+    public String getAlias();
+
+    /**
+     * Returns a byte array containing an image (most often a photo or an avatar)
+     * that the contact uses as a representation.
+     * @return byte[] an image representing the contact.
+     */
     public byte[] getImage();
 
-    //islocal
+    /**
+     * Determines whether or not this contact represents our own identity.
+     * @return true in case this is a contact that represents ourselves and
+     * false otherwise.
+     */
     public boolean isLocal();
 
-    //getLastReceivedPresenceStatus
     /**
      * Returns the status of the contact as per the last status update we've
      * received for it. Note that this method is not to perform any network
@@ -40,4 +64,11 @@ public interface Contact
      * pertaining to this contact.
      */
     public PresenceStatus getPresenceStatus();
+
+    /**
+     * Returns a reference to the protocol provider that created the contact.
+     * @return a refererence to an instance of the ProtocolProviderService
+     */
+    public ProtocolProviderService getProtocolProvider();
+
 }
