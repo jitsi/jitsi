f7a63ae30280f45c1061fd3a24a7fa094d0f240b
Yana Stamcheva <yana@jitsi.org>
2006-02-05 19:34:41 +0000

    message window menus

--- src/net/java/sip/communicator/impl/gui/main/message/menu/EditMenu.java
+++ src/net/java/sip/communicator/impl/gui/main/message/menu/EditMenu.java
@@ -1,3 +1,10 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+
 package net.java.sip.communicator.impl.gui.main.message.menu;
 
 import java.awt.Graphics;
@@ -7,6 +14,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
 import net.java.sip.communicator.impl.gui.main.utils.Constants;
+import net.java.sip.communicator.impl.gui.main.utils.ImageLoader;
 import net.java.sip.communicator.impl.gui.main.customcontrols.AntialiasedMenuItem;
 import net.java.sip.communicator.impl.gui.main.i18n.Messages;
 import net.java.sip.communicator.impl.gui.main.utils.AntialiasingManager;
@@ -15,15 +23,15 @@ public class EditMenu extends JMenu {
 	
 	private AntialiasedMenuItem cutMenuItem 
 							= new AntialiasedMenuItem(Messages.getString("cut")
-									, new ImageIcon(Constants.CUT_ICON));
+									, new ImageIcon(ImageLoader.getImage(ImageLoader.CUT_ICON)));
 
 	private AntialiasedMenuItem copyMenuItem 
 							= new AntialiasedMenuItem(Messages.getString("copy")
-									, new ImageIcon(Constants.COPY_ICON));
+									, new ImageIcon(ImageLoader.getImage(ImageLoader.COPY_ICON)));
 	
 	private AntialiasedMenuItem pasteMenuItem 
 							= new AntialiasedMenuItem(Messages.getString("paste")
-									, new ImageIcon(Constants.PASTE_ICON));
+									, new ImageIcon(ImageLoader.getImage(ImageLoader.PASTE_ICON)));
 
 	public EditMenu(){
 		
--- src/net/java/sip/communicator/impl/gui/main/message/menu/FileMenu.java
+++ src/net/java/sip/communicator/impl/gui/main/message/menu/FileMenu.java
@@ -1,3 +1,10 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+
 package net.java.sip.communicator.impl.gui.main.message.menu;
 
 import java.awt.Graphics;
@@ -10,6 +17,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
 import net.java.sip.communicator.impl.gui.main.utils.Constants;
+import net.java.sip.communicator.impl.gui.main.utils.ImageLoader;
 import net.java.sip.communicator.impl.gui.main.customcontrols.AntialiasedMenuItem;
 import net.java.sip.communicator.impl.gui.main.i18n.Messages;
 import net.java.sip.communicator.impl.gui.main.message.MessageWindow;
@@ -20,19 +28,19 @@ public class FileMenu extends JMenu
 
 	private AntialiasedMenuItem saveMenuItem 
 						= new AntialiasedMenuItem(Messages.getString("save")
-									, new ImageIcon(Constants.SAVE_ICON));
+									, new ImageIcon(ImageLoader.getImage(ImageLoader.SAVE_ICON)));
 
 	private AntialiasedMenuItem printMenuItem 
 						= new AntialiasedMenuItem(Messages.getString("print")
-									, new ImageIcon(Constants.PRINT_ICON));
+									, new ImageIcon(ImageLoader.getImage(ImageLoader.PRINT_ICON)));
 	
 	private AntialiasedMenuItem closeMenuItem 
 						= new AntialiasedMenuItem(Messages.getString("close")
-									, new ImageIcon(Constants.CLOSE_ICON));
+									, new ImageIcon(ImageLoader.getImage(ImageLoader.CLOSE_ICON)));
 	
 	private AntialiasedMenuItem quitMenuItem 
 						= new AntialiasedMenuItem(Messages.getString("quit")
-									, new ImageIcon(Constants.QUIT_ICON));
+									, new ImageIcon(ImageLoader.getImage(ImageLoader.QUIT_ICON)));
 	
 	private MessageWindow parentWindow;
 	
