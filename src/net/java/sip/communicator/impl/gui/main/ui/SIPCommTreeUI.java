3711e42871c463e6fe99b21dad27a5a78b0123d2
Yana Stamcheva <yana@jitsi.org>
2006-02-05 19:36:24 +0000

    extended tree UI and toolbar separator UI

---
+++ src/net/java/sip/communicator/impl/gui/main/ui/SIPCommTreeUI.java
@@ -0,0 +1,36 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+
+package net.java.sip.communicator.impl.gui.main.ui;
+
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.Rectangle;
+
+import javax.swing.JComponent;
+import javax.swing.plaf.basic.BasicTreeUI;
+import javax.swing.tree.TreePath;
+
+import net.java.sip.communicator.impl.gui.main.utils.Constants;
+
+public class SIPCommTreeUI extends BasicTreeUI {
+
+	public SIPCommTreeUI() {
+		
+	}
+
+	protected void paintHorizontalLine(Graphics g, JComponent c, int y,
+			int left, int right) {
+
+	}
+
+	protected void paintVerticalLine(Graphics g, JComponent c, int x, int top,
+			int bottom) {
+
+	}
+
+}
