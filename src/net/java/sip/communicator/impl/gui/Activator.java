c09d37d179e395284988763fb41052baf2419178
Yana Stamcheva <yana@jitsi.org>
2006-02-07 10:00:22 +0000

    Activator and manifest

--- src/net/java/sip/communicator/impl/gui/Activator.java
+++ src/net/java/sip/communicator/impl/gui/Activator.java
@@ -3,12 +3,7 @@ package net.java.sip.communicator.impl.gui;
 import java.util.Hashtable;
 
 import net.java.sip.communicator.impl.gui.main.CommunicatorMain;
-import net.java.sip.communicator.service.configuration.ConfigurationService;
-import net.java.sip.communicator.service.contactlist.MetaContact;
-import net.java.sip.communicator.service.contactlist.MetaContactGroup;
-import net.java.sip.communicator.service.contactlist.MetaContactListException;
-import net.java.sip.communicator.service.contactlist.MetaContactListService;
-import net.java.sip.communicator.service.contactlist.event.MetaContactListListener;
+import net.java.sip.communicator.impl.gui.main.login.LoginWindow;
 import net.java.sip.communicator.service.gui.UIService;
 import net.java.sip.communicator.service.protocol.AccountID;
 import net.java.sip.communicator.service.protocol.AccountManager;
@@ -16,12 +11,10 @@ import net.java.sip.communicator.service.protocol.AccountProperties;
 import net.java.sip.communicator.service.protocol.Contact;
 import net.java.sip.communicator.service.protocol.ProtocolNames;
 import net.java.sip.communicator.service.protocol.ProtocolProviderService;
-import net.java.sip.communicator.slick.protocol.icq.IcqSlickFixture;
 import net.java.sip.communicator.util.Logger;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 
 
@@ -35,6 +28,8 @@ public class Activator implements BundleActivator
 	
 	private CommunicatorMain communicatorMain = new CommunicatorMain();
 	
+	private LoginWindow	loginWindow = new LoginWindow();
+	
 	private AccountID icqAccountID;
 	
 	public void start(BundleContext bundleContext) throws Exception 
@@ -47,13 +42,11 @@ public class Activator implements BundleActivator
             this.uiService =
                 new UIServiceImpl();
 
-            ServiceReference clistReference
-            	= bundleContext.getServiceReference(MetaContactListService.class.getName());
+            //ServiceReference clistReference
+            	//= bundleContext.getServiceReference(MetaContactListService.class.getName());
            
             //MetaContactListService contactListService
             	//= (MetaContactListService)bundleContext.getService(clistReference);
-            
-            MetaContactListService contactListService = new MetaContactListServiceImpl(); 
             
             ServiceReference[] serRefs = null;
             String osgiFilter = "(" + AccountManager.PROTOCOL_PROPERTY_NAME
@@ -61,7 +54,7 @@ public class Activator implements BundleActivator
             
             serRefs = bundleContext.getServiceReferences(
                     AccountManager.class.getName(), osgiFilter);
-            
+                        
             icqAccountManager = (AccountManager)bundleContext.getService(serRefs[0]);
             
             Hashtable icqAccountProperties = new Hashtable();
@@ -79,9 +72,9 @@ public class Activator implements BundleActivator
                     ProtocolProviderService.class.getName(),
                     osgiFilter);
             
-            Object icqProtocolProvider
-            			= bundleContext.getService(serRefs[0]);
-            
+            ProtocolProviderService icqProtocolProvider
+            			= (ProtocolProviderService)bundleContext.getService(serRefs[0]);
+                   
             logger.info("UI Service...[  STARTED ]");
 
             bundleContext.registerService(
@@ -89,9 +82,11 @@ public class Activator implements BundleActivator
                         
             logger.info("UI Service ...[REGISTERED]");
             
-            communicatorMain.setContactList(contactListService);
+            //communicatorMain.setContactList(contactListService);
             
             communicatorMain.show();
+            
+            loginWindow.show();
         }
         finally
         {
@@ -104,74 +99,4 @@ public class Activator implements BundleActivator
 		logger.info("UI Service ...[STOPED]");		
 	}
 	
-	private class MetaContactListServiceImpl 
-		implements MetaContactListService {
-
-		public MetaContactGroup getRoot() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public MetaContact findMetaContactByContact(Contact contact) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public MetaContact findMetaContactByID(String metaContactID) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public void addContactListListener(MetaContactListListener l) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void removeContactListListener(MetaContactListListener l) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void moveContact(Contact contact, MetaContact newParent) throws MetaContactListException {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void removeContact(Contact contact) throws MetaContactListException {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void addNewContactToMetaContact(ProtocolProviderService provider, MetaContact metaContact, String contactID) throws MetaContactListException {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void createMetaContact(ProtocolProviderService provider, MetaContactGroup contactGroup, String contactID) throws MetaContactListException {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void moveMetaContact(MetaContact metaContact, MetaContactGroup newGroup) throws MetaContactListException {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void removeMetaContact(MetaContact metaContact) throws MetaContactListException {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void createMetaContactGroup(String groupName) throws MetaContactListException {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void removeMetaContactGroup(MetaContactGroup groupToRemove) throws MetaContactListException {
-			// TODO Auto-generated method stub
-			
-		}
-		
-	}
-
 }
