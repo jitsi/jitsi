18f0a563ce0b3864931d6e17ea2458d53cc8ac09
Emil Ivov <emcho@jitsi.org>
2006-02-08 05:05:26 +0000

    class.getResource() does not seem to work inside oscar. need to access rsources through the classloader

--- src/net/java/sip/communicator/impl/gui/main/utils/ImageLoader.java
+++ src/net/java/sip/communicator/impl/gui/main/utils/ImageLoader.java
@@ -16,101 +16,101 @@ import javax.imageio.ImageIO;
 import net.java.sip.communicator.util.Logger;
 
 public class ImageLoader {
-	
+
 	private static Logger log = Logger.getLogger(ImageLoader.class);
-	
-	public static final ImageID EMPTY_16x16_ICON 
+
+	public static final ImageID EMPTY_16x16_ICON
 											= new ImageID("EMPTY_16x16_ICON");
 
-	public static final ImageID QUICK_MENU_ADD_ICON 
+	public static final ImageID QUICK_MENU_ADD_ICON
 											= new ImageID("QUICK_MENU_ADD_ICON");
 
-	public static final ImageID QUICK_MENU_CONFIGURE_ICON 
+	public static final ImageID QUICK_MENU_CONFIGURE_ICON
 											= new ImageID("QUICK_MENU_CONFIGURE_ICON");
 
-	public static final ImageID QUICK_MENU_SEARCH_ICON 
+	public static final ImageID QUICK_MENU_SEARCH_ICON
 											= new ImageID("QUICK_MENU_SEARCH_ICON");
 
-	public static final ImageID QUICK_MENU_INFO_ICON 
+	public static final ImageID QUICK_MENU_INFO_ICON
 											= new ImageID("QUICK_MENU_INFO_ICON");
 
-	public static final ImageID QUICK_MENU_BUTTON_BG 
+	public static final ImageID QUICK_MENU_BUTTON_BG
 											= new ImageID("QUICK_MENU_BUTTON_BG");
 
-	public static final ImageID QUICK_MENU_BUTTON_ROLLOVER_BG 
+	public static final ImageID QUICK_MENU_BUTTON_ROLLOVER_BG
 											= new ImageID("QUICK_MENU_BUTTON_ROLLOVER_BG");
 
-	public static final ImageID CALL_BUTTON_BG 
+	public static final ImageID CALL_BUTTON_BG
 											= new ImageID("CALL_BUTTON_BG");
 
-	public static final ImageID HANGUP_BUTTON_BG 
+	public static final ImageID HANGUP_BUTTON_BG
 											= new ImageID("HANGUP_BUTTON_BG");
 
-	public static final ImageID CALL_ROLLOVER_BUTTON_BG 
+	public static final ImageID CALL_ROLLOVER_BUTTON_BG
 											= new ImageID("CALL_ROLLOVER_BUTTON_BG");
 
-	public static final ImageID CALL_PRESSED_BUTTON_BG 
+	public static final ImageID CALL_PRESSED_BUTTON_BG
 											= new ImageID("CALL_PRESSED_BUTTON_BG");
 
-	public static final ImageID HANGUP_ROLLOVER_BUTTON_BG 
+	public static final ImageID HANGUP_ROLLOVER_BUTTON_BG
 											= new ImageID("HANGUP_ROLLOVER_BUTTON_BG");
 
-	public static final ImageID HANGUP_PRESSED_BUTTON_BG 
+	public static final ImageID HANGUP_PRESSED_BUTTON_BG
 											= new ImageID("HANGUP_PRESSED_BUTTON_BG");
 
-	public static final ImageID STATUS_SELECTOR_BOX 
+	public static final ImageID STATUS_SELECTOR_BOX
 											= new ImageID("STATUS_SELECTOR_BOX");
 
 	public static final ImageID BUTTON_BG 	= new ImageID("BUTTON_BG");
 
-	public static final ImageID BUTTON_ROLLOVER_BG 
+	public static final ImageID BUTTON_ROLLOVER_BG
 											= new ImageID("BUTTON_ROLLOVER_BG");
 
-	public static final ImageID ONE_DIAL_BUTTON 
+	public static final ImageID ONE_DIAL_BUTTON
 											= new ImageID("ONE_DIAL_BUTTON");
 
-	public static final ImageID TWO_DIAL_BUTTON 
+	public static final ImageID TWO_DIAL_BUTTON
 											= new ImageID("TWO_DIAL_BUTTON");
 
-	public static final ImageID THREE_DIAL_BUTTON 
+	public static final ImageID THREE_DIAL_BUTTON
 											= new ImageID("THREE_DIAL_BUTTON");
 
-	public static final ImageID FOUR_DIAL_BUTTON 
+	public static final ImageID FOUR_DIAL_BUTTON
 											= new ImageID("FOUR_DIAL_BUTTON");
 
-	public static final ImageID FIVE_DIAL_BUTTON 
+	public static final ImageID FIVE_DIAL_BUTTON
 											= new ImageID("FIVE_DIAL_BUTTON");
 
-	public static final ImageID SIX_DIAL_BUTTON 
+	public static final ImageID SIX_DIAL_BUTTON
 											= new ImageID("SIX_DIAL_BUTTON");
 
-	public static final ImageID SEVEN_DIAL_BUTTON 
+	public static final ImageID SEVEN_DIAL_BUTTON
 											= new ImageID("SEVEN_DIAL_BUTTON");
 
-	public static final ImageID EIGHT_DIAL_BUTTON 
+	public static final ImageID EIGHT_DIAL_BUTTON
 											= new ImageID("EIGHT_DIAL_BUTTON");
 
-	public static final ImageID NINE_DIAL_BUTTON 
+	public static final ImageID NINE_DIAL_BUTTON
 											= new ImageID("NINE_DIAL_BUTTON");
 
-	public static final ImageID STAR_DIAL_BUTTON 
+	public static final ImageID STAR_DIAL_BUTTON
 											= new ImageID("STAR_DIAL_BUTTON");
 
-	public static final ImageID ZERO_DIAL_BUTTON 
+	public static final ImageID ZERO_DIAL_BUTTON
 											= new ImageID("ZERO_DIAL_BUTTON");
 
-	public static final ImageID DIEZ_DIAL_BUTTON 
+	public static final ImageID DIEZ_DIAL_BUTTON
 											= new ImageID("DIEZ_DIAL_BUTTON");
 
 	public static final ImageID DEFAULT_USER_PHOTO
 											= new ImageID("DEFAULT_USER_PHOTO");
-	
+
 	public static final ImageID DEFAULT_CHAT_USER_PHOTO
 											= new ImageID("DEFAULT_CHAT_USER_PHOTO");
 
 	public static final ImageID CALL_PANEL_MINIMIZE_BUTTON
 											= new ImageID("CALL_PANEL_MINIMIZE_BUTTON");
-	
+
 	public static final ImageID CALL_PANEL_RESTORE_BUTTON
 											= new ImageID("CALL_PANEL_RESTORE_BUTTON");
 
@@ -121,74 +121,74 @@ public class ImageLoader {
 											= new ImageID("CALL_PANEL_RESTORE_ROLLOVER_BUTTON");
 
 
-	public static final ImageID ADD_TO_CHAT_BUTTON 
+	public static final ImageID ADD_TO_CHAT_BUTTON
 											= new ImageID("ADD_TO_CHAT_BUTTON");
 
-	public static final ImageID ADD_TO_CHAT_ROLLOVER_BUTTON 
+	public static final ImageID ADD_TO_CHAT_ROLLOVER_BUTTON
 											= new ImageID("ADD_TO_CHAT_ROLLOVER_BUTTON");
 
-	public static final ImageID ADD_TO_CHAT_ICON 
+	public static final ImageID ADD_TO_CHAT_ICON
 											= new ImageID("ADD_TO_CHAT_ICON");
 
-	public static final ImageID TOOLBAR_DIVIDER 
+	public static final ImageID TOOLBAR_DIVIDER
 											= new ImageID("TOOLBAR_DIVIDER");
 
-	public static final ImageID RIGHT_ARROW_ICON 
+	public static final ImageID RIGHT_ARROW_ICON
 											= new ImageID("RIGHT_ARROW_ICON");
 
-	public static final ImageID RIGHT_ARROW_ROLLOVER_ICON 
+	public static final ImageID RIGHT_ARROW_ROLLOVER_ICON
 											= new ImageID("RIGHT_ARROW_ROLLOVER_ICON");
 
-	public static final ImageID BOTTOM_ARROW_ICON 
+	public static final ImageID BOTTOM_ARROW_ICON
 											= new ImageID("BOTTOM_ARROW_ICON");
 
-	public static final ImageID BOTTOM_ARROW_ROLLOVER_ICON 
+	public static final ImageID BOTTOM_ARROW_ROLLOVER_ICON
 											= new ImageID("BOTTOM_ARROW_ROLLOVER_ICON");
 
 	/////////////////////// Edit Text Toolbar icons //////////////////////////
-	
-	public static final ImageID ALIGN_LEFT_BUTTON 
+
+	public static final ImageID ALIGN_LEFT_BUTTON
 											= new ImageID("ALIGN_LEFT_BUTTON");
 
-	public static final ImageID ALIGN_RIGHT_BUTTON  
+	public static final ImageID ALIGN_RIGHT_BUTTON
 											= new ImageID("ALIGN_RIGHT_BUTTON");
 
-	public static final ImageID ALIGN_CENTER_BUTTON  
+	public static final ImageID ALIGN_CENTER_BUTTON
 											= new ImageID("ALIGN_RIGHT_BUTTON");
 
-	public static final ImageID ALIGN_LEFT_ROLLOVER_BUTTON   
+	public static final ImageID ALIGN_LEFT_ROLLOVER_BUTTON
 											= new ImageID("ALIGN_LEFT_ROLLOVER_BUTTON");
 
-	public static final ImageID ALIGN_RIGHT_ROLLOVER_BUTTON   
+	public static final ImageID ALIGN_RIGHT_ROLLOVER_BUTTON
 											= new ImageID("ALIGN_RIGHT_ROLLOVER_BUTTON");
 
-	public static final ImageID ALIGN_CENTER_ROLLOVER_BUTTON   
+	public static final ImageID ALIGN_CENTER_ROLLOVER_BUTTON
 											= new ImageID("ALIGN_CENTER_ROLLOVER_BUTTON");
 
-	public static final ImageID TEXT_BOLD_BUTTON   
+	public static final ImageID TEXT_BOLD_BUTTON
 											= new ImageID("TEXT_BOLD_BUTTON");
 
-	public static final ImageID TEXT_ITALIC_BUTTON    
+	public static final ImageID TEXT_ITALIC_BUTTON
 											= new ImageID("TEXT_ITALIC_BUTTON");
 
-	public static final ImageID TEXT_UNDERLINED_BUTTON   
+	public static final ImageID TEXT_UNDERLINED_BUTTON
 											= new ImageID("TEXT_ITALIC_BUTTON");
 
-	public static final ImageID TEXT_BOLD_ROLLOVER_BUTTON   
+	public static final ImageID TEXT_BOLD_ROLLOVER_BUTTON
 											= new ImageID("TEXT_BOLD_ROLLOVER_BUTTON");
 
-	public static final ImageID TEXT_ITALIC_ROLLOVER_BUTTON   
+	public static final ImageID TEXT_ITALIC_ROLLOVER_BUTTON
 											= new ImageID("TEXT_ITALIC_ROLLOVER_BUTTON");
 
-	public static final ImageID TEXT_UNDERLINED_ROLLOVER_BUTTON   
+	public static final ImageID TEXT_UNDERLINED_ROLLOVER_BUTTON
 											= new ImageID("TEXT_UNDERLINED_ROLLOVER_BUTTON");
 
 	// ///////////////////////// Main Toolbar icons ////////////////////////////
 
-	public static final ImageID MSG_TOOLBAR_BUTTON_BG   
+	public static final ImageID MSG_TOOLBAR_BUTTON_BG
 											= new ImageID("MSG_TOOLBAR_BUTTON_BG");
 
-	public static final ImageID MSG_TOOLBAR_ROLLOVER_BUTTON_BG   
+	public static final ImageID MSG_TOOLBAR_ROLLOVER_BUTTON_BG
 											= new ImageID("MSG_TOOLBAR_ROLLOVER_BUTTON_BG");
 
 	public static final ImageID COPY_ICON 	= new ImageID("COPY_ICON");
@@ -196,8 +196,8 @@ public class ImageLoader {
 	public static final ImageID CUT_ICON 	= new ImageID("CUT_ICON");
 
 	public static final ImageID PASTE_ICON 	= new ImageID("PASTE_ICON");
-	
-	public static final ImageID SMILIES_ICON 
+
+	public static final ImageID SMILIES_ICON
 											= new ImageID("SMILIES_ICON");
 
 	public static final ImageID SAVE_ICON 	= new ImageID("SAVE_ICON");
@@ -208,12 +208,12 @@ public class ImageLoader {
 
 	public static final ImageID QUIT_ICON 	= new ImageID("QUIT_ICON");
 
-	public static final ImageID PREVIOUS_ICON 
+	public static final ImageID PREVIOUS_ICON
 											= new ImageID("PREVIOUS_ICON");
 
 	public static final ImageID NEXT_ICON 	= new ImageID("NEXT_ICON");
 
-	public static final ImageID HISTORY_ICON 
+	public static final ImageID HISTORY_ICON
 											= new ImageID("HISTORY_ICON");
 
 	public static final ImageID SEND_FILE_ICON
@@ -224,27 +224,27 @@ public class ImageLoader {
 
 	// ///////////////////// Chat contact icons ////////////////////////////////
 
-	public static final ImageID CHAT_CONTACT_INFO_BUTTON 
+	public static final ImageID CHAT_CONTACT_INFO_BUTTON
 											= new ImageID("CHAT_CONTACT_INFO_BUTTON");
 
-	public static final ImageID CHAT_CONTACT_INFO_ROLLOVER_BUTTON 
+	public static final ImageID CHAT_CONTACT_INFO_ROLLOVER_BUTTON
 											= new ImageID("CHAT_CONTACT_INFO_ROLLOVER_BUTTON");
-	
-	public static final ImageID CHAT_CONTACT_CALL_BUTTON 
+
+	public static final ImageID CHAT_CONTACT_CALL_BUTTON
 											= new ImageID("CHAT_CONTACT_CALL_BUTTON");
 
-	public static final ImageID CHAT_CONTACT_CALL_ROLLOVER_BUTTON 
+	public static final ImageID CHAT_CONTACT_CALL_ROLLOVER_BUTTON
 											= new ImageID("CHAT_CONTACT_CALL_ROLLOVER_BUTTON");
 
-	public static final ImageID CHAT_CONTACT_SEND_FILE_BUTTON 
+	public static final ImageID CHAT_CONTACT_SEND_FILE_BUTTON
 											= new ImageID("CHAT_CONTACT_SEND_FILE_BUTTON");
 
-	public static final ImageID CHAT_SEND_FILE_ROLLOVER_BUTTON 
+	public static final ImageID CHAT_SEND_FILE_ROLLOVER_BUTTON
 											= new ImageID("CHAT_SEND_FILE_ROLLOVER_BUTTON");
 
 	// ///////////////////// Optionpane icons /////////////////////////////
 
-	public static final ImageID WARNING_ICON 
+	public static final ImageID WARNING_ICON
 											= new ImageID("WARNING_ICON");
 
 	// //////////////////// RightButton menu icons ////////////////////////
@@ -258,13 +258,13 @@ public class ImageLoader {
 	public static final ImageID HISTORY_16x16_ICON
 											= new ImageID("HISTORY_16x16_ICON");
 
-	public static final ImageID SEND_FILE_16x16_ICON 
+	public static final ImageID SEND_FILE_16x16_ICON
 											= new ImageID("SEND_FILE_16x16_ICON");
 
-	public static final ImageID GROUPS_16x16_ICON  
+	public static final ImageID GROUPS_16x16_ICON
 											= new ImageID("GROUPS_16x16_ICON");
 
-	public static final ImageID INFO_16x16_ICON 
+	public static final ImageID INFO_16x16_ICON
 											= new ImageID("INFO_16x16_ICON");
 
 	public static final ImageID ADD_CONTACT_16x16_ICON
@@ -272,30 +272,30 @@ public class ImageLoader {
 
 	public static final ImageID RENAME_16x16_ICON
 											= new ImageID("RENAME_16x16_ICON");
-	
-	public static final ImageID MORE_INFO_ICON 
+
+	public static final ImageID MORE_INFO_ICON
 											= new ImageID("MORE_INFO_ICON");
 
-	public static final ImageID TOOLBAR_DRAG_ICON 
+	public static final ImageID TOOLBAR_DRAG_ICON
 											= new ImageID("TOOLBAR_DRAG_ICON");
-	
+
 	/*
 	 * =========================================================================
 	 * --------------------- PROTOCOLS STATUS ICONS ---------------------------
 	 * ========================================================================
 	 */
 
-	public static final ImageID ICQ_LOGO 	= new ImageID("ICQ_LOGO");	
+	public static final ImageID ICQ_LOGO 	= new ImageID("ICQ_LOGO");
 
 	public static final ImageID ICQ_FF_CHAT_ICON
 											= new ImageID("ICQ_FF_CHAT_ICON");
 
-	public static final ImageID ICQ_AWAY_ICON		
+	public static final ImageID ICQ_AWAY_ICON
 											= new ImageID("ICQ_AWAY_ICON");
 
 	public static final ImageID ICQ_NA_ICON = new ImageID("ICQ_NA_ICON");
 
-	public static final ImageID ICQ_DND_ICON 
+	public static final ImageID ICQ_DND_ICON
 											= new ImageID("ICQ_DND_ICON");
 
 	public static final ImageID ICQ_OCCUPIED_ICON
@@ -321,27 +321,27 @@ public class ImageLoader {
 	public static final ImageID SIP_LOGO 	= new ImageID("SIP_LOGO");
 
 
-	public static final ImageID SIP_ONLINE_ICON	
+	public static final ImageID SIP_ONLINE_ICON
 											= new ImageID("SIP_ONLINE_ICON");
 
-	public static final ImageID SIP_OFFLINE_ICON 
+	public static final ImageID SIP_OFFLINE_ICON
 											= new ImageID("SIP_OFFLINE_ICON");
 
-	public static final ImageID SIP_INVISIBLE_ICON 
+	public static final ImageID SIP_INVISIBLE_ICON
 											= new ImageID("SIP_INVISIBLE_ICON");
 
-	public static final ImageID SIP_AWAY_ICON 
+	public static final ImageID SIP_AWAY_ICON
 											= new ImageID("SIP_AWAY_ICON");
 
 	public static final ImageID SIP_NA_ICON	= new ImageID("SIP_NA_ICON");
 
-	public static final ImageID SIP_DND_ICON 
+	public static final ImageID SIP_DND_ICON
 											= new ImageID("SIP_DND_ICON");
 
-	public static final ImageID SIP_OCCUPIED_ICON 
+	public static final ImageID SIP_OCCUPIED_ICON
 											= new ImageID("SIP_OCCUPIED_ICON");
 
-	public static final ImageID SIP_CHAT_ICON 
+	public static final ImageID SIP_CHAT_ICON
 											= new ImageID("SIP_CHAT_ICON");
 
 	/*
@@ -350,7 +350,7 @@ public class ImageLoader {
 	 * ========================================================================
 	 */
 
-	public static final ImageID USER_ONLINE_ICON  
+	public static final ImageID USER_ONLINE_ICON
 											= new ImageID("USER_ONLINE_ICON");
 
 	/*
@@ -385,53 +385,53 @@ public class ImageLoader {
 
 	/**
 	 * Load default smilies pack
-	 * 
+	 *
 	 * @return the ArrayList of all smilies.
-	 * 
+	 *
 	 */
 	public static ArrayList getDefaultSmiliesPack() {
 
 		ArrayList defaultPackList = new ArrayList();
 
-		defaultPackList.add(new Smily(ImageLoader.SMILY1, 
+		defaultPackList.add(new Smily(ImageLoader.SMILY1,
 							new String[] { "$-)", "$)" }));
-		
-		defaultPackList.add(new Smily(ImageLoader.SMILY2, 
+
+		defaultPackList.add(new Smily(ImageLoader.SMILY2,
 							new String[] { "8-)", "8)" }));
-		
-		defaultPackList.add(new Smily(ImageLoader.SMILY3, 
+
+		defaultPackList.add(new Smily(ImageLoader.SMILY3,
 							new String[] { ":-*", ":*" }));
-		
-		defaultPackList.add(new Smily(ImageLoader.SMILY4, 
+
+		defaultPackList.add(new Smily(ImageLoader.SMILY4,
 							new String[] { ":-0", ":0" }));
-		
-		defaultPackList.add(new Smily(ImageLoader.SMILY5, 
+
+		defaultPackList.add(new Smily(ImageLoader.SMILY5,
 							new String[] { ":-((", ":((" }));
-		
-		defaultPackList.add(new Smily(ImageLoader.SMILY6, 
+
+		defaultPackList.add(new Smily(ImageLoader.SMILY6,
 							new String[] { ":-~", ":~" }));
-		
-		defaultPackList.add(new Smily(ImageLoader.SMILY7, 
+
+		defaultPackList.add(new Smily(ImageLoader.SMILY7,
 							new String[] { ":-|", ":|" }));
-		
-		defaultPackList.add(new Smily(ImageLoader.SMILY8, 
+
+		defaultPackList.add(new Smily(ImageLoader.SMILY8,
 							new String[] { ":-P", ":P", ":-p", ":p" }));
-				
-		defaultPackList.add(new Smily(ImageLoader.SMILY9, 
+
+		defaultPackList.add(new Smily(ImageLoader.SMILY9,
 							new String[] { ":-))", ":))" }));
-		
-		defaultPackList.add(new Smily(ImageLoader.SMILY10, 
+
+		defaultPackList.add(new Smily(ImageLoader.SMILY10,
 							new String[] { ":-(", ":(" }));
-		
-		defaultPackList.add(new Smily(ImageLoader.SMILY11, 
+
+		defaultPackList.add(new Smily(ImageLoader.SMILY11,
 							new String[] { ":-)", ":)" }));
-		
-		defaultPackList.add(new Smily(ImageLoader.SMILY12, 
+
+		defaultPackList.add(new Smily(ImageLoader.SMILY12,
 							new String[] { ";-)", ";)" }));
 
 		return defaultPackList;
 	}
-	
+
 	/**
 	 * Loads an image from a given image identifier.
 	 */
@@ -439,7 +439,7 @@ public class ImageLoader {
 	public static Image getImage(ImageID imageID) {
 
 		String path = Images.getString(imageID.getId());
-		
+
 		Image image = null;
 
 		try {
@@ -449,7 +449,7 @@ public class ImageLoader {
 				log.trace("Loading image : " + path + "...");
 			}
 
-			image = ImageIO.read(ImageLoader.class.getResource(path));
+			image = ImageIO.read(ImageLoader.class.getClassLoader().getResource(path));
 
 			if (log.isTraceEnabled()) {
 				log.trace("Loading image : " + path + "... [ DONE ]");
@@ -463,16 +463,16 @@ public class ImageLoader {
 
 		return image;
 	}
-		
+
 	/**
 	 *  Represents the Image Identifier.
 	 */
 	public static class ImageID {
-		
+
 		private String id;
-		
+
 		private ImageID(String id){
-		
+
 			this.id = id;
 		}
 
