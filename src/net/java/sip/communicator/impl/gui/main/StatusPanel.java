c631eeb1f1c07d3e15cd1f9c60520603c0057795
Yana Stamcheva <yana@jitsi.org>
2006-02-17 14:15:13 +0000

    presence management added

--- src/net/java/sip/communicator/impl/gui/main/StatusPanel.java
+++ src/net/java/sip/communicator/impl/gui/main/StatusPanel.java
@@ -8,19 +8,27 @@
 package net.java.sip.communicator.impl.gui.main;
 
 import java.awt.FlowLayout;
-import java.util.ArrayList;
+import java.awt.Image;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Map;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
-import net.java.sip.communicator.impl.gui.main.customcontrols.SelectorBox;
 import net.java.sip.communicator.impl.gui.main.utils.Constants;
-import net.java.sip.communicator.impl.gui.main.utils.SelectorBoxItem;
+import net.java.sip.communicator.service.protocol.OperationFailedException;
+import net.java.sip.communicator.service.protocol.OperationSetPresence;
+import net.java.sip.communicator.service.protocol.PresenceStatus;
 
 public class StatusPanel extends JPanel {
 
-	private String[] userProtocols;
-
+	private String[] userProtocols; 
+    
+	private Hashtable protocolStatusCombos = new Hashtable();
+    
+    private OperationSetPresence presence;
+    
 	public StatusPanel(String[] userProtocols) {
 
 		this.setLayout(new FlowLayout(FlowLayout.RIGHT, 0, 0));
@@ -36,16 +44,58 @@ public class StatusPanel extends JPanel {
 	private void init() {
 
 		for (int i = 0; i < userProtocols.length; i++) {
+            
+            Map protocolStatusMap = Constants
+                .getProtocolStatusIcons(userProtocols[i]);
 
-			ArrayList protocolStatusList = Constants
-					.getProtocolStatusIcons(userProtocols[i]);
+			StatusSelectorBox protocolStatusCombo 
+                = new StatusSelectorBox(
+					protocolStatusMap, 
+                    (Image)protocolStatusMap.get(Constants.OFFLINE_STATUS));
 
-			//TODO:to change this line!!!!!!!!!!!!
-			SelectorBox protocolStatusCombo = new SelectorBox(
-					protocolStatusList.toArray(), (SelectorBoxItem) protocolStatusList
-							.get(protocolStatusList.size() - 2));
-
+            this.protocolStatusCombos.put(  userProtocols[i], 
+                                            protocolStatusCombo);
+            
 			this.add(protocolStatusCombo);
 		}
 	}
+    
+    public void setSelectedStatus(String protocol, Object status){
+        
+        Map protocolStatusMap = Constants
+            .getProtocolStatusIcons(protocol);
+        
+        StatusSelectorBox selectorBox
+            = (StatusSelectorBox)protocolStatusCombos.get(protocol);
+        
+        selectorBox.setIconImage((Image)protocolStatusMap.get(status));
+        
+        selectorBox.repaint();
+    }
+
+    public Hashtable getProtocolStatusCombos() {
+        return protocolStatusCombos;
+    }
+
+    public void setProtocolStatusCombos(
+            Hashtable protocolStatusCombos) {
+        this.protocolStatusCombos = protocolStatusCombos;
+    }
+
+    public OperationSetPresence getPresence() {
+        return presence;
+    }
+
+    public void setPresence(OperationSetPresence presence) {
+        this.presence = presence;
+        
+        Enumeration statusCombos 
+            = this.getProtocolStatusCombos().elements();
+        
+        while(statusCombos.hasMoreElements()){
+            
+            ((StatusSelectorBox)statusCombos.nextElement())
+                .setPresence(presence);
+        }
+    }
 }
---
+++ src/net/java/sip/communicator/impl/gui/main/StatusSelectorBox.java
@@ -0,0 +1,172 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+
+package net.java.sip.communicator.impl.gui.main;
+
+import java.awt.Component;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JMenuItem;
+
+import net.java.sip.communicator.impl.gui.main.customcontrols.AntialiasedPopupMenu;
+import net.java.sip.communicator.impl.gui.main.customcontrols.SIPCommButton;
+import net.java.sip.communicator.impl.gui.main.utils.ImageLoader;
+import net.java.sip.communicator.service.protocol.OperationFailedException;
+import net.java.sip.communicator.service.protocol.OperationSetPresence;
+import net.java.sip.communicator.service.protocol.PresenceStatus;
+import net.java.sip.communicator.service.protocol.icqconstants.IcqStatusEnum;
+
+public class StatusSelectorBox extends SIPCommButton
+    implements ActionListener{
+
+    private AntialiasedPopupMenu popup;
+
+    private Map itemsMap;
+    
+    private OperationSetPresence presence;
+    
+    public StatusSelectorBox() {
+        
+        super(  ImageLoader.getImage(ImageLoader.STATUS_SELECTOR_BOX),
+                ImageLoader.getImage(ImageLoader.STATUS_SELECTOR_BOX),
+                null);
+        
+        this.popup = new AntialiasedPopupMenu();
+        
+        this.popup.setInvoker(this);
+        
+        this.addActionListener(this);
+    }
+    
+    public StatusSelectorBox(Map itemsMap, Image selectedItem) {
+        
+        super(  ImageLoader.getImage(ImageLoader.STATUS_SELECTOR_BOX),
+                ImageLoader.getImage(ImageLoader.STATUS_SELECTOR_BOX),
+                selectedItem);
+        
+        this.itemsMap = itemsMap;
+        
+        this.popup = new AntialiasedPopupMenu();
+        
+        this.popup.setInvoker(this);
+        
+        this.addActionListener(this);
+        
+        this.init();
+    }
+
+    public void init() {
+
+        Iterator iter = itemsMap.entrySet().iterator();
+        
+        while (iter.hasNext()) {
+
+            Map.Entry entry = (Map.Entry)iter.next();
+        
+            JMenuItem item 
+                = new JMenuItem
+                    (((IcqStatusEnum)entry.getKey()).getStatusName(), 
+                     new ImageIcon((Image)entry.getValue()));
+            
+            item.addActionListener(this);
+            
+            this.popup.add(item);
+        
+        }       
+    }
+
+    public void addItem(String text, Icon icon){
+        
+        JMenuItem item = new JMenuItem( text, icon);
+
+        item.addActionListener(this);
+
+        this.popup.add(item);
+    }
+    
+    public void actionPerformed (ActionEvent e) {
+        
+        if (e.getSource() instanceof SIPCommButton){
+    
+            if (!this.popup.isVisible()) {
+                this.popup.setLocation(this.calculatePopupLocation());
+                this.popup.setVisible(true);            
+            }       
+        }
+        else if (e.getSource() instanceof JMenuItem){
+            
+            JMenuItem menuItem = (JMenuItem) e.getSource();            
+                   
+            Iterator statusSet = this.presence.getSupportedStatusSet();
+            
+            while (statusSet.hasNext()){
+                
+                PresenceStatus status 
+                    = ((PresenceStatus)statusSet.next());
+                
+                if(status.getStatusName().equals(menuItem.getText())){
+                    
+                    try {
+                        this.presence.publishPresenceStatus(status, "");
+                    } catch (IllegalArgumentException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    } catch (IllegalStateException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    } catch (OperationFailedException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+                    
+                    break;
+                }
+                
+            }
+            
+            this.setIconImage(((ImageIcon)menuItem.getIcon()).getImage());
+        }
+    }
+    
+    public Point calculatePopupLocation(){
+        
+        Component component = this;
+        Point point = new Point();
+        int x = this.getX();
+        int y = this.getY();
+        
+        while(component.getParent() != null){
+            
+            component = component.getParent();
+            
+            x += component.getX();
+            y += component.getY();
+        }
+        
+        point.x = x;
+        point.y = y + this.getHeight();
+        
+        return point;
+    }
+
+    public OperationSetPresence getPresence() {
+        return presence;
+    }
+
+    public void setPresence(OperationSetPresence presence) {
+        this.presence = presence;
+    }
+    
+
+}
