2c3c5c0314a98105453b812fdb86b78a8b875532
Yana Stamcheva <yana@jitsi.org>
2006-02-23 15:36:01 +0000

    Custom text field and password field component

---
+++ src/net/java/sip/communicator/impl/gui/main/customcontrols/SIPCommPasswordField.java
@@ -0,0 +1,48 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+
+package net.java.sip.communicator.impl.gui.main.customcontrols;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.RoundRectangle2D;
+
+import javax.swing.JPasswordField;
+
+import net.java.sip.communicator.impl.gui.main.utils.AntialiasingManager;
+import net.java.sip.communicator.impl.gui.main.utils.Constants;
+
+public class SIPCommPasswordField extends JPasswordField {
+
+    public SIPCommPasswordField(){
+        super();   
+    }
+    
+    public SIPCommPasswordField(int columns){
+        super(columns);   
+    }
+    
+    protected void paintBorder(Graphics g){
+        
+        Graphics2D g2 = (Graphics2D)g;
+        
+        g2.setColor(Constants.TOOLBAR_SEPARATOR_COLOR);
+        
+        g2.setStroke(new BasicStroke(3.0f));
+        
+        g2.drawRoundRect(0, 0, this.getWidth() - 1, this.getHeight() - 1, 7, 7);        
+    }
+    
+    protected void paintComponent(Graphics g){
+        
+        AntialiasingManager.activateAntialiasing(g);
+        
+        super.paintComponent(g);
+    }
+}
