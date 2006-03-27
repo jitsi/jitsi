7723edcf5e49e57c623c02a82d80907a7805789a
Yana Stamcheva <yana@jitsi.org>
2006-03-27 16:51:18 +0000

    comments added

--- src/net/java/sip/communicator/service/gui/ContainerID.java
+++ src/net/java/sip/communicator/service/gui/ContainerID.java
@@ -1,5 +1,17 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
 package net.java.sip.communicator.service.gui;
 
+/**
+ * The <tt>ContainerID</tt> wraps a string which is meant to point 
+ * to a container which could contain plugin components.
+ * 
+ * @author Yana Stamcheva
+ */
 public class ContainerID{
 
     private String containerName;
