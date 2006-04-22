0714b1658724366bc3436679b426ae9a4e2d9d0d
Emil Ivov <emcho@jitsi.org>
2006-04-22 07:44:09 +0000

    Created stub

---
+++ src/net/java/sip/communicator/service/protocol/AuthorizationRequest.java
@@ -0,0 +1,20 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.service.protocol;
+
+/**
+ * @todo desribe
+ *
+ * @author Emil Ivov
+ */
+public class AuthorizationRequest
+{
+    public AuthorizationRequest()
+    {
+        super();
+    }
+}
---
+++ src/net/java/sip/communicator/service/protocol/AuthorizationResponse.java
@@ -0,0 +1,20 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.service.protocol;
+
+/**
+ * @todo describe
+ *
+ * @author Emil Ivov
+ */
+public class AuthorizationResponse
+{
+    public AuthorizationResponse()
+    {
+        super();
+    }
+}
