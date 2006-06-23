018cfdb8f0379adb93a17afc1421f50f8f90e347
Yana Stamcheva <yana@jitsi.org>
2006-06-23 14:34:33 +0000

    add contact dialog

---
+++ src/net/java/sip/communicator/impl/gui/main/contactlist/addcontact/AddContactDialog.java
@@ -0,0 +1,111 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.impl.gui.main.contactlist.addcontact;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+import net.java.sip.communicator.impl.gui.main.i18n.Messages;
+import net.java.sip.communicator.service.contactlist.MetaContact;
+import net.java.sip.communicator.service.contactlist.MetaContactGroup;
+import net.java.sip.communicator.service.contactlist.MetaContactListService;
+import net.java.sip.communicator.service.protocol.ProtocolProviderService;
+
+public class AddContactDialog extends JDialog
+    implements ActionListener {
+
+    private AddContactPanel addContactPanel = new AddContactPanel();
+    
+    private JButton addButton = new JButton(Messages.getString("add"));
+    
+    private JButton cancelButton = new JButton(Messages.getString("cancel"));
+    
+    private JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    
+    private JPanel mainPanel = new JPanel(new BorderLayout());
+    
+    private MetaContactListService clist;
+    
+    private MetaContact metaContact;
+    
+    private MetaContactGroup group;
+    
+    private ProtocolProviderService pps;
+    
+    public AddContactDialog(MetaContactListService clist,
+            MetaContact metaContact,
+            ProtocolProviderService pps) {
+        
+        this.clist = clist;
+        this.metaContact = metaContact;
+        this.pps = pps;
+        
+        this.init();
+    }
+    
+    public AddContactDialog(MetaContactListService clist,
+            MetaContactGroup group,
+            ProtocolProviderService pps) {
+        
+        this.clist = clist;
+        this.group = group;
+        this.pps = pps;
+        
+        this.init();
+    }
+
+    private void init() {
+        this.setTitle(Messages.getString("addContact"));
+        
+        this.setSize(520, 250);
+        
+        this.setModal(true);
+        
+        this.addButton.setName("add");
+        this.cancelButton.setName("cancel");
+        
+        this.addButton.addActionListener(this);
+        this.cancelButton.addActionListener(this);
+        
+        this.buttonsPanel.add(addButton);
+        this.buttonsPanel.add(cancelButton);
+        
+        this.mainPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 0, 10));
+        
+        this.mainPanel.add(addContactPanel, BorderLayout.CENTER);
+        this.mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
+        
+        this.getContentPane().add(mainPanel);
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        JButton button = (JButton)e.getSource();
+        String name = button.getName();
+        
+        if (name.equals("add")) {
+            if (metaContact != null) {
+                this.clist.addNewContactToMetaContact(pps, metaContact,
+                    addContactPanel.getUIN());
+            }
+            else if (group != null) {
+                this.clist.createMetaContact(
+                        pps, group, addContactPanel.getUIN());
+            }
+            this.dispose();
+        }
+        else {
+            this.dispose();
+        }
+    }
+}
