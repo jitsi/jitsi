71020bd233cb2e98ff88c3bfde714d30c10a2f41
Emil Ivov <emcho@jitsi.org>
2006-06-23 16:09:02 +0000

    Make sure we don't override existing display name's with those of the first contact added to a meta contact

--- src/net/java/sip/communicator/impl/contactlist/MetaContactImpl.java
+++ src/net/java/sip/communicator/impl/contactlist/MetaContactImpl.java
@@ -287,8 +287,11 @@ public class MetaContactImpl
 
             this.protoContacts.add(contact);
 
-            //if this is our firt contact - set the display name too.
-            if(this.protoContacts.size() == 1){
+            //if this is our firt contact and we don't already have a display
+            //name, use theirs.
+            if(this.protoContacts.size() == 1
+                &&( this.displayName == null
+                    || this.displayName.trim().length() == 0)){
                 //be careful not to use setDisplayName() here cause this will
                 //bring us into a deadlock.
                 this.displayName = new String(contact.getDisplayName().getBytes());
