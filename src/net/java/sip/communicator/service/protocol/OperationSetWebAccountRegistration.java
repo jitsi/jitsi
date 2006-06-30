adc023e48f1ede68abde66410fd4d7ae5ce54bc2
Emil Ivov <emcho@jitsi.org>
2006-06-30 17:14:33 +0000

    An operation set that allows protocol implementors to provide for a quick way of creating new accounts - by simply opening a web page on the right place.

---
+++ src/net/java/sip/communicator/service/protocol/OperationSetWebAccountRegistration.java
@@ -0,0 +1,31 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.service.protocol;
+
+import java.net.*;
+
+/**
+ * The operation set is there to propose an extremely simple and light way of
+ * registering new accounts for a given protocol. The
+ * getAccountRegistrationURL() returns a URL that when opened in a web browser
+ * allows you to register your own account with the corresponding protocol.
+ * <p>
+ *
+ * @author Emil Ivov
+ */
+public interface OperationSetWebAccountRegistration
+    extends OperationSet
+{
+    /**
+     * Returns a URL that points to a page which allows for on-line registration
+     * of accounts belonging to the service supported by this protocol provider.
+     * <p>
+     * @return a URL pointing to a web page where one could register their
+     * account for the current protocol.
+     */
+    public URL getAccountRegistrationURL();
+}
