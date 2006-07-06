6a4b944b9409114adb99a502c1bbb158eb98d20e
Damian Minkov <damencho@jitsi.org>
2006-07-06 11:45:36 +0000

    Added UserinInfo Implementation
    Moved common operations int InfoRetreiver

--- src/net/java/sip/communicator/impl/protocol/icq/OperationSetServerStoredContactInfoIcqImpl.java
+++ src/net/java/sip/communicator/impl/protocol/icq/OperationSetServerStoredContactInfoIcqImpl.java
@@ -20,125 +20,26 @@ import net.kano.joscar.snac.*;
 public class OperationSetServerStoredContactInfoIcqImpl
     implements OperationSetServerStoredContactInfo
 {
-    private Hashtable retreivedDetails = new Hashtable();
-    private static int requestID = 0;
-
-    /**
-     * A callback to the ICQ provider that created us.
-     */
-    private ProtocolProviderServiceIcqImpl icqProvider = null;
+    private InfoRetreiver infoRetreiver;
 
     protected OperationSetServerStoredContactInfoIcqImpl
-        (ProtocolProviderServiceIcqImpl icqProvider)
+        (InfoRetreiver infoRetreiver)
     {
-        this.icqProvider = icqProvider;
+        this.infoRetreiver = infoRetreiver;
     }
 
     public Iterator getDetailsAndDescendants(Contact contat, Class detailClass)
     {
-        Vector details = getContactDetails(contat);
-        Vector result = new Vector();
-
-        Iterator iter = details.iterator();
-        while (iter.hasNext())
-        {
-            Object item = (Object) iter.next();
-            if(detailClass.isInstance(item))
-                result.add(item);
-        }
-
-        return result.iterator();
+        return infoRetreiver.getDetailsAndDescendants(contat.getAddress(), detailClass);
     }
 
     public Iterator getDetails(Contact contat, Class detailClass)
     {
-        Vector details = getContactDetails(contat);
-        Vector result = new Vector();
-
-        Iterator iter = details.iterator();
-        while (iter.hasNext())
-        {
-            Object item = (Object) iter.next();
-            if(detailClass.equals(item.getClass()))
-                result.add(item);
-        }
-
-        return result.iterator();
+        return infoRetreiver.getDetails(contat.getAddress(), detailClass);
     }
 
     public Iterator getAllDetailsForContact(Contact contact)
     {
-        return getContactDetails(contact).iterator();
-    }
-
-    private Vector getContactDetails(Contact contact)
-    {
-        Vector result = (Vector)retreivedDetails.get(contact);
-
-        if(result == null)
-        {
-            //retreive the details
-            long toICQUin = Long.parseLong(contact.getAddress());
-            FullInfoRequest infoRequest = new FullInfoRequest(toICQUin);
-
-            int reqID = requestID++;
-
-            UserInfoResponseRetriever responseRetriever =
-                new UserInfoResponseRetriever(reqID);
-
-            SnacCommand cmd = new ToIcqCmd(
-                toICQUin,
-                infoRequest.getType(),
-                reqID,
-                infoRequest);
-
-            icqProvider.getAimConnection().getInfoService()
-                .sendSnacRequest(cmd, responseRetriever);
-
-            synchronized(responseRetriever)
-            {
-                try{
-                    responseRetriever.wait(10000);
-                }
-                catch (InterruptedException ex)
-                {
-                    //we don't care
-                }
-            }
-
-            result = responseRetriever.result;
-            retreivedDetails.put(contact, result);
-        }
-
-        return result;
-    }
-
-    private class UserInfoResponseRetriever extends SnacRequestAdapter
-    {
-        int requestID;
-        Vector result = null;
-
-        UserInfoResponseRetriever(int requestID)
-        {
-            this.requestID = requestID;
-        }
-
-        public void handleResponse(SnacResponseEvent e)
-        {
-            SnacCommand snac = e.getSnacCommand();
-
-            if (snac instanceof FullInfoCmd)
-            {
-                FullInfoCmd infoSnac = (FullInfoCmd)snac;
-
-                if(infoSnac.isLastOfSequences() &&
-                   infoSnac.getRequestID() == requestID)
-                {
-                    //get the result
-                    result = infoSnac.getInfo();
-                    synchronized(this){this.notifyAll();}
-                }
-            }
-        }
+        return infoRetreiver.getContactDetails(contact.getAddress()).iterator();
     }
 }
