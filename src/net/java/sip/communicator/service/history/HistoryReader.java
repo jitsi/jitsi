df0b834097c973dc3e14a9e5e54b5be22632a08a
Damian Minkov <damencho@jitsi.org>
2006-07-26 15:46:11 +0000

    MessageHistory Tests
    Fixed som javadocs. Added filed for the searching with keywords. The field specifies where to look for the keyword.
    The field is the name of the field in HistoryRecord structure.

--- src/net/java/sip/communicator/service/history/HistoryReader.java
+++ src/net/java/sip/communicator/service/history/HistoryReader.java
@@ -17,6 +17,8 @@ public interface HistoryReader {
      * Searches the history for all records with timestamp after
      * <tt>startDate</tt>.
      *
+     * @param startDate the date after all records will be returned
+     * @return the found records
      * @throws RuntimeException
      *             Thrown if an exception occurs during the execution of the
      *             query, such as internal IO error.
@@ -27,6 +29,8 @@ public interface HistoryReader {
      * Searches the history for all records with timestamp before
      * <tt>endDate</tt>.
      *
+     * @param endDate the date before which all records will be returned
+     * @return the found records
      * @throws RuntimeException
      *             Thrown if an exception occurs during the execution of the
      *             query, such as internal IO error.
@@ -37,6 +41,9 @@ public interface HistoryReader {
      * Searches the history for all records with timestamp between
      * <tt>startDate</tt> and <tt>endDate</tt>.
      *
+     * @param startDate start of the interval in which we search
+     * @param endDate end of the interval in which we search
+     * @return the found records
      * @throws RuntimeException
      *             Thrown if an exception occurs during the execution of the
      *             query, such as internal IO error.
@@ -47,30 +54,42 @@ public interface HistoryReader {
     /**
      * Searches the history for all records containing the <tt>keyword</tt>.
      *
+     * @param keyword the keyword to search for
+     * @param field the field where to look for the keyword
+     * @return the found records
      * @throws RuntimeException
      *             Thrown if an exception occurs during the execution of the
      *             query, such as internal IO error.
      */
-    QueryResultSet findByKeyword(String keyword) throws RuntimeException;
+    QueryResultSet findByKeyword(String keyword, String field) throws RuntimeException;
 
     /**
      * Searches the history for all records containing all <tt>keywords</tt>.
      *
+     * @param keywords array of keywords we search for
+     * @param field the field where to look for the keyword
+     * @return the found records
      * @throws RuntimeException
      *             Thrown if an exception occurs during the execution of the
      *             query, such as internal IO error.
      */
-    QueryResultSet findByKeywords(String[] keywords) throws RuntimeException;
+    QueryResultSet findByKeywords(String[] keywords, String field) throws RuntimeException;
 
     /**
      * Searches for all history records containing all <tt>keywords</tt>,
      * with timestamp between <tt>startDate</tt> and <tt>endDate</tt>.
      *
-     * @throws RuntimeException
+     * @param startDate start of the interval in which we search
+     * @param endDate end of the interval in which we search
+     * @param keywords array of keywords we search for
+     * @param field the field where to look for the keyword
+     * @return the found records
+     * @throws UnsupportedOperationException
      *             Thrown if an exception occurs during the execution of the
      *             query, such as internal IO error.
      */
-    QueryResultSet findByPeriod(Date startDate, Date endDate, String[] keywords)
+    QueryResultSet findByPeriod(Date startDate, Date endDate,
+                                String[] keywords, String field)
             throws UnsupportedOperationException;
 
 }
