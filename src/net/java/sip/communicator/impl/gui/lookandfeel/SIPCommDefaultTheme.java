5a2a87b7ba8182950f9df1656bd3bba10f02e927
Yana Stamcheva <yana@jitsi.org>
2006-07-28 11:12:04 +0000

    new table background color added

--- src/net/java/sip/communicator/impl/gui/lookandfeel/SIPCommDefaultTheme.java
+++ src/net/java/sip/communicator/impl/gui/lookandfeel/SIPCommDefaultTheme.java
@@ -127,6 +127,7 @@ public class SIPCommDefaultTheme
             "FormattedTextField.border", textFieldBorder,
 
             "Table.gridColor", LIGHT_BLUE_GRAY,
+            "Table.background", getDesktopColor(),
 
             "ToggleButton.gradient", buttonGradient,
 
