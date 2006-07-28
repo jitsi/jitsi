1fa9e9357ce10977a6b4e3a909334e1fb1959af4
Emil Ivov <emcho@jitsi.org>
2006-07-28 13:49:13 +0000

    Renamed getAccountUserID to getUserID

--- src/net/java/sip/communicator/impl/gui/customcontrols/LabelTableCellRenderer.java
+++ src/net/java/sip/communicator/impl/gui/customcontrols/LabelTableCellRenderer.java
@@ -29,33 +29,33 @@ import net.java.sip.communicator.service.protocol.ProtocolProviderService;
  * Custom <tt>TableCellRenderer</tt> that renders
  * <tt>ProtocolProviderService</tt> objects, <tt>MetaContactGroup</tt>
  * objects and JLabels.
- * 
+ *
  * @author Yana Stamcheva
  */
 public class LabelTableCellRenderer extends JPanel
     implements TableCellRenderer {
-    
+
     private JLabel label = new JLabel();
-    
+
     public LabelTableCellRenderer(){
         label.setHorizontalAlignment(JLabel.CENTER);
         this.setOpaque(true);
-                
+
         this.add(label, BorderLayout.CENTER);
     }
-    
+
     public Component getTableCellRendererComponent(JTable table, Object value,
             boolean isSelected, boolean hasFocus, int row, int column) {
-        
+
         if(value instanceof JLabel) {
             JLabel labelValue = (JLabel)value;
-            
+
             label.setText(labelValue.getText());
             label.setIcon(labelValue.getIcon());
         }
         else if (value instanceof ProtocolProviderService) {
             ProtocolProviderService pps = (ProtocolProviderService)value;
-            label.setText(pps.getAccountID().getAccountUserID());
+            label.setText(pps.getAccountID().getUserID());
         }
         else if (value instanceof MetaContactGroup) {
             MetaContactGroup group = (MetaContactGroup) value;
@@ -64,12 +64,12 @@ public class LabelTableCellRenderer extends JPanel
         else {
             label.setText(value.toString());
         }
-        
+
         if(isSelected)
             this.setBackground(Constants.SELECTED_END_COLOR);
         else
             this.setBackground(UIManager.getColor("Table.background"));
-        
+
         return this;
     }
 }
