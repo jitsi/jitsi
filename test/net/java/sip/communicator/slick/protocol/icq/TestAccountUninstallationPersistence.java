9bae89fed8c349a31eb52f502774177c5495d274
Emil Ivov <emcho@jitsi.org>
2006-07-28 13:48:38 +0000

    Added tests that verify that we are properly and persistently storing and restoring protocol accounts

---
+++ test/net/java/sip/communicator/slick/protocol/icq/TestAccountUninstallationPersistence.java
@@ -0,0 +1,102 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.slick.protocol.icq;
+
+import junit.framework.*;
+import org.osgi.framework.*;
+import net.java.sip.communicator.service.protocol.*;
+import net.java.sip.communicator.service.configuration.*;
+
+/**
+ * Contains tests verifying persistence of account uninstallation. In other
+ * words we try to make sure that once uninstalled an account remains
+ * uninstalled.
+ *
+ * @author Emil Ivov
+ */
+public class TestAccountUninstallationPersistence
+        extends TestCase
+{
+    /**
+     * Creates a new test instance wrapper around the test with the specified
+     * name.
+     * @param testName the name of the test that we will be executing.
+     */
+    public TestAccountUninstallationPersistence(String testName)
+    {
+        super(testName);
+    }
+
+    /**
+     * Retrieves a reference to the icq bundle, stops it and uninstalls it and
+     * then reinstalls it in order to make sure that accounts are not reloaded
+     * once removed.
+     *
+     * @throws java.lang.Exception if something goes wrong while manipulating
+     * the bundles.
+     */
+    public void testAccountUninstallationPersistency()
+        throws Exception
+    {
+        Bundle providerBundle = IcqSlickFixture.providerBundle;
+
+        providerBundle.stop();
+
+        assertTrue("Couldn't stop the protocol provider bundle. State was "
+                   + providerBundle.getState()
+                   ,    Bundle.ACTIVE   != providerBundle.getState()
+                     && Bundle.STOPPING != providerBundle.getState());
+
+        providerBundle.uninstall();
+
+        assertEquals("Couldn't stop the protocol provider bundle."
+                     , Bundle.UNINSTALLED, providerBundle.getState());
+
+        //Now reinstall the bundle and restart the provider
+        providerBundle
+            = IcqSlickFixture.bc.installBundle(providerBundle.getLocation());
+
+        assertEquals("Couldn't re-install protocol provider bundle."
+                     , Bundle.INSTALLED, providerBundle.getState());
+
+        providerBundle.start();
+        assertEquals("Couldn't re-start protocol provider bundle."
+                     , Bundle.ACTIVE, providerBundle.getState());
+
+
+        //verify that the provider is not reinstalled
+        ServiceReference[] icqProviderRefs = null;
+        try
+        {
+            icqProviderRefs = IcqSlickFixture.bc.getServiceReferences(
+                ProtocolProviderService.class.getName(),
+                "(" + ProtocolProviderFactory.PROTOCOL
+                    + "=" +ProtocolNames.ICQ + ")");
+        }
+        catch (InvalidSyntaxException ex)
+        {
+            fail("We apparently got our filter wrong");
+        }
+
+        //make sure we didn't retrieve a service
+        assertTrue("A Protocol Provider Service was still regged as an osgi service "
+                      +"for ICQ UIN:" + IcqSlickFixture.icqAccountID
+                      + "After it was explicitly uninstalled"
+                      ,icqProviderRefs == null || icqProviderRefs.length == 0);
+
+        //and a nasty hack at the end - delete the configuration file so that
+        //we get a fresh start on next run.
+        ServiceReference confReference
+            = IcqSlickFixture.bc.getServiceReference(
+                ConfigurationService.class.getName());
+        ConfigurationService configurationService
+            = (ConfigurationService) IcqSlickFixture.bc.getService(confReference);
+
+        configurationService.purgeStoredConfiguration();
+
+    }
+}
