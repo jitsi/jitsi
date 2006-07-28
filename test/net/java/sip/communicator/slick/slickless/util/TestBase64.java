3f6e547b5194780a120bd63081d6814fcda0f8d0
Emil Ivov <emcho@jitsi.org>
2006-07-28 13:48:44 +0000

    Added tests over the Base64 encoding algorithms

---
+++ test/net/java/sip/communicator/slick/slickless/util/TestBase64.java
@@ -0,0 +1,90 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.slick.slickless.util;
+
+import junit.framework.*;
+import net.java.sip.communicator.util.*;
+import net.java.sip.communicator.slick.protocol.icq.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Tests the methods of the Base64 class.
+ * @author Emil Ivov
+ */
+public class TestBase64 extends TestCase
+{
+    private Base64 base64 = null;
+
+    /**
+     * Create a TestBase64 wrapper over the test with the specified name.
+     * @param name the name of the test to run
+     */
+    public TestBase64(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * Initializes the fixture.
+     * @throws Exception if anything goes wrong.
+     */
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        base64 = new Base64();
+
+    }
+
+    /**
+     * Finalization
+     * @throws Exception if anything goes wrong.
+     */
+    protected void tearDown() throws Exception
+    {
+        base64 = null;
+
+        super.tearDown();
+    }
+
+    /**
+     * Encodes a sample string, decodes it and makes sure that the decoded
+     * string has the same value as the original
+     */
+    public void testEncodeDecode()
+    {
+        String data = "string to encode";
+        byte[] expectedReturn = data.getBytes();
+
+        byte[] encodedData = base64.encode(data.getBytes());
+        byte[] actualReturn = base64.decode(encodedData);
+        assertTrue("encode decode failed.", Arrays.equals(
+            expectedReturn, actualReturn));
+    }
+
+    /**
+     * Encodes a sample string, decodes it and makes sure that the decoded
+     * string has the same value as the original
+     */
+    public void testEncodeDecode1()
+    {
+        String data = "string to encode";
+        byte[] expectedReturn = data.getBytes();
+
+        byte[] encodedData = base64.encode(data.getBytes());
+
+        String encodedString = new String(encodedData);
+
+        byte[] actualReturn = base64.decode(encodedString);
+        assertTrue("encode decode failed.", Arrays.equals(
+            expectedReturn, actualReturn));
+
+        assertEquals("Original and destination string do not match"
+                     , data, new String(actualReturn));
+    }
+
+}
