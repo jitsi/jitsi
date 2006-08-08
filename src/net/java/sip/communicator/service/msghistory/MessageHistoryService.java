a9ab92ccd2bd2d5a578d0b1a8b1ee35b76c48855
Damian Minkov <damencho@jitsi.org>
2006-08-08 07:44:13 +0000

    Added javadocs

--- src/net/java/sip/communicator/service/msghistory/MessageHistoryService.java
+++ src/net/java/sip/communicator/service/msghistory/MessageHistoryService.java
@@ -9,26 +9,100 @@ package net.java.sip.communicator.service.msghistory;
 import java.util.*;
 
 import net.java.sip.communicator.service.contactlist.*;
-import net.java.sip.communicator.service.history.*;
 
 /**
+ * The Message History Service stores messages exchanged through the various protocols
+ *
  * @author Alexander Pelov
  * @author Damian Minkov
  */
 public interface MessageHistoryService
 {
+    /**
+     * Returns all the messages exchanged by all the contacts
+     * in the supplied metacontact after the given date
+     *
+     * @param contact MetaContact
+     * @param startDate Date the start date of the conversations
+     * @return Collection of MessageReceivedEvents or MessageDeliveredEvents
+     * @throws RuntimeException
+     */
     Collection findByStartDate(MetaContact contact, Date startDate)
         throws RuntimeException;
+
+    /**
+     * Returns all the messages exchanged by all the contacts
+     * in the supplied metacontact before the given date
+     *
+     * @param contact MetaContact
+     * @param endDate Date the end date of the conversations
+     * @return Collection of MessageReceivedEvents or MessageDeliveredEvents
+     * @throws RuntimeException
+     */
     Collection findByEndDate(MetaContact contact, Date endDate)
         throws RuntimeException;
+
+    /**
+     * Returns all the messages exchanged by all the contacts
+     * in the supplied metacontact between the given dates
+     *
+     * @param contact MetaContact
+     * @param startDate Date the start date of the conversations
+     * @param endDate Date the end date of the conversations
+     * @return Collection of MessageReceivedEvents or MessageDeliveredEvents
+     * @throws RuntimeException
+     */
     Collection findByPeriod(MetaContact contact, Date startDate, Date endDate)
         throws RuntimeException;
+
+    /**
+     * Returns all the messages exchanged by all the contacts
+     * in the supplied metacontact between the given dates and having the given
+     * keywords
+     *
+     * @param contact MetaContact
+     * @param startDate Date the start date of the conversations
+     * @param endDate Date the end date of the conversations
+     * @param keywords array of keywords
+     * @return Collection of MessageReceivedEvents or MessageDeliveredEvents
+     * @throws RuntimeException
+     */
     Collection findByPeriod(MetaContact contact, Date startDate, Date endDate, String[] keywords)
-        throws UnsupportedOperationException;
+        throws RuntimeException;
+
+    /**
+     * Returns all the messages exchanged by all the contacts
+     * in the supplied metacontact having the given keyword
+     *
+     * @param contact MetaContact
+     * @param keyword keyword
+     * @return Collection of MessageReceivedEvents or MessageDeliveredEvents
+     * @throws RuntimeException
+     */
     Collection findByKeyword(MetaContact contact, String keyword)
         throws RuntimeException;
+
+    /**
+     * Returns all the messages exchanged by all the contacts
+     * in the supplied metacontact having the given keywords
+     *
+     * @param contact MetaContact
+     * @param keywords keyword
+     * @return Collection of MessageReceivedEvents or MessageDeliveredEvents
+     * @throws RuntimeException
+     */
     Collection findByKeywords(MetaContact contact, String[] keywords)
         throws RuntimeException;
+
+    /**
+     * Returns the supplied number of recent messages exchanged by all the contacts
+     * in the supplied metacontact
+     *
+     * @param contact MetaContact
+     * @param count messages count
+     * @return Collection of MessageReceivedEvents or MessageDeliveredEvents
+     * @throws RuntimeException
+     */
     Collection findLast(MetaContact contact, int count)
         throws RuntimeException;
 }
