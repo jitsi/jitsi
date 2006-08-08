e2976c311a3eea9ab4ee6ef5d642bd250000cc84
Emil Ivov <emcho@jitsi.org>
2006-08-08 15:05:43 +0000

    Implementing SIP support (work in progress)
    Entrry point for tests in the GENERIC SLICK (may be unneeded ...)

---
+++ test/net/java/sip/communicator/slick/protocol/GenericProtocolProviderServiceLick.java
@@ -0,0 +1,55 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.slick.protocol;
+
+import junit.framework.*;
+import org.osgi.framework.*;
+import net.java.sip.communicator.util.*;
+
+/**
+ * The generic protocol provider SLICK implements a set of tests that any
+ * protocol provider implementation should pass. It does not contain any
+ * protocol specific code and only uses generic calls to methods defined by
+ * the ProtocolProviderService.
+ * <p>
+ * Protocol specific tests are implemented from some protocols in
+ * slick.protocol.<protocol_name> subpackages.
+ * <p>
+ * Tests in this SLICK use accounts defined in the accounts xml file and
+ * create two instances of the tested provider making sure that they can see
+ * each other and communicate properly.
+ * <p>
+ * @author Emil Ivov
+ */
+public class GenericProtocolProviderServiceLick
+    extends TestCase
+    implements BundleActivator
+{
+    private static final Logger logger =
+        Logger.getLogger(GenericProtocolProviderServiceLick.class);
+
+    /**
+     * Registers generic protocol test suits for currently registered protocol
+     * providers.
+     *
+     * @param context A currently valid bundle context
+     */
+    public void start(BundleContext context)
+    {
+
+    }
+
+    /**
+     * Prepares the SLICK for shutdown.
+     *
+     * @param context a valid OSGI bundle context.
+     */
+    public void stop(BundleContext context)
+    {
+
+    }
+}
