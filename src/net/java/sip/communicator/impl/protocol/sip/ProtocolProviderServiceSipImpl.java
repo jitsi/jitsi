4721ee74ec3f7f9dda35ddd05851b83636d09ed8
Emil Ivov <emcho@jitsi.org>
2006-08-08 15:06:34 +0000

    Implementing SIP support (work in progress)
    continuing work on sip support

--- src/net/java/sip/communicator/impl/protocol/sip/ProtocolProviderServiceSipImpl.java
+++ src/net/java/sip/communicator/impl/protocol/sip/ProtocolProviderServiceSipImpl.java
@@ -14,6 +14,7 @@ import javax.sip.*;
 import javax.sip.message.*;
 import javax.sip.header.*;
 import javax.sip.address.*;
+import net.java.sip.communicator.util.*;
 
 /**
  * A SIP implementation of the Protocol Provider Service.
@@ -22,6 +23,14 @@ import javax.sip.address.*;
 public class ProtocolProviderServiceSipImpl
   implements ProtocolProviderService
 {
+    private static final Logger logger =
+        Logger.getLogger(ProtocolProviderServiceSipImpl.class);
+
+    /**
+     * The hashtable with the operation sets that we support locally.
+     */
+    private Hashtable supportedOperationSets = new Hashtable();
+
     /**
      * The identifier of the account that this provider represents.
      */
@@ -36,6 +45,12 @@ public class ProtocolProviderServiceSipImpl
      * indicates whether or not the provider is initialized and ready for use.
      */
     private boolean isInitialized = false;
+
+    /**
+     * A list of all listeners registered for
+     * <tt>RegistrationStateChangeEvent</tt>s.
+     */
+    private List registrationListeners = new ArrayList();
 
     /**
      * The SipFactory instance used to create the SipStack and the Address
@@ -84,8 +99,32 @@ public class ProtocolProviderServiceSipImpl
     public void addRegistrationStateChangeListener(
         RegistrationStateChangeListener listener)
     {
-        /** @todo implement addRegistrationStateChangeListener() */
+        registrationListeners.add(listener);
     }
+
+    /**
+     * Creates a RegistrationStateChange event corresponding to the specified
+     * old and new jain sip states and notifies all currently registered
+     * listeners.
+     *
+     */
+    private void fireRegistrationStateChanged(  )
+    {
+        RegistrationStateChangeEvent event = null;
+
+        logger.debug("Dispatching " + event + " to "
+                     + registrationListeners.size()+ " listeners.");
+
+        for (int i = 0; i < registrationListeners.size(); i++)
+        {
+            RegistrationStateChangeListener listener
+                = (RegistrationStateChangeListener)registrationListeners.get(i);
+            listener.registrationStateChanged(event);
+        }
+
+        logger.trace("Done.");
+    }
+
 
     /**
      * Ends the registration of this protocol provider with the current
@@ -128,8 +167,7 @@ public class ProtocolProviderServiceSipImpl
      */
     public String getProtocolName()
     {
-        /** @todo implement getProtocolName() */
-        return "";
+        return ProtocolNames.SIP;
     }
 
     /**
@@ -149,7 +187,7 @@ public class ProtocolProviderServiceSipImpl
     public void removeRegistrationStateChangeListener(
         RegistrationStateChangeListener listener)
     {
-        /** @todo implement removeRegistrationStateChangeListener() */
+        this.registrationListeners.remove(listener);
     }
 
     /**
@@ -166,8 +204,7 @@ public class ProtocolProviderServiceSipImpl
      */
     public Map getSupportedOperationSets()
     {
-        /** @todo implement getSupportedOperationSets() */
-        return null;
+        return supportedOperationSets;
     }
 
     /**
@@ -203,6 +240,80 @@ public class ProtocolProviderServiceSipImpl
         {
             this.accountID = accountID;
 
+            sipFactory = SipFactory.getInstance();
+            sipFactory.setPathName("gov.nist");
+            Properties properties = new Properties();
+
+            //set the transport
+//            String transport
+//                = (String)accountID.getAccountProperties()
+//                    .get(PROTOCOL_TRANSPORT);
+//            if(transport == null || transport.trim().length() == 0)
+//            {
+//                transport = ListeningPoint.UDP;
+//            }
+
+            //no need to check whether specified transport is valid.
+            //the stack will do that for us.
+
+            //set the proxy
+            String proxyAddress
+                = (String)accountID.getAccountProperties()
+                    .get(ProtocolProviderFactory.PROXY_ADDRESS);
+
+//            if (proxyAddress != null && proxyAddress.trim().length() > 0)
+
+//            String proxyPort
+//                = accountID.getAccountProperties()
+//                        .get(ProtocolProviderFactory.PROXY_PORT);
+
+//            String proxyPort
+//            properties.setProperty("javax.sip.OUTBOUND_PROXY", peerHostPort + "/"
+//                                   + transport);
+            // If you want to use UDP then uncomment this.
+            properties.setProperty("javax.sip.STACK_NAME", "SIP Communicator:"
+                + getAccountID().getAccountUniqueID());
+
+            // NIST SIP specific properties
+            properties.setProperty("gov.nist.javax.sip.DEBUG_LOG"
+                                   , "shootistdebug.txt");
+            properties.setProperty("gov.nist.javax.sip.SERVER_LOG",
+                                    "shootistlog.txt");
+
+            // Drop the client connection after we are done with the transaction.
+            properties.setProperty("gov.nist.javax.sip.CACHE_CLIENT_CONNECTIONS"
+                                   , "false");
+
+            // Log level
+            properties.setProperty("gov.nist.javax.sip.TRACE_LEVEL", "TRACE");
+
+            try
+            {
+                // Create SipStack object
+                sipStack = sipFactory.createSipStack(properties);
+                logger.debug("Created stack: " + sipStack);
+            }
+            catch (PeerUnavailableException e)
+            {
+                // could not find
+                // gov.nist.jain.protocol.ip.sip.SipStackImpl
+                // in the classpath
+                logger.fatal("Failed to initialize SIP Stack.", e);
+            }
+
+            try
+            {
+                headerFactory = sipFactory.createHeaderFactory();
+                addressFactory = sipFactory.createAddressFactory();
+                messageFactory = sipFactory.createMessageFactory();
+                //    udpListeningPoint = sipStack.createListeningPoint("127.0.0.1",
+                //        5060, "udp");
+                //    sipProvider = sipStack.createSipProvider(udpListeningPoint);
+                //    sipProvider.addSipListener(null);
+            }
+            catch (Throwable t)
+            {}
+
             isInitialized = true;
         }
     }
@@ -215,5 +326,4 @@ public class ProtocolProviderServiceSipImpl
     public void shutdown()
     {
     }
-
 }
