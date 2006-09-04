cde704af26ed18ab7a4f8a42232b21c87e7a36fd
Damian Minkov <damencho@jitsi.org>
2006-09-04 11:34:25 +0000

    Added case in/senstivie search

--- src/net/java/sip/communicator/service/history/HistoryReader.java
+++ src/net/java/sip/communicator/service/history/HistoryReader.java
@@ -10,7 +10,10 @@ import java.util.*;
 import net.java.sip.communicator.service.history.event.*;
 
 /**
+ * Used to serach over the history records
+ *
  * @author Alexander Pelov
+ * @author Damian Minkov
  */
 public interface HistoryReader {
 
@@ -65,6 +68,20 @@ public interface HistoryReader {
     QueryResultSet findByKeyword(String keyword, String field) throws RuntimeException;
 
     /**
+     * Searches the history for all records containing the <tt>keyword</tt>.
+     *
+     * @param keyword the keyword to search for
+     * @param field the field where to look for the keyword
+     * @param caseSensitive is keywords search case sensitive
+     * @return the found records
+     * @throws RuntimeException
+     *             Thrown if an exception occurs during the execution of the
+     *             query, such as internal IO error.
+     */
+    QueryResultSet findByKeyword(String keyword, String field, boolean caseSensitive)
+        throws RuntimeException;
+
+    /**
      * Searches the history for all records containing all <tt>keywords</tt>.
      *
      * @param keywords array of keywords we search for
@@ -75,6 +92,20 @@ public interface HistoryReader {
      *             query, such as internal IO error.
      */
     QueryResultSet findByKeywords(String[] keywords, String field) throws RuntimeException;
+
+    /**
+     * Searches the history for all records containing all <tt>keywords</tt>.
+     *
+     * @param keywords array of keywords we search for
+     * @param field the field where to look for the keyword
+     * @param caseSensitive is keywords search case sensitive
+     * @return the found records
+     * @throws RuntimeException
+     *             Thrown if an exception occurs during the execution of the
+     *             query, such as internal IO error.
+     */
+    QueryResultSet findByKeywords(String[] keywords, String field, boolean caseSensitive)
+        throws RuntimeException;
 
     /**
      * Searches for all history records containing all <tt>keywords</tt>,
@@ -94,12 +125,41 @@ public interface HistoryReader {
             throws UnsupportedOperationException;
 
     /**
+     * Searches for all history records containing all <tt>keywords</tt>,
+     * with timestamp between <tt>startDate</tt> and <tt>endDate</tt>.
+     *
+     * @param startDate start of the interval in which we search
+     * @param endDate end of the interval in which we search
+     * @param keywords array of keywords we search for
+     * @param field the field where to look for the keyword
+     * @param caseSensitive is keywords search case sensitive
+     * @return the found records
+     * @throws UnsupportedOperationException
+     *             Thrown if an exception occurs during the execution of the
+     *             query, such as internal IO error.
+     */
+    QueryResultSet findByPeriod(Date startDate, Date endDate,
+                                String[] keywords, String field, boolean caseSensitive)
+        throws UnsupportedOperationException;
+
+    /**
+     * Returns the supplied number of recent messages
+     *
+     * @param count messages count
+     * @return the found records
+     * @throws RuntimeException
+     */
+    QueryResultSet findLast(int count) throws RuntimeException;
+
+    /**
+     * Adding progress listener for monitoring progress of search process
      *
      * @param listener HistorySearchProgressListener
      */
     void addSearchProgressListener(HistorySearchProgressListener listener);
 
     /**
+     * Removing progress listener
      *
      * @param listener HistorySearchProgressListener
      */
