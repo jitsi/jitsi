0cc9f98bef2ba5eb136631ddbb6f67205fcc11d4
Damian Minkov <damencho@jitsi.org>
2006-09-21 09:02:42 +0000

    Jabber impl Tests

---
+++ test/net/java/sip/communicator/slick/protocol/jabber/JabberProtocolProviderServiceLick.java
@@ -0,0 +1,106 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.slick.protocol.jabber;
+
+import java.util.*;
+
+import org.osgi.framework.*;
+import junit.framework.*;
+
+/**
+ * Jabber specific testing for a Jabber Protocol Provider Service implementation.
+ * The test suite registers two accounts for
+ *
+ * @author Damian Minkov
+ */
+public class JabberProtocolProviderServiceLick
+    extends    TestSuite
+    implements BundleActivator
+{
+    /**
+     * The prefix used for property names containing settings for our first
+     * testing account.
+     */
+    public static final String ACCOUNT_1_PREFIX
+        = "accounts.jabber.account1.";
+
+    /**
+     * The prefix used for property names containing settings for our second
+     * testing account.
+     */
+    public static final String ACCOUNT_2_PREFIX
+        = "accounts.jabber.account2.";
+
+    /**
+     * The name of the property that indicates whether the user would like to
+     * only run the offline tests.
+     */
+    public static final String DISABLE_ONLINE_TESTS_PROPERTY_NAME
+        = "accounts.jabber.DISABLE_ONLINE_TESTING";
+
+    /**
+     * The name of the property the value of which is a formatted string that
+     * contains the contact list that.
+     */
+    public static final String CONTACT_LIST_PROPERTY_NAME
+        = "accounts.jabber.CONTACT_LIST";
+
+    /**
+     * Initializes and registers all tests that we'll run as a part of this
+     * slick.
+     *
+     * @param context a currently valid bundle context.
+     */
+    public void start(BundleContext context)
+    {
+        setName("JabberProtocolProviderSlick");
+
+        Hashtable properties = new Hashtable();
+        properties.put("service.pid", getName());
+
+        JabberSlickFixture.bc = context;
+
+        // verify whether the user wants to avoid online testing
+        String offlineMode = System.getProperty(
+            DISABLE_ONLINE_TESTS_PROPERTY_NAME, null);
+
+        if (offlineMode != null && offlineMode.equalsIgnoreCase("true"))
+            JabberSlickFixture.onlineTestingDisabled = true;
+
+
+        addTestSuite(TestAccountInstallation.class);
+        addTestSuite(TestProtocolProviderServiceJabberImpl.class);
+
+        addTest(TestOperationSetPresence.suite());
+
+        //the following should only be run when we want online testing.
+        if(!JabberSlickFixture.onlineTestingDisabled)
+        {
+            addTest(TestOperationSetPersistentPresence.suite());
+
+            addTest(TestOperationSetBasicInstantMessaging.suite());
+
+            addTest(TestOperationSetTypingNotifications.suite());
+        }
+
+
+        addTest(TestAccountUninstallation.suite());
+        addTestSuite(TestAccountUninstallationPersistence.class);
+
+        context.registerService(getClass().getName(), this, properties);
+    }
+
+    /**
+     * Prepares the slick for shutdown.
+     *
+     * @param context a currently valid bundle context.
+     */
+    public void stop(BundleContext context)
+    {
+
+    }
+}
---
+++ test/net/java/sip/communicator/slick/protocol/jabber/TestAccountInstallation.java
@@ -0,0 +1,239 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.slick.protocol.jabber;
+
+import java.util.*;
+
+import org.osgi.framework.*;
+import junit.framework.*;
+import net.java.sip.communicator.service.protocol.*;
+import net.java.sip.communicator.util.*;
+
+public class TestAccountInstallation
+    extends TestCase
+{
+    private static final Logger logger =
+        Logger.getLogger(TestAccountInstallation.class);
+
+    /**
+     * Creates the test with the specified method name.
+     * @param name the name of the method to execute.
+     */
+    public TestAccountInstallation(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * JUnit setup method.
+     * @throws Exception in case anything goes wrong.
+     */
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+    }
+
+    /**
+     * JUnit teardown method.
+     * @throws Exception in case anything goes wrong.
+     */
+    protected void tearDown() throws Exception
+    {
+        super.tearDown();
+    }
+
+    /**
+     * Installs an account and verifies whether the installation has gone well.
+     */
+    public void testInstallAccount()
+    {
+        // first obtain a reference to the provider factory
+        ServiceReference[] serRefs = null;
+        String osgiFilter = "(" + ProtocolProviderFactory.PROTOCOL
+                            + "="+ProtocolNames.JABBER+")";
+        try{
+            serRefs = JabberSlickFixture.bc.getServiceReferences(
+                    ProtocolProviderFactory.class.getName(), osgiFilter);
+        }
+        catch (InvalidSyntaxException ex)
+        {
+            //this really shouldhn't occur as the filter expression is static.
+            fail(osgiFilter + " is not a valid osgi filter");
+        }
+
+        assertTrue(
+            "Failed to find a provider factory service for protocol Jabber",
+            serRefs != null && serRefs.length >  0);
+
+        //Keep the reference for later usage.
+        ProtocolProviderFactory jabberProviderFactory = (ProtocolProviderFactory)
+            JabberSlickFixture.bc.getService(serRefs[0]);
+
+        //make sure the account is empty
+        assertTrue("There was an account registered with the account mananger "
+                   +"before we've installed any",
+                   jabberProviderFactory.getRegisteredAccounts().size() == 0);
+
+
+        //Prepare the properties of the first jabber account.
+
+        Hashtable jabberAccount1Properties = getAccountProperties(
+            JabberProtocolProviderServiceLick.ACCOUNT_1_PREFIX);
+        Hashtable jabberAccount2Properties = getAccountProperties(
+            JabberProtocolProviderServiceLick.ACCOUNT_2_PREFIX);
+
+        //try to install an account with a null account id
+        try{
+            jabberProviderFactory.installAccount(
+                null, jabberAccount1Properties);
+            fail("installing an account with a null account id must result "
+                 +"in a NullPointerException");
+        }catch(NullPointerException exc)
+        {
+            //that's what had to happen
+        }
+
+        //now really install the accounts
+        jabberProviderFactory.installAccount(
+            (String)jabberAccount1Properties.get(ProtocolProviderFactory.USER_ID)
+            , jabberAccount1Properties);
+        jabberProviderFactory.installAccount(
+            (String)jabberAccount2Properties.get(ProtocolProviderFactory.USER_ID)
+            , jabberAccount2Properties);
+
+
+        //try to install one of the accounts one more time and verify that an
+        //excepion is thrown.
+        try{
+            jabberProviderFactory.installAccount(
+                (String)jabberAccount1Properties.get(ProtocolProviderFactory.USER_ID)
+                , jabberAccount1Properties);
+
+            fail("An IllegalStateException must be thrown when trying to "+
+                 "install a duplicate account");
+
+        }catch(IllegalStateException exc)
+        {
+            //that's what supposed to happen.
+        }
+
+        //Verify that the provider factory is aware of our installation
+        assertTrue(
+            "The newly installed account was not in the acc man's "
+            +"registered accounts!",
+            jabberProviderFactory.getRegisteredAccounts().size() == 2);
+
+        //Verify protocol providers corresponding to the new account have
+        //been properly registered with the osgi framework.
+
+        osgiFilter =
+            "(&("+ProtocolProviderFactory.PROTOCOL +"="+ProtocolNames.JABBER+")"
+             +"(" + ProtocolProviderFactory.USER_ID
+             + "=" + (String)jabberAccount1Properties.get(
+                            ProtocolProviderFactory.USER_ID)
+             + "))";
+
+        try
+        {
+            serRefs = JabberSlickFixture.bc.getServiceReferences(
+                    ProtocolProviderService.class.getName(),
+                    osgiFilter);
+        }
+        catch (InvalidSyntaxException ex)
+        {
+            //this really shouldhn't occur as the filter expression is static.
+            fail(osgiFilter + "is not a valid osgi filter");
+        }
+
+        assertTrue("An protocol provider was apparently not installed as "
+                + "requested."
+                , serRefs != null && serRefs.length > 0);
+
+        Object jabberProtocolProvider
+            = JabberSlickFixture.bc.getService(serRefs[0]);
+
+        assertTrue("The installed protocol provider does not implement "
+                  + "the protocol provider service."
+                  ,jabberProtocolProvider instanceof ProtocolProviderService);
+    }
+
+    /**
+     * Returns all properties necessary for the intialization of the account
+     * with <tt>accountPrefix</tt>.
+     * @param accountPrefix the prefix contained by all property names for the
+     * the account we'd like to initialized
+     * @return a Hashtable that can be used when creating the account in a
+     * protocol provider factory.
+     */
+    private Hashtable getAccountProperties(String accountPrefix)
+    {
+        Hashtable table = new Hashtable();
+
+        String userID = System.getProperty(
+            accountPrefix + ProtocolProviderFactory.USER_ID, null);
+
+        assertNotNull(
+            "The system property named "
+            + accountPrefix + ProtocolProviderFactory.USER_ID
+            +" has to tontain a valid Jabber address that could be used during "
+            +"SIP Communicator's tests."
+            , userID);
+
+        table.put(ProtocolProviderFactory.USER_ID, userID);
+
+        String passwd = System.getProperty(
+            accountPrefix + ProtocolProviderFactory.PASSWORD, null );
+
+        assertNotNull(
+            "The system property named "
+            + accountPrefix + ProtocolProviderFactory.PASSWORD
+            +" has to contain the password corresponding to the account "
+            + "specified in "
+            + accountPrefix + ProtocolProviderFactory.USER_ID
+            , passwd);
+
+        table.put(ProtocolProviderFactory.PASSWORD, passwd);
+
+//        String serverAddress = System.getProperty(
+//            accountPrefix + ProtocolProviderFactory.SERVER_ADDRESS, null );
+//
+//        assertNotNull(
+//            "The system property named "
+//            + accountPrefix + ProtocolProviderFactory.SERVER_ADDRESS
+//            +" has to contain a valid server address to use for testing."
+//            , serverAddress);
+//
+//        table.put(ProtocolProviderFactory.SERVER_ADDRESS, serverAddress);
+//
+//        String serverPort = System.getProperty(
+//            accountPrefix + ProtocolProviderFactory.SERVER_PORT, null );
+//
+//        if(serverPort != null)
+//        {
+//            table.put(ProtocolProviderFactory.SERVER_PORT, serverPort);
+//        }
+//
+//        String proxyAddress = System.getProperty(
+//            accountPrefix + ProtocolProviderFactory.PROXY_ADDRESS, null );
+//
+//        if(serverPort != null)
+//        {
+//            table.put(ProtocolProviderFactory.PROXY_ADDRESS, proxyAddress);
+//
+//            String proxyPort = System.getProperty(
+//            accountPrefix + ProtocolProviderFactory.PROXY_PORT, null );
+//
+//            if(proxyPort != null)
+//            {
+//                table.put(ProtocolProviderFactory.PROXY_PORT, proxyPort);
+//            }
+//        }
+
+
+        return table;
+    }
+}
