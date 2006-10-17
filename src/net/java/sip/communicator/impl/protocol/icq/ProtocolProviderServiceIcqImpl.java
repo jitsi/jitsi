74a46263946ef6d0b367fa0347708eb7fe60e6a2
Damian Minkov <damencho@jitsi.org>
2006-10-17 15:58:37 +0000

    Fix for icq passwords longer than 8 characters

--- src/net/java/sip/communicator/impl/protocol/icq/ProtocolProviderServiceIcqImpl.java
+++ src/net/java/sip/communicator/impl/protocol/icq/ProtocolProviderServiceIcqImpl.java
@@ -213,6 +213,12 @@ public class ProtocolProviderServiceIcqImpl
                 }
             }
 
+            // it seems icq servers doesn't accept password with
+            // length more then 8. But allow such registrations
+            // we must trim such passwords to 8 characters
+            if(password.length() > 0)
+                password = password.substring(0, 8);
+
             //init the necessary objects
             session = new DefaultAppSession();
             aimSession = session.openAimSession(
