30b48242b4b2c1e1052b0aa7b2e9bfafbd7792c0
Emil Ivov <emcho@jitsi.org>
2006-10-23 16:23:52 +0000

    Working on the JMF implementation of the media service.
    Not using service binder anymore (temporarily?) and added utility methods for retrieving common used service implementations.

--- src/net/java/sip/communicator/impl/media/MediaActivator.java
+++ src/net/java/sip/communicator/impl/media/MediaActivator.java
@@ -6,14 +6,158 @@
  */
 package net.java.sip.communicator.impl.media;
 
-import org.ungoverned.gravity.servicebinder.*;
+import org.osgi.framework.*;
+import net.java.sip.communicator.util.*;
+import net.java.sip.communicator.service.configuration.*;
+import net.java.sip.communicator.service.netaddr.*;
+import java.util.*;
+import net.java.sip.communicator.service.media.*;
+import net.java.sip.communicator.service.fileaccess.*;
+
 
 /**
  * Invoke "Service Binder" to parse the service XML and register
  * all services.
  *
  * @author Martin Andre
+ * @author Emil Ivov
  */
-public class MediaActivator extends GenericActivator {
+public class MediaActivator
+    implements BundleActivator
+{
+    private Logger logger = Logger.getLogger(MediaActivator.class.getName());
+
+    private static MediaServiceImpl mediaServiceImpl = null;
+
+    private static BundleContext        bundleContext         = null;
+    private static ConfigurationService configurationService  = null;
+    private static NetworkAddressManagerService networkAddressManagerService
+                                                              = null;
+    private static FileAccessService    fileAccessService     = null;
+
+    private ServiceRegistration mediaServiceRegistration      = null;
+
+    /**
+     * Called when this bundle is started so the Framework can perform the
+     * bundle-specific activities necessary to start this bundle.
+     *
+     * @param context The execution context of the bundle being started.
+     * @throws Exception If this method throws an exception, this bundle is
+     *   marked as stopped and the Framework will remove this bundle's
+     *   listeners, unregister all services registered by this bundle, and
+     *   release all services used by this bundle.
+     */
+    public void start(BundleContext context)
+        throws Exception
+    {
+        logger.debug("Started.");
+        this.bundleContext = context;
+        Hashtable hashtable = new Hashtable();
+
+        mediaServiceImpl = new MediaServiceImpl();
+
+        //load all icq providers
+        mediaServiceImpl.start();
+
+        //reg the icq account man.
+        mediaServiceRegistration = context.registerService(
+            MediaService.class.getName(),
+            mediaServiceImpl,
+            hashtable);
+
+
+        logger.debug("Media Service ... [REGISTERED]");
+    }
+
+    /**
+     * Returns a reference to a ConfigurationService implementation currently
+     * registered in the bundle context or null if no such implementation was
+     * found.
+     *
+     * @return a currently valid implementation of the ConfigurationService.
+     */
+    public static ConfigurationService getConfigurationService()
+    {
+        if (configurationService == null)
+        {
+            ServiceReference confReference
+                = bundleContext.getServiceReference(
+                    ConfigurationService.class.getName());
+            configurationService
+                = (ConfigurationService) bundleContext.getService(confReference);
+        }
+        return configurationService;
+    }
+
+    /**
+     * Returns a reference to a NetworkAddressManagerService implementation
+     * currently registered in the bundle context or null if no such
+     * implementation was found.
+     *
+     * @return a currently valid implementation of the
+     * NetworkAddressManagerService .
+     */
+    public static NetworkAddressManagerService getNetworkAddressManagerService()
+    {
+        if (networkAddressManagerService == null)
+        {
+            ServiceReference namReference
+                = bundleContext.getServiceReference(
+                    NetworkAddressManagerService.class.getName());
+            networkAddressManagerService = (NetworkAddressManagerService)
+                bundleContext.getService(namReference);
+        }
+        return networkAddressManagerService;
+    }
+
+    /**
+     * Returns a reference to a FileAccessService implementation
+     * currently registered in the bundle context or null if no such
+     * implementation was found.
+     *
+     * @return a currently valid implementation of the
+     * FileAccessService .
+     */
+    public static FileAccessService getFileAccessService()
+    {
+        if (fileAccessService == null && bundleContext != null)
+        {
+            ServiceReference faReference
+                = bundleContext.getServiceReference(
+                    FileAccessService.class.getName());
+
+            fileAccessService = (FileAccessService)bundleContext
+                .getService(faReference);
+        }
+        return fileAccessService;
+    }
+
+
+    /**
+     * Returns a reference to the bundle context that we were started with.
+     * @return a reference to the BundleContext instance that we were started
+     * witn.
+     */
+    public static BundleContext getBundleContext()
+    {
+        return bundleContext;
+    }
+
+    /**
+     * Called when this bundle is stopped so the Framework can perform the
+     * bundle-specific activities necessary to stop the bundle.
+     *
+     * @param context The execution context of the bundle being stopped.
+     * @throws Exception If this method throws an exception, the bundle is
+     *   still marked as stopped, and the Framework will remove the bundle's
+     *   listeners, unregister all services registered by the bundle, and
+     *   release all services used by the bundle.
+     */
+    public void stop(BundleContext context)
+        throws Exception
+    {
+        mediaServiceImpl.stop();
+        mediaServiceRegistration.unregister();
+    }
 
 }
