45c7ac54f187e36b67d96be8cf704508781658ca
Emil Ivov <emcho@jitsi.org>
2006-12-01 01:52:05 +0000

    add $LIBPATH/native to LD_LIBRARY_PATH so that jmf .so's are available to the media package

--- resources/install/debian/sip-communicator.sh
+++ resources/install/debian/sip-communicator.sh
@@ -7,11 +7,11 @@ if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 eval set -- "$TEMP"
 
 while true ; do
-		case "$1" in
-				-V|--version) echo "SIP Communicator version 1.0-alpha1"; exit 0;;
-				--) shift ; break ;;
-				*) echo "Internal error!" ; exit 1 ;;
-		esac
+        case "$1" in
+                -V|--version) echo "SIP Communicator version 1.0-alpha1"; exit 0;;
+                --) shift ; break ;;
+                *) echo "Internal error!" ; exit 1 ;;
+        esac
 done
 
 javabin=`which java`
@@ -19,12 +19,19 @@ javabin=`which java`
 SCDIR=/usr/share/sip-communicator
 LIBPATH=$SCDIR/lib
 
+#set add LIBPATH to LD_LIBRARY_PATH for any sc natives (e.g. jmf .so's)
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBPATH/native
+
+#make LD_PRELOAD use libaoss so that java doesn't hog on the audio device.
 export LD_PRELOAD=/usr/lib/libaoss.so
+
+#create .sip-commnicator/log in home or otherwise java.util.logging will freak
+mkdir -p $HOME/.sip-communicator/log
 
 cd $SCDIR
 
 if [ -f $javabin ]
 then
-		$javabin -classpath "$LIBPATH/BrowserLauncher2.jar:$LIBPATH/JainSipApi1.2.jar:$LIBPATH/JainSipRi1.2.jar:$LIBPATH/Stun4J.jar:$LIBPATH/cindy.jar:$LIBPATH/commons-logging.jar:$LIBPATH/concurrent.jar:$LIBPATH/felix.jar:$LIBPATH/jml-1.0a3.jar:$LIBPATH/joscar-0.9.4-cvs-bin.jar:$LIBPATH/kxml-min.jar:$LIBPATH/nist-sdp-1.0.jar:$LIBPATH/oscar-aim.jar:$LIBPATH/retroweaver-rt.jar:$LIBPATH/retroweaver.jar:$LIBPATH/servicebinder.jar:$LIBPATH/sip-sdp.jar:$LIBPATH/smack.jar:$LIBPATH/smackx.jar" -Dicq.custom.message.charset=windows-1251 -Dfelix.config.properties=file:$LIBPATH/felix.client.run.properties -Djava.util.logging.config.file=$LIBPATH/logging.properties org.apache.felix.main.Main
-		exit $?
+        $javabin -classpath "$LIBPATH/felix.jar:$LIBPATH/kxml-min.jar:$LIBPATH/servicebinder.jar:$LIBPATH/bundle/org.apache.felix.servicebinder-0.8.0-SNAPSHOT.jar:$LIBPATH/jmf.jar:$SCDIR/sc-bundles/util.jar" -Dicq.custom.message.charset=windows-1251 -Dfelix.config.properties=file:$LIBPATH/felix.client.run.properties -Djava.util.logging.config.file=$LIBPATH/logging.properties org.apache.felix.main.Main
+        exit $?
 fi
