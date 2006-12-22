85941de145cf1831a1829541a2958bf3fa535bde
Emil Ivov <emcho@jitsi.org>
2006-12-22 14:09:15 +0000

    Javadoc Comment fixes.

--- src/net/java/sip/communicator/impl/gui/main/message/menus/HelpMenu.java
+++ src/net/java/sip/communicator/impl/gui/main/message/menus/HelpMenu.java
@@ -17,52 +17,54 @@ import net.java.sip.communicator.impl.gui.main.message.*;
 import net.java.sip.communicator.util.*;
 /**
  * The <tt>HelpMenu</tt> is a menu in the main application menu bar.
- * 
+ *
  * @author Yana Stamcheva
  */
 public class HelpMenu
-    extends JMenu 
+    extends JMenu
     implements ActionListener
 {
 
     private Logger logger = Logger.getLogger(HelpMenu.class.getName());
-    
+
     private I18NString aboutString = Messages.getI18NString("about");
-    
+
     private JMenuItem aboutItem = new JMenuItem(aboutString.getText());
-    
+
     private ChatWindow chatWindow;
 
     /**
      * Creates an instance of <tt>HelpMenu</tt>.
-     * @param mainFrame The parent <tt>MainFrame</tt>.
+     * @param chatWindow The parent <tt>MainFrame</tt>.
      */
     public HelpMenu(ChatWindow chatWindow) {
 
         super(Messages.getI18NString("help").getText());
-        
+
         this.chatWindow = chatWindow;
-        
+
         this.add(aboutItem);
-        
+
         this.aboutItem.setName("about");
-        
+
         this.aboutItem.addActionListener(this);
-        
-        this.aboutItem.setMnemonic(aboutString.getMnemonic());        
+
+        this.aboutItem.setMnemonic(aboutString.getMnemonic());
     }
 
     /**
      * Handles the <tt>ActionEvent</tt> when one of the menu items is selected.
+     *
+     * @param evt the ActionEvent that has just occurred.
      */
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent evt) {
 
-        JMenuItem menuItem = (JMenuItem) e.getSource();
+        JMenuItem menuItem = (JMenuItem) evt.getSource();
         String itemName = menuItem.getName();
 
         if (itemName.equals("about")) {
             AboutWindow aboutWindow = new AboutWindow(chatWindow);
-            
+
             aboutWindow.setVisible(true);
         }
     }
