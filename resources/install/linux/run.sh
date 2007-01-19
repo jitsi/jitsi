8425ffb8e938fbf73e1f41335dff8c4e88469ea0
Emil Ivov <emcho@jitsi.org>
2007-01-19 11:17:11 +0000

    disabling sip-communicator launcher.

--- resources/install/linux/run.sh
+++ resources/install/linux/run.sh
@@ -2,4 +2,4 @@ mkdir -p $HOME/.sip-communicator/log
 
 export PATH=$PATH:native
 export JAVA_HOME=jre
-${JAVA_HOME}/bin/java -classpath "lib/BrowserLauncher2.jar:lib/felix.jar:lib/kxml-min.jar:lib/servicebinder.jar:lib/bundle/org.apache.felix.servicebinder-0.8.0-SNAPSHOT.jar:sc-bundles/util.jar" -Dicq.custom.message.charset=windows-1251 -Dfelix.config.properties=file:./lib/felix.client.run.properties -Djava.util.logging.config.file=lib/logging.properties net.java.sip.communicator.launcher.SIPCommunicator
+${JAVA_HOME}/bin/java -classpath "lib/BrowserLauncher2.jar:lib/felix.jar:lib/kxml-min.jar:lib/servicebinder.jar:lib/bundle/org.apache.felix.servicebinder-0.8.0-SNAPSHOT.jar:sc-bundles/util.jar" -Dicq.custom.message.charset=windows-1251 -Dfelix.config.properties=file:./lib/felix.client.run.properties -Djava.util.logging.config.file=lib/logging.properties org.apache.felix.main.Main
