1b519373efab59c1e3118007e32c602ac7ae65ee
Yana Stamcheva <yana@jitsi.org>
2007-01-25 16:32:27 +0000

    launch browser in a separate thread

--- src/net/java/sip/communicator/impl/browserlauncher/BrowserLauncherImpl.java
+++ src/net/java/sip/communicator/impl/browserlauncher/BrowserLauncherImpl.java
@@ -25,40 +25,58 @@ public class BrowserLauncherImpl
 
     public void openURL(String url)
     {
-        String osName = System.getProperty("os.name");
+        new LaunchBrowser(url).start();
+    }
+    
+    /**
+     * Launch browser in a separate thread.
+     */
+    private class LaunchBrowser extends Thread
+    {
+        private String url;
         
-        try
+        public LaunchBrowser(String url)
         {
-           if (osName.startsWith("Mac OS"))
-           {
-               FileManager.openURL(url);
-           }
-           else if (osName.startsWith("Windows"))
-           {
-              Runtime.getRuntime().exec(
-                  "rundll32 url.dll,FileProtocolHandler " + url);
-           }
-           else
-           {
-              String[] browsers = {"firefox", "icedove", "opera", "konqueror",
-                  "epiphany", "mozilla", "netscape" };
-              
-              String browser = null;
-              
-              for (int i = 0; i < browsers.length && browser == null; i ++)
-              {
-                 if (Runtime.getRuntime().exec(
-                       new String[] {"which", browsers[i]}).waitFor() == 0)
-                    browser = browsers[i];
-              }
-              if (browser == null)
-                 throw new Exception("Could not find web browser");
-              else
-                 Runtime.getRuntime().exec(new String[] {browser, url});
-           }
+            this.url = url;
         }
-        catch (Exception e) {
-           logger.error("Failed to launch browser: " + e);
-       }
+        
+        public void run()
+        {
+            try
+            {
+                String osName = System.getProperty("os.name");
+                
+                if (osName.startsWith("Mac OS"))
+                {
+                    FileManager.openURL(url);
+                }
+                else if (osName.startsWith("Windows"))
+                {
+                   Runtime.getRuntime().exec(
+                       "rundll32 url.dll,FileProtocolHandler " + url);
+                }
+                else
+                {
+                   String[] browsers = {"firefox", "icedove", "opera", "konqueror",
+                       "epiphany", "mozilla", "netscape" };
+                   
+                   String browser = null;
+                   
+                   for (int i = 0; i < browsers.length && browser == null; i ++)
+                   {
+                      if (Runtime.getRuntime().exec(
+                            new String[] {"which", browsers[i]}).waitFor() == 0)
+                         browser = browsers[i];
+                   }
+                   if (browser == null)
+                      throw new Exception("Could not find web browser");
+                   else
+                      Runtime.getRuntime().exec(new String[] {browser, url});
+                }
+            }
+            catch (Exception e) {
+               logger.error("Failed to launch browser: " + e);
+            }
+        }
     }
 }
