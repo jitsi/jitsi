7c1c6be3e07c86a9c36f402b8fbea8d60856fe27
Emil Ivov <emcho@jitsi.org>
2007-02-22 12:33:02 +0000

    Added multichat operation set definition and Gibberish protocol implementation. Related issues: Issue #241 , Issue #244, Issue #250
    A gibberish implementation of an AccountID

---
+++ src/net/java/sip/communicator/impl/protocol/gibberish/GibberishAccountID.java
@@ -0,0 +1,32 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.impl.protocol.gibberish;
+
+import net.java.sip.communicator.service.protocol.*;
+import java.util.Map;
+
+/**
+ * The Gibberish implementation of a sip-communicator account id.
+ * @author Emil Ivov
+ */
+public class GibberishAccountID
+    extends AccountID
+{
+    /**
+     * Creates an account id from the specified id and account properties.
+     *
+     * @param userID the user identifier correspnding to thi account
+     * @param accountProperties any other properties necessary for the account.
+     */
+    GibberishAccountID(String userID, Map accountProperties)
+    {
+        super(userID
+              , accountProperties
+              , "Gibberish"
+              , "gibberish.org");
+    }
+}
