9b91d48d984ded5c00f8a20ca2cde95f8468377b
Emil Ivov <emcho@jitsi.org>
2007-02-22 12:38:28 +0000

    Added multichat operation set definition and Gibberish protocol implementation. Related issues: Issue #241 , Issue #244, Issue #250

--- src/net/java/sip/communicator/impl/gui/main/account/AccountRegFirstPage.java
+++ src/net/java/sip/communicator/impl/gui/main/account/AccountRegFirstPage.java
@@ -27,7 +27,7 @@ import net.java.sip.communicator.service.gui.event.*;
  * The <tt>AccountRegFirstPage</tt> is the first page of the account
  * registration wizard. This page contains a list of all registered
  * <tt>AccountRegistrationWizard</tt>s.
- * 
+ *
  * @author Yana Stamcheva
  */
 public class AccountRegFirstPage extends JPanel
@@ -36,60 +36,60 @@ public class AccountRegFirstPage extends JPanel
                 ListSelectionListener,
                 MouseListener
 {
-        
+
     private String nextPageIdentifier;
 
     private ExtendedTableModel tableModel;
-    
+
     private JTable accountRegsTable = new JTable();
-    
+
     private JScrollPane tableScrollPane = new JScrollPane();
-    
+
     private AccountRegWizardContainerImpl wizardContainer;
-    
+
     private JPanel textPanel = new JPanel(new GridLayout(0, 1, 5, 5));
-    
+
     private JTextArea messageTextArea = new JTextArea(
             Messages.getI18NString("selectAccountRegistration").getText());
-    
-    private JLabel pageTitleLabel 
+
+    private JLabel pageTitleLabel
         = new JLabel(
-            Messages.getI18NString("selectAccountRegWizardTitle").getText(), 
+            Messages.getI18NString("selectAccountRegWizardTitle").getText(),
                 JLabel.CENTER);
-    
+
     public AccountRegFirstPage(AccountRegWizardContainerImpl container) {
-        super(new BorderLayout(10, 10));    
-        
+        super(new BorderLayout(10, 10));
+
         this.wizardContainer = container;
-        
+
         this.tableModel = new ExtendedTableModel();
-        
+
         this.setPreferredSize(new Dimension(500, 400));
-        
+
         this.accountRegsTable.setSelectionMode(
                 ListSelectionModel.SINGLE_SELECTION);
-        
+
         this.accountRegsTable.getSelectionModel()
             .addListSelectionListener(this);
-        
+
         this.tableInit();
-        
+
         this.messageTextArea.setLineWrap(true);
         this.messageTextArea.setWrapStyleWord(true);
         this.messageTextArea.setOpaque(false);
         this.messageTextArea.setEditable(false);
-        
+
         this.pageTitleLabel.setFont(Constants.FONT.deriveFont(Font.BOLD, 18));
-        
+
         this.textPanel.add(pageTitleLabel);
         this.textPanel.add(messageTextArea);
-        
+
         this.add(textPanel, BorderLayout.NORTH);
         this.add(tableScrollPane, BorderLayout.CENTER);
-        
+
         this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
     }
-    
+
     /**
      * Initializes the account registration's table.
      */
@@ -99,27 +99,27 @@ public class AccountRegFirstPage extends JPanel
         this.tableModel.addColumn("id");
         this.tableModel.addColumn(Messages.getI18NString("name").getText());
         this.tableModel.addColumn(Messages.getI18NString("description").getText());
-     
+
         accountRegsTable.setRowHeight(22);
         accountRegsTable.setShowHorizontalLines(false);
         accountRegsTable.setShowVerticalLines(false);
         accountRegsTable.setModel(this.tableModel);
         accountRegsTable.addMouseListener(this);
         accountRegsTable.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
-                
+
         TableColumnModel columnModel = accountRegsTable.getColumnModel();
-        
+
         columnModel.removeColumn(columnModel.getColumn(0));
-        
+
         columnModel.getColumn(0)
             .setCellRenderer(new LabelTableCellRenderer());
-                
+
         columnModel.getColumn(1)
             .setCellRenderer(new LabelTableCellRenderer());
-        
+
         columnModel.getColumn(0).setPreferredWidth(100);
         columnModel.getColumn(1).setPreferredWidth(377);
-        
+
         this.tableScrollPane.getViewport().add(accountRegsTable);
     }
 
@@ -129,17 +129,17 @@ public class AccountRegFirstPage extends JPanel
      * wizard in the table.
      */
     public void accountRegistrationAdded(AccountRegistrationEvent event) {
-                
-        final AccountRegistrationWizard wizard 
+
+        final AccountRegistrationWizard wizard
             = (AccountRegistrationWizard)event.getSource();
-        
+
         String pName = wizard.getProtocolName();
-        
+
         final JLabel registrationLabel = new JLabel();
         registrationLabel.setText(pName);
         registrationLabel.setIcon(
                 new ImageIcon(Constants.getProtocolIcon(pName)));
-        
+
         this.tableModel.addRow(new Object[]{wizard, registrationLabel,
                         wizard.getProtocolDescription()});
     }
@@ -150,9 +150,9 @@ public class AccountRegFirstPage extends JPanel
      * line from the table.
      */
     public void accountRegistrationRemoved(AccountRegistrationEvent event) {
-        AccountRegistrationWizard wizard 
+        AccountRegistrationWizard wizard
             = (AccountRegistrationWizard)event.getSource();
-        
+
         tableModel.removeRow(tableModel.rowIndexOf(wizard));
     }
 
@@ -167,7 +167,7 @@ public class AccountRegFirstPage extends JPanel
     }
 
     /**
-     * Implements the <code>WizardPage.getNextPageIdentifier</code> method. 
+     * Implements the <code>WizardPage.getNextPageIdentifier</code> method.
      * @return the identifier of the next wizard page, which in this case
      * is set dynamically when user selects a row in the table.
      */
@@ -181,7 +181,7 @@ public class AccountRegFirstPage extends JPanel
     }
 
     /**
-     * Implements the <code>WizardPage.getBackPageIdentifier</code> method. 
+     * Implements the <code>WizardPage.getBackPageIdentifier</code> method.
      * @return this identifier of the previous wizard page, which in this
      * case is null because this is the first page of the wizard.
      */
@@ -205,7 +205,7 @@ public class AccountRegFirstPage extends JPanel
 
     /**
      * Before the panel is displayed checks the selections and enables the
-     * next button if a checkbox is already selected or disables it if 
+     * next button if a checkbox is already selected or disables it if
      * nothing is selected.
      */
     public void pageShowing() {
@@ -213,13 +213,13 @@ public class AccountRegFirstPage extends JPanel
             this.wizardContainer.setNextFinishButtonEnabled(true);
         else
             this.wizardContainer.setNextFinishButtonEnabled(false);
-        
+
         if(wizardContainer.getCurrentWizard() != null)
             this.wizardContainer.unregisterWizardPages();
-        
+
         this.wizardContainer.removeWizzardIcon();
-    }    
-    
+    }
+
     /**
      * Handles the <tt>ListSelectionEvent</tt> triggered when user selects
      * a row in the contained in this page table. When a value is selected
@@ -241,54 +241,54 @@ public class AccountRegFirstPage extends JPanel
         AccountRegistrationWizard wizard
             = (AccountRegistrationWizard)tableModel
                 .getValueAt(accountRegsTable.getSelectedRow(), 0);
-        
+
         this.wizardContainer.setCurrentWizard(wizard);
-        
+
         Iterator i = wizard.getPages();
         boolean firstPage = true;
-        
+
         Object identifier = null;
-        
+
         while(i.hasNext()) {
             WizardPage page = (WizardPage)i.next();
-            
+
             identifier = page.getIdentifier();
-            
+
             if(firstPage) {
                 firstPage = false;
-                
+
                 nextPageIdentifier = (String)identifier;
             }
-            
+
             this.wizardContainer.registerWizardPage(identifier, page);
         }
-        
+
         this.wizardContainer.getSummaryPage()
             .setPreviousPageIdentifier(identifier);
-        
+
         try {
             this.wizardContainer.setWizzardIcon(
                 ImageIO.read(new ByteArrayInputStream(wizard.getIcon())));
         }
-        catch (IOException e1) {         
+        catch (IOException e1) {
             e1.printStackTrace();
         }
     }
 
     public void pageBack() {
     }
-    
+
     /**
      * Returns a list of all registered Account Registration Wizards.
      * @return a list of all registered Account Registration Wizards
      */
     public Iterator getWizardsList() {
         ArrayList wizardsList = new ArrayList();
-        
+
         for(int i = 0; i < tableModel.getRowCount(); i ++) {
             wizardsList.add(tableModel.getValueAt(i, 0));
         }
-        
+
         return wizardsList.iterator();
     }
 
