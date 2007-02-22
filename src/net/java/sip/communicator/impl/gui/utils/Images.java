06d7a97b9a9d5aaaf551b5cc906d14996659ac1b
Emil Ivov <emcho@jitsi.org>
2007-02-22 12:38:14 +0000

    Added multichat operation set definition and Gibberish protocol implementation. Related issues: Issue #241 , Issue #244, Issue #250
    Added logging

--- src/net/java/sip/communicator/impl/gui/utils/Images.java
+++ src/net/java/sip/communicator/impl/gui/utils/Images.java
@@ -8,18 +8,22 @@
 package net.java.sip.communicator.impl.gui.utils;
 
 import java.util.*;
+import net.java.sip.communicator.util.*;
 
 /**
  * Manages the access to the properties file containing all image paths.
- * 
+ *
  * @author Yana Stamcheva
  */
-public class Images {
+public class Images
+{
+    private static final Logger logger
+        = Logger.getLogger(Images.class);
 
-    private static final String BUNDLE_NAME 
+    private static final String BUNDLE_NAME
         = "net.java.sip.communicator.impl.gui.utils.images";
 
-    private static final ResourceBundle RESOURCE_BUNDLE 
+    private static final ResourceBundle RESOURCE_BUNDLE
         = ResourceBundle.getBundle(BUNDLE_NAME);
 
     private Images() {
@@ -30,12 +34,15 @@ public class Images {
      * @param key The key of the image.
      * @return An image path corresponding to the given image key.
      */
-    public static String getString(String key) {
-        try {
+    public static String getString(String key)
+    {
+        try
+        {
             return RESOURCE_BUNDLE.getString(key);
-
-        } catch (MissingResourceException e) {
-
+        }
+        catch (MissingResourceException exc)
+        {
+            logger.warn("Failed to find image for path " + key, exc);
             return '!' + key + '!';
         }
     }
