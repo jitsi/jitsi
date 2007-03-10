fda1b63a70724d218b54b33c2169dc66e9824b09
Emil Ivov <emcho@jitsi.org>
2007-03-10 14:33:12 +0000

    committing contribution from JM Heitz

---
+++ src/net/java/sip/communicator/service/protocol/DTMFTone.java
@@ -0,0 +1,142 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.service.protocol;
+
+/**
+ * Class for representing all the different DTMF tones.
+ *
+ * @author JM HEITZ
+ */
+public final class DTMFTone
+{
+    /**
+     * The "A" DTMF Tone
+     */
+    public static final DTMFTone DTMF_A=new DTMFTone("_A");
+    /**
+     * The "B" DTMF Tone
+     */
+    public static final DTMFTone DTMF_B=new DTMFTone("_B");
+    /**
+     * The "C" DTMF Tone
+     */
+    public static final DTMFTone DTMF_C=new DTMFTone("_C");
+    /**
+     * The "D" DTMF Tone
+     */
+    public static final DTMFTone DTMF_D=new DTMFTone("_D");
+    /**
+     * The "0" DTMF Tone
+     */
+    public static final DTMFTone DTMF_0=new DTMFTone("_0");
+    /**
+     * The "1" DTMF Tone
+     */
+    public static final DTMFTone DTMF_1=new DTMFTone("_1");
+    /**
+     * The "2" DTMF Tone
+     */
+    public static final DTMFTone DTMF_2=new DTMFTone("_2");
+    /**
+     * The "3" DTMF Tone
+     */
+    public static final DTMFTone DTMF_3=new DTMFTone("_3");
+    /**
+     * The "4" DTMF Tone
+     */
+    public static final DTMFTone DTMF_4=new DTMFTone("_4");
+    /**
+     * The "5" DTMF Tone
+     */
+    public static final DTMFTone DTMF_5=new DTMFTone("_5");
+    /**
+     * The "6" DTMF Tone
+     */
+    public static final DTMFTone DTMF_6=new DTMFTone("_6");
+    /**
+     * The "7" DTMF Tone
+     */
+    public static final DTMFTone DTMF_7=new DTMFTone("_7");
+    /**
+     * The "8" DTMF Tone
+     */
+    public static final DTMFTone DTMF_8=new DTMFTone("_8");
+    /**
+     * The "9" DTMF Tone
+     */
+    public static final DTMFTone DTMF_9=new DTMFTone("_9");
+    /**
+     * The "*" DTMF Tone
+     */
+    public static final DTMFTone DTMF_STAR=new DTMFTone("_STAR");
+    /**
+     * The "#" DTMF Tone
+     */
+    public static final DTMFTone DTMF_SHARP=new DTMFTone("_SHARP");
+
+
+    /**
+     * The value of the DTMF tone
+     */
+    private final String value;
+
+    /**
+     * Creates a DTMF instance with the specified tone value. The metod is
+     * private since one would only have to use predefined static instances.
+     *
+     * @param value one of te DTMF_XXX fields, indicating the value of the tone.
+     */
+    private DTMFTone(String value)
+    {
+        this.value = value;
+    }
+
+    /**
+     * Returns the string representation of this DTMF tone.
+     *
+     * @return the <tt>String</tt> representation of this DTMF tone.
+     */
+    public String getValue()
+    {
+        return this.value;
+    }
+
+    /**
+     * Indicates whether some other object is "equal to" this tone.
+     * <p>
+     * @param target the reference object with which to compare.
+     *
+     * @return  <tt>true</tt> if target represents the same tone as this
+     * object.
+     */
+    public boolean equals(Object target)
+    {
+        if(!(target instanceof DTMFTone))
+        {
+            return false;
+        }
+        DTMFTone targetDTMFTone = (DTMFTone)(target);
+
+        return targetDTMFTone.value.equals(this.value);
+    }
+
+    /**
+     * Returns a hash code value for the object. This method is
+     * supported for the benefit of hashtables such as those provided by
+     * <code>java.util.Hashtable</code>. The method would actually return the
+     * hashcode of the string representation of this DTMF tone.
+     * <p>
+     *
+     * @return  a hash code value for this object (same as calling
+     * getValue().hashCode()).
+     */
+    public int hashCode()
+    {
+        return getValue().hashCode();
+    }
+
+}
---
+++ src/net/java/sip/communicator/service/protocol/OperationSetDTMF.java
@@ -0,0 +1,37 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.service.protocol;
+
+/**
+ * An operation set that allows other modules to send DMF tones through this
+ * protocol provider.
+ *
+ * @author JM HEITZ
+ *
+ */
+public interface OperationSetDTMF
+    extends OperationSet
+{
+    /**
+     * Sends the <tt>DTMFTone</tt> <tt>tone</tt> to <tt>callParticipant</tt>.
+     *
+     * @param callParticipant the  call participant to send <tt>tone</tt> to.
+     * @param tone the DTMF tone to send to <tt>callParticipant</tt>.
+     *
+     * @throws OperationFailedException with code OPERATION_NOT_SUPPORTED if
+     * DTMF tones are not supported for <tt>callParticipant</tt>.
+     *
+     * @throws NullPointerException if one of the arguments is null.
+     *
+     * @throws IllegalArgumentException in case the call participant does not
+     * belong to the underlying implementation.
+     */
+    public boolean sendDTMF(CallParticipant callParticipant, DTMFTone tone)
+        throws OperationFailedException,
+               NullPointerException,
+               ClassCastException;
+}
