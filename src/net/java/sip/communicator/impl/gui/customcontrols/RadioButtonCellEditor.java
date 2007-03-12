ae9a34e3547cb049f80189c31bedaf2edf7d7d8f
Yana Stamcheva <yana@jitsi.org>
2007-03-12 17:52:08 +0000

    radio button editor and renderer

---
+++ src/net/java/sip/communicator/impl/gui/customcontrols/RadioButtonCellEditor.java
@@ -0,0 +1,47 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.impl.gui.customcontrols;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class RadioButtonCellEditor
+    extends DefaultCellEditor
+    implements ItemListener
+{
+    private JRadioButton button;
+
+    public RadioButtonCellEditor(JCheckBox checkBox)
+    {
+        super(checkBox);
+    }
+
+    public Component getTableCellEditorComponent(JTable table, Object value,
+        boolean isSelected, int row, int column)
+    {
+        if (value == null)
+            return null;
+        
+        button = (JRadioButton) value;
+        button.addItemListener(this);
+        
+        return (Component) value;
+    }
+
+    public Object getCellEditorValue()
+    {
+        button.removeItemListener(this);
+        return button;
+    }
+
+    public void itemStateChanged(ItemEvent e)
+    {
+        super.fireEditingStopped();
+    }
+  }
---
+++ src/net/java/sip/communicator/impl/gui/customcontrols/RadioButtonTableCellRenderer.java
@@ -0,0 +1,24 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.impl.gui.customcontrols;
+
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.table.*;
+
+public class RadioButtonTableCellRenderer
+    implements TableCellRenderer
+{
+    public Component getTableCellRendererComponent(JTable table, Object value,
+        boolean isSelected, boolean hasFocus, int row, int column)
+    {
+      if (value == null)
+        return null;
+      return (Component) value;
+    }
+}
