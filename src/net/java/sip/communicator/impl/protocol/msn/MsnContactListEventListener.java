c1a1868fd33ea87f154e81d540ac9907d92770d8
Emil Ivov <emcho@jitsi.org>
2007-04-23 18:11:03 +0000

    working on multi chat and a jabber implementation
    renaming msn's event listener

---
+++ src/net/java/sip/communicator/impl/protocol/msn/MsnContactListEventListener.java
@@ -0,0 +1,81 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.impl.protocol.msn;
+
+import net.sf.jml.*;
+
+/**
+ * Contactlist modification listener receives events
+ * for successful chngings
+ *
+ * @author Damian Minkov
+ */
+public interface MsnContactListEventListener
+    extends java.util.EventListener
+{
+    /**
+     * Message is successfully delivered
+     * @param transactionID int the transaction that send the message
+     */
+    public void messageDelivered(int transactionID);
+
+    /**
+     * Message is not delivered
+     * @param transactionID int the transaction that send the message
+     */
+    public void messageDeliveredFailed(int transactionID);
+
+    /**
+     * Indicates that a contact is successfully added
+     * @param contact MsnContact the contact
+     */
+    public void contactAdded(MsnContact contact);
+
+    /**
+     * Indicates that a contact is successfully added to the group
+     * @param contact MsnContact the contact
+     * @param group MsnGroup the group
+     */
+    public void contactAddedInGroup(MsnContact contact, MsnGroup group);
+
+    /**
+     * Indicates successful removing of a contact
+     * @param contact MsnContact the removed contact
+     */
+    public void contactRemoved(MsnContact contact);
+
+    /**
+     * Indicates successful removing of a contact from a group
+     * @param contact MsnContact the contact removed
+     * @param group MsnGroup the group
+     */
+    public void contactRemovedFromGroup(MsnContact contact, MsnGroup group);
+
+    /**
+     * Indicates that a group is successfully added
+     * @param group MsnGroup the added group
+     */
+    public void groupAdded(MsnGroup group);
+
+    /**
+     * Indicates that a group is successfully renamed
+     * @param group MsnGroup the renmaed group with the new name
+     */
+    public void groupRenamed(MsnGroup group);
+
+    /**
+     * Indicates successful removing of a group
+     * @param id String the id of the removed group
+     */
+    public void groupRemoved(String id);
+
+    /**
+     * Indicates that we are logged out
+     * beacuse account logged in from other location
+     */
+    public void loggingFromOtherLocation();
+}
