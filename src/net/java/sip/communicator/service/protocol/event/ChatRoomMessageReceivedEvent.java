b9c3f1d2e2f686f8214b6ea2a53f5d655b69de0c
Emil Ivov <emcho@jitsi.org>
2007-05-14 11:38:46 +0000

    Added event types in order to allow distinguishing action and system messages.

--- src/net/java/sip/communicator/service/protocol/event/ChatRoomMessageReceivedEvent.java
+++ src/net/java/sip/communicator/service/protocol/event/ChatRoomMessageReceivedEvent.java
@@ -34,6 +34,35 @@ public class ChatRoomMessageReceivedEvent
     private Message message = null;
 
     /**
+     * The type of message event that this instance represents.
+     */
+    private int eventType = -1;
+
+    /**
+     * An event type indicating that the message being received is a standard
+     * conversation message sent by another member of the chatroom to all
+     * current participants.
+     */
+    public static final int CONVERSATION_MESSAGE_RECEIVED = 1;
+
+    /**
+     * An event type indicating that the message being received is a special
+     * message that sent by either another member or the server itself,
+     * indicating that some kind of action (other than the delivery of a
+     * conversation message) has occurred. Action messages are widely used
+     * in IRC through the /action and /me commands
+     */
+    public static final int ACTION_MESSAGE_RECEIVED = 2;
+
+    /**
+     * An event type indicting that the message being received is a system
+     * message being sent by the server or a system administrator, possibly
+     * notifying us of something important such as ongoing maintenance
+     * activities or server downtime.
+     */
+    public static final int SYSTEM_MESSAGE_RECEIVED = 3;
+
+    /**
      * Creates a <tt>MessageReceivedEvent</tt> representing reception of the
      * <tt>source</tt> message received from the specified <tt>from</tt>
      * contact.
@@ -48,11 +77,33 @@ public class ChatRoomMessageReceivedEvent
                                         Date timestamp,
                                         Message message)
     {
+        this(source, from, timestamp, message, CONVERSATION_MESSAGE_RECEIVED);
+    }
+
+    /**
+     * Creates a <tt>MessageReceivedEvent</tt> representing reception of the
+     * <tt>source</tt> message received from the specified <tt>from</tt>
+     * contact.
+     *
+     * @param source the <tt>ChatRoom</tt> for which the message is received.
+     * @param from the <tt>ChatRoomMember</tt> that has sent this message.
+     * @param timestamp the exact date when the event ocurred.
+     * @param message the received <tt>Message</tt>.
+     * @param eventType the type of message event that this instance represents
+     * (one of the XXX_MESSAGE_RECEIVED static fields).
+     */
+    public ChatRoomMessageReceivedEvent(ChatRoom        source,
+                                        ChatRoomMember  from,
+                                        Date            timestamp,
+                                        Message         message,
+                                        int             eventType)
+    {
         super(source);
 
         this.from = from;
         this.timestamp = timestamp;
         this.message = message;
+        this.eventType = eventType;
     }
 
     /**
@@ -92,5 +143,17 @@ public class ChatRoomMessageReceivedEvent
     public ChatRoom getSourceChatRoom()
     {
         return (ChatRoom) getSource();
+    }
+
+    /**
+     * Returns the type of message event represented by this event instance.
+     * Message event types are generally one of the values of the
+     * XXX_MESSAGE_RECEIVED fields of this class.
+     * @return one of the values of the XXX_MESSAGE_RECEIVED fields of this
+     * class indicating the type of message received event received.
+     */
+    public int getEventType()
+    {
+        return eventType;
     }
 }
