8f3b952b66cc9b68724bad028550d1fa126ff2d1
Emil Ivov <emcho@jitsi.org>
2007-05-14 14:10:33 +0000

    Committing geolocation for Jabber from Guillaume Schreiner - Louis Pasteur University, France
    a geolocation oriented extension of a smack xmpp message.

---
+++ src/net/java/sip/communicator/impl/protocol/jabber/extensions/geolocation/GeolocationMessage.java
@@ -0,0 +1,59 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.impl.protocol.jabber.extensions.geolocation;
+
+import org.jivesoftware.smack.packet.Message;
+
+/**
+ * This class extends the smack Message class and allows creating a
+ * GeolocationMessage automatically setting the geolocation packet extension.
+ *
+ * @author Guillaume Schreiner
+ */
+public class GeolocationMessage
+    extends Message
+{
+    /**
+     * Creates a new, "normal" message.
+     *
+     * @param geoloc the geolocation packet extension to add to this message.
+     */
+    public GeolocationMessage(GeolocationPacketExtension geoloc)
+    {
+        super();
+        this.addExtension(geoloc);
+    }
+
+    /**
+     * Creates a new "normal" message to the specified recipient and adds the
+     * specified <tt>geoloc</tt> extension to it.
+     *
+     * @param to the recipient of the message.
+     * @param geoloc the geolocation packet extension to add to this message.
+     */
+    public GeolocationMessage(String to, GeolocationPacketExtension geoloc)
+    {
+        super(to);
+        this.addExtension(geoloc);
+    }
+
+    /**
+     * Creates a new message with the specified type and recipient and adds the
+     * specified <tt>geoloc</tt> extension to it.
+     *
+     * @param to the recipient of the message.
+     * @param geoloc the geolocation packet extension to add to this message.
+     * @param type the message type.
+     */
+    public GeolocationMessage(String                     to,
+                              Message.Type               type,
+                              GeolocationPacketExtension geoloc)
+    {
+        super(to, type);
+        addExtension(geoloc);
+    }
+}
---
+++ src/net/java/sip/communicator/impl/protocol/jabber/extensions/geolocation/GeolocationPresence.java
@@ -0,0 +1,59 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.impl.protocol.jabber.extensions.geolocation;
+
+import net.java.sip.communicator.service.protocol.*;
+import net.java.sip.communicator.service.protocol.jabberconstants.*;
+
+import org.jivesoftware.smack.packet.*;
+import net.java.sip.communicator.impl.protocol.jabber.*;
+
+/**
+ * This class represents a Jabber presence message includin a Geolocation
+ * Extension.
+ *
+ * @author Guillaume Schreiner
+ */
+public class GeolocationPresence
+{
+
+    /**
+     * the presence message to send via a XMPPConnection
+     */
+    private Presence prez = null;
+
+    /**
+     *
+     * @param persistentPresence OperationSetPresence
+     */
+    public GeolocationPresence(OperationSetPresence persistentPresence)
+    {
+        this.prez = new Presence(Presence.Type.available);
+
+        // set the custom status message
+        this.prez.setStatus(persistentPresence
+                            .getCurrentStatusMessage());
+
+        // set the presence mode (available, NA, free for chat)
+        this.prez.setMode(
+            OperationSetPersistentPresenceJabberImpl.presenceStatusToJabberMode(
+                (JabberStatusEnum) persistentPresence
+                .getPresenceStatus()));
+    }
+
+    public void setGeolocationExtention(GeolocationPacketExtension ext)
+    {
+        this.prez.addExtension(ext);
+    }
+
+    public Presence getGeolocPresence()
+    {
+        return this.prez;
+    }
+
+}
+
