57ca1d7271eb786db09099a590137a3483998294
Emil Ivov <emcho@jitsi.org>
2007-07-10 12:33:27 +0000

    sendDTMF() method should be void as we throw an exception in case of a failure

--- src/net/java/sip/communicator/service/protocol/OperationSetDTMF.java
+++ src/net/java/sip/communicator/service/protocol/OperationSetDTMF.java
@@ -30,7 +30,7 @@ public interface OperationSetDTMF
      * @throws IllegalArgumentException in case the call participant does not
      * belong to the underlying implementation.
      */
-    public boolean sendDTMF(CallParticipant callParticipant, DTMFTone tone)
+    public void sendDTMF(CallParticipant callParticipant, DTMFTone tone)
         throws OperationFailedException,
                NullPointerException,
                ClassCastException;
