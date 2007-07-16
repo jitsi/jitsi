519d8b4f32c77957bd7d8b8672fda903ae9471d9
Emil Ivov <emcho@jitsi.org>
2007-07-16 14:09:48 +0000

    removed a unnecessary call to registrations.sip()

--- src/net/java/sip/communicator/impl/protocol/sip/ProtocolProviderFactorySipImpl.java
+++ src/net/java/sip/communicator/impl/protocol/sip/ProtocolProviderFactorySipImpl.java
@@ -284,7 +284,6 @@ public class ProtocolProviderFactorySipImpl
             reg.unregister();
         }
 
-        Enumeration idEnum = registeredAccounts.keys();
         registeredAccounts.clear();
     }
 
