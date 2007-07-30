aefd2e6697f1c2dcd379319d870af8dece0954d6
Yana Stamcheva <yana@jitsi.org>
2007-07-30 10:11:18 +0000

    format code

--- src/net/java/sip/communicator/impl/gui/main/chat/menus/SettingsMenu.java
+++ src/net/java/sip/communicator/impl/gui/main/chat/menus/SettingsMenu.java
@@ -3,13 +3,12 @@ package net.java.sip.communicator.impl.gui.main.chat.menus;
 import java.awt.event.*;
 
 import javax.swing.*;
+import javax.swing.event.*;
 
-import net.java.sip.communicator.impl.gui.*;
 import net.java.sip.communicator.impl.gui.customcontrols.*;
 import net.java.sip.communicator.impl.gui.i18n.*;
 import net.java.sip.communicator.impl.gui.main.chat.*;
 import net.java.sip.communicator.impl.gui.utils.*;
-import net.java.sip.communicator.service.configuration.*;
 
 /**
  * The <tt>SettingsMenu</tt> is the menu in the chat window menu bar where
@@ -20,8 +19,10 @@ import net.java.sip.communicator.service.configuration.*;
  * 
  * @author Yana Stamcheva
  */
-public class SettingsMenu extends SIPCommMenu
-    implements ActionListener {
+public class SettingsMenu
+    extends SIPCommMenu
+    implements  ActionListener
+{
     
     private I18NString typingNotifString
         = Messages.getI18NString("enableTypingNotifications");
@@ -49,7 +50,8 @@ public class SettingsMenu extends SIPCommMenu
      * 
      * @param chatWindow The <tt>ChatWindow</tt>.
      */
-    public SettingsMenu(ChatWindow chatWindow){
+    public SettingsMenu(ChatWindow chatWindow)
+    {
         super(Messages.getI18NString("settings").getText());
         
         this.chatWindow = chatWindow;
@@ -94,7 +96,8 @@ public class SettingsMenu extends SIPCommMenu
     /**
      * Handles the <tt>ActionEvent</tt> when one of the menu items is selected.
      */
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e)
+    {
         JCheckBoxMenuItem item = (JCheckBoxMenuItem)e.getSource();
         
         if (item.getName().equals("typingNotifications"))
