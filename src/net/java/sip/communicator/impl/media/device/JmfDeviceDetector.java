13a83fc3ce48a34edc0d089352c4c8958626f808
Damian Minkov <damencho@jitsi.org>
2007-10-29 18:04:58 +0000

    Was not supposed to be committed!

--- src/net/java/sip/communicator/impl/media/device/JmfDeviceDetector.java
+++ src/net/java/sip/communicator/impl/media/device/JmfDeviceDetector.java
@@ -166,29 +166,19 @@ public class JmfDeviceDetector
         }
 
         //Linux
-//        try
-//        {
-//            V4LAuto v4lAuto = new V4LAuto();
-//            nDevices = v4lAuto.autoDetectDevices();
-//            logger.info("Detected "
-//                        + nDevices
-//                        +" V4L video capture device.");
-//        }
-//        catch (Throwable exc)
-//        {
-//            logger.debug("No V4l video detected: " + exc.getMessage());
-//        }
-        
-//        try
-//        {
-//            new net.sf.fmj.media.cdp.civil.CaptureDevicePlugger().
-//                    addCaptureDevices();
-//            CaptureDeviceManager.commit();
-//        }
-//        catch (Throwable exc)
-//        {
-//            logger.debug("No civil video detected: ", exc);
-//        }
+        try
+        {
+            V4LAuto v4lAuto = new V4LAuto();
+            nDevices = v4lAuto.autoDetectDevices();
+            logger.info("Detected "
+                        + nDevices
+                        +" V4L video capture device.");
+        }
+        catch (Throwable exc)
+        {
+            logger.debug("No V4l video detected: " + exc.getMessage());
+        }
+
     }
 
     private void detectDirectAudio()
