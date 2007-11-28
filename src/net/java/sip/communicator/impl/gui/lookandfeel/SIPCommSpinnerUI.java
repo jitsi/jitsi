d25efed3510217041ffa9f7f679dd64f56b85afc
Yana Stamcheva <yana@jitsi.org>
2007-11-28 11:13:01 +0000

    basic sip communicator spinner ui

---
+++ src/net/java/sip/communicator/impl/gui/lookandfeel/SIPCommSpinnerUI.java
@@ -0,0 +1,42 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.impl.gui.lookandfeel;
+
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.plaf.*;
+import javax.swing.plaf.basic.*;
+
+public class SIPCommSpinnerUI
+    extends BasicSpinnerUI
+{
+    public static ComponentUI createUI(JComponent c) {
+        return new SIPCommSpinnerUI();
+    }
+
+    protected Component createNextButton()
+    {
+        Component c = createArrowButton(SwingConstants.NORTH);
+            installNextButtonListeners(c);
+            return c;
+    }
+
+    protected Component createPreviousButton()
+    {
+        Component c = createArrowButton(SwingConstants.SOUTH);
+            installPreviousButtonListeners(c);
+            return c;
+    }
+
+    private Component createArrowButton(int direction)
+    {
+        JButton b = new BasicArrowButton(direction);
+
+        return b;
+    }
+}
