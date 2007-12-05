c9f7f7170c031b095e009aa9676bd7e4bf3c4730
Yana Stamcheva <yana@jitsi.org>
2007-12-05 14:40:11 +0000

    some more diversification of the branding resources

---
+++ src/net/java/sip/communicator/plugin/branding/BrandingResources.java
@@ -0,0 +1,72 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.plugin.branding;
+
+import java.text.*;
+import java.util.*;
+
+/**
+ * The Messages class manages the access to the internationalization properties
+ * files.
+ *
+ * @author Yana Stamcheva
+ */
+public class BrandingResources
+{
+    private static final String RESOUCRE_LOCATION
+        = "net.java.sip.communicator.plugin.branding.brandingResources";
+
+    private static final ResourceBundle resourceBundle 
+        = ResourceBundle.getBundle(RESOUCRE_LOCATION);
+
+    /**
+     * Returns an internationalized string corresponding to the given key.
+     *
+     * @param key The key of the string.
+     * @return An internationalized string corresponding to the given key.
+     */
+    public static String getString(String key)
+    {
+        try
+        {
+            return resourceBundle.getString(key);
+        }
+        catch (MissingResourceException e)
+        {
+            return '!' + key + '!';
+        }
+    }
+    
+    /**
+     * Returns an internationalized string corresponding to the given key,
+     * by replacing all occurences of '?' with the given string param.
+     * @param key The key of the string.
+     * @param params the params, that should replace {1}, {2}, etc. in the
+     * string given by the key parameter 
+     * @return An internationalized string corresponding to the given key,
+     * by replacing all occurences of {#number} with the given string param.
+     */
+    public static String getString(String key, String[] params)
+    {
+        String resourceString;
+
+        try
+        {
+            resourceString = resourceBundle.getString(key);
+
+            resourceString = MessageFormat.format(
+                resourceString, (Object[]) params);
+
+        }
+        catch (MissingResourceException e)
+        {
+            resourceString = '!' + key + '!';
+        }
+
+        return resourceString;
+    }
+}
