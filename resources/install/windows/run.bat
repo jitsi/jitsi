dab4c1f2ab2dd2f15e90ef204c8764d3843db47f
Thomas Hofer <t.hofer@liwest.at>
2008-04-21 11:07:07 +0000

    removed "servicebin" command

--- resources/install/windows/run.bat
+++ resources/install/windows/run.bat
@@ -1,4 +1,3 @@
 mkdir "%UserProfile%/.sip-communicator/log"
 set PATH=%PATH%;native
 start javaw -classpath "lib/jdic-all.jar;lib/jdic_stub.jar;lib/felix.jar;lib/sc-launcher.jar;sc-bundles/util.jar" -Dfelix.config.properties=file:./lib/felix.client.run.properties -Djava.util.logging.config.file=lib/logging.properties net.java.sip.communicator.launcher.SIPCommunicator
-servicebin
