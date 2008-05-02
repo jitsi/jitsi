8a798484c90526f90475d310cbfcfecc9310a3e1
Yana Stamcheva <yana@jitsi.org>
2008-05-02 16:35:50 +0000

    id replaced by identifier. It was causing wrong string on the Authentication window.

--- src/net/java/sip/communicator/impl/gui/main/login/AuthenticationWindow.java
+++ src/net/java/sip/communicator/impl/gui/main/login/AuthenticationWindow.java
@@ -32,7 +32,7 @@ public class AuthenticationWindow
     private JTextArea realmTextArea = new JTextArea();
 
     private JLabel uinLabel = new JLabel(
-        Messages.getI18NString("id").getText());
+        Messages.getI18NString("identifier").getText());
 
     private JLabel passwdLabel = new JLabel(
         Messages.getI18NString("passwd").getText());
