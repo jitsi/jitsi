5f858afdb36a31881326c06bc04d121e1b519526
Damian Minkov <damencho@jitsi.org>
2008-05-20 12:54:17 +0000

    Add property to media service to disable video support(skip device detection)

--- src/net/java/sip/communicator/impl/media/device/JmfDeviceDetector.java
+++ src/net/java/sip/communicator/impl/media/device/JmfDeviceDetector.java
@@ -154,6 +154,22 @@ public class JmfDeviceDetector
             logger.debug("No FMJ javasound detected: " + exc.getMessage());
         }
 
+        String disableVideoObj = (String) MediaActivator.
+            getConfigurationService().getProperty(
+                MediaServiceImpl.DISABLE_VIDEO_SUPPORT_PROPERTY_NAME);
+
+        // video is enabled by default
+        boolean disableVideo = false;
+        if (disableVideoObj != null) 
+        {
+            disableVideo = Boolean.valueOf(disableVideoObj)
+                .booleanValue();
+        }
+        
+        // if video is disabled skip device detection
+        if(disableVideo)
+            return;
+        
         // Try to configgure capture devices for any operating system.
         //those that do not apply will silently fail.
         logger.info("Looking for video capture devices");
