4db2d5cd142801a67271c2b664674abf5a203864
Thomas Hofer <t.hofer@liwest.at>
2008-07-18 06:51:38 +0000

    improved performance on initalization 1. use toolkit for one screen systems 2. get the bounds from graphics device all at the same time and not one after another on multi monitor systems.
    this improves the performance on multimonitorsystems on windows by a factor of ~3

--- src/net/java/sip/communicator/impl/gui/customcontrols/ScreenInformation.java
+++ src/net/java/sip/communicator/impl/gui/customcontrols/ScreenInformation.java
@@ -14,31 +14,77 @@ import java.awt.*;
  * @author Thomas Hofer
  * 
  */
-class ScreenInformation
+public class ScreenInformation
 {
     private static Rectangle screenBounds = null;
 
-    static synchronized Rectangle getScreenBounds()
+    public static synchronized void init()
+    {
+        if (screenBounds == null)
+        {
+            final GraphicsEnvironment ge = GraphicsEnvironment
+                    .getLocalGraphicsEnvironment();
+            final GraphicsDevice[] gs = ge.getScreenDevices();
+
+            screenBounds = new Rectangle();
+
+            if (gs.length > 1)
+            {
+
+                // create a thread for each display, as the query is very slow
+                Thread thread[] = new Thread[gs.length];
+                for (int j = 0; j < gs.length; j++)
+                {
+                    final int j1 = j;
+                    thread[j] = new Thread(new Runnable()
+                    {
+                        public void run()
+                        {
+                            Rectangle screenDeviceBounds = new Rectangle();
+                            GraphicsDevice gd = gs[j1];
+                            GraphicsConfiguration[] gc = gd.getConfigurations();
+                            for (int i = 0; i < gc.length; i++)
+                            {
+                                screenDeviceBounds = screenDeviceBounds
+                                        .union(gc[i].getBounds());
+                            }
+                            screenBounds.setBounds(screenBounds
+                                    .union(screenDeviceBounds));
+
+                        }
+                    });
+                    thread[j].start();
+                }
+                for (int j = 0; j < gs.length; j++)
+                {
+                    // wait for all threads here
+                    try
+                    {
+                        thread[j].join();
+                    } catch (InterruptedException e)
+                    {
+                    }
+                }
+            } else
+            {
+                // only one display, get the screen size directy. this method
+                // is much faster, but can only handle the primary display
+                Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+                screenBounds.setBounds(0, 0, dim.width, dim.height);
+            }
+
+        }
+    }
+
+    public static synchronized Rectangle getScreenBounds()
     {
         // the initialization needs a moment
         // prevent a concurrent initalization
-
         if (screenBounds == null)
         {
-            screenBounds = new Rectangle();
-            GraphicsEnvironment ge = GraphicsEnvironment
-                    .getLocalGraphicsEnvironment();
-            GraphicsDevice[] gs = ge.getScreenDevices();
-            for (int j = 0; j < gs.length; j++)
-            {
-                GraphicsDevice gd = gs[j];
-                GraphicsConfiguration[] gc = gd.getConfigurations();
-                for (int i = 0; i < gc.length; i++)
-                {
-                    screenBounds = screenBounds.union(gc[i].getBounds());
-                }
-            }
+            init();
         }
+
         return screenBounds;
     }
 }
