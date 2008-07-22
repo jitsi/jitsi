2b9abe5cb9f4d71f23e14ec48a24abf0bf432960
Thomas Hofer <t.hofer@liwest.at>
2008-07-22 08:31:08 +0000

    Fix for "remotely on hold" issue after diskussion with libomir marinov - who owns the original code

--- src/net/java/sip/communicator/impl/media/CallSessionImpl.java
+++ src/net/java/sip/communicator/impl/media/CallSessionImpl.java
@@ -1481,13 +1481,13 @@ public class CallSessionImpl
     {
         String attribute;
 
-        if (ON_HOLD_LOCALLY == (onHold | ON_HOLD_LOCALLY))
+        if (ON_HOLD_LOCALLY == (onHold & ON_HOLD_LOCALLY))
             attribute =
-                (ON_HOLD_REMOTELY == (onHold | ON_HOLD_REMOTELY)) ? "inactive"
+                (ON_HOLD_REMOTELY == (onHold & ON_HOLD_REMOTELY)) ? "inactive"
                         : "sendonly";
         else
             attribute =
-                (ON_HOLD_REMOTELY == (onHold | ON_HOLD_REMOTELY)) ? "recvonly"
+                (ON_HOLD_REMOTELY == (onHold & ON_HOLD_REMOTELY)) ? "recvonly"
                         : null;
 
         if (attribute != null)
