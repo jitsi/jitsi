03847cd2912f0681ea2b1cfd7560b3aaaee3d627
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2008-10-17 08:23:23 +0000

    Deletes MediaDispatcher because it seems to be renamed to MediaEventDispatcher and is no longer is use.

--- src/net/java/sip/communicator/impl/media/MediaEventDispatcher.java
+++ src/net/java/sip/communicator/impl/media/MediaEventDispatcher.java
@@ -4,80 +4,84 @@
  * Distributable under LGPL license.
  * See terms of license at gnu.org.
  */
-
-
-
 package net.java.sip.communicator.impl.media;
-
-//~--- non-JDK imports --------------------------------------------------------
-
-import net.java.sip.communicator.service.media.event.*;
-
-//~--- JDK imports ------------------------------------------------------------
 
 import java.util.*;
 
+import net.java.sip.communicator.service.media.event.*;
+
 /**
  * This is a utility class that can be used by objects that support constrained
- * properties.  You can use an instance of this class as a member field
- * of your bean and delegate various work to it.
- *
+ * properties. You can use an instance of this class as a member field of your
+ * bean and delegate various work to it.
+ * 
  * @author Martin Andre
  * @author Emil Ivov
  */
-public class MediaEventDispatcher {
+public class MediaEventDispatcher
+{
 
     /**
      * All media listeners registered so far.
      */
-    private Vector mediaListeners;
+    private List<MediaListener> mediaListeners;
 
-    public MediaEventDispatcher() {}
+    public MediaEventDispatcher()
+    {
+    }
 
     /**
      * Add a mediaListener to the listener list.
-     *
-     * @param listener  The MediaListener to be added
+     * 
+     * @param listener The MediaListener to be added
      */
-    protected synchronized void addMediaListener(MediaListener listener) {
-        if (mediaListeners == null) {
-            mediaListeners = new Vector();
+    protected synchronized void addMediaListener(MediaListener listener)
+    {
+        if (mediaListeners == null)
+        {
+            mediaListeners = new Vector<MediaListener>();
         }
 
-        mediaListeners.addElement(listener);
+        mediaListeners.add(listener);
     }
 
     /**
      * Remove a MediaListener from the listener list.
-     *
+     * 
      * @param listener The MediaListener to be removed
      */
-    protected synchronized void removeMediaListener(MediaListener listener) {
-        if (mediaListeners == null) {
-            return;
+    protected synchronized void removeMediaListener(MediaListener listener)
+    {
+        if (mediaListeners != null)
+        {
+            mediaListeners.remove(listener);
         }
-
-        mediaListeners.removeElement(listener);
     }
 
     /**
      * Alert all media listeners that we're receiving a media stream.
+     * 
      * @param mediaEvent the source of the event
      */
-    protected void fireReceivedMediaStream(MediaEvent mediaEvent) {
-        Vector targets = null;
+    protected void fireReceivedMediaStream(MediaEvent mediaEvent)
+    {
+        MediaListener[] targets = null;
 
-        synchronized (this) {
-            if (mediaListeners != null) {
-                targets = (Vector) mediaListeners.clone();
+        synchronized (this)
+        {
+            if (mediaListeners != null)
+            {
+                targets =
+                    mediaListeners.toArray(new MediaListener[mediaListeners
+                        .size()]);
             }
         }
 
-        if (targets != null) {
-            for (int i = 0; i < targets.size(); i++) {
-                MediaListener target = (MediaListener) targets.elementAt(i);
-
-                target.receivedMediaStream(mediaEvent);
+        if (targets != null)
+        {
+            for (int i = 0; i < targets.length; i++)
+            {
+                targets[i].receivedMediaStream(mediaEvent);
             }
         }
     }
@@ -85,24 +89,26 @@ public class MediaEventDispatcher {
     /**
      * Alert all media listeners that status has changed.
      */
-    protected void fireMediaServiceStatusChanged() {
-        Vector targets = null;
+    protected void fireMediaServiceStatusChanged()
+    {
+        MediaListener[] targets = null;
 
-        synchronized (this) {
-            if (mediaListeners != null) {
-                targets = (Vector) mediaListeners.clone();
+        synchronized (this)
+        {
+            if (mediaListeners != null)
+            {
+                targets =
+                    mediaListeners.toArray(new MediaListener[mediaListeners
+                        .size()]);
             }
         }
 
-        if (targets != null) {
-            for (int i = 0; i < targets.size(); i++) {
-                MediaListener target = (MediaListener) targets.elementAt(i);
-
-                target.mediaServiceStatusChanged();
+        if (targets != null)
+        {
+            for (int i = 0; i < targets.length; i++)
+            {
+                targets[i].mediaServiceStatusChanged();
             }
         }
     }
 }
-
-
-//~ Formatted by Jindent --- http://www.jindent.com
