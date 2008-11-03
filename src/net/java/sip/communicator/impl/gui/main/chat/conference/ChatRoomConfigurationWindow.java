5ebc0fb659ff6bde0c0d2180658f4f6b008b7cb8
Yana Stamcheva <yana@jitsi.org>
2008-11-03 23:29:09 +0000

    Class was accidently moved to the wrong package.

--- src/net/java/sip/communicator/impl/gui/main/chat/conference/ChatRoomConfigurationWindow.java
+++ src/net/java/sip/communicator/impl/gui/main/chat/conference/ChatRoomConfigurationWindow.java
@@ -4,7 +4,7 @@
  * Distributable under LGPL license.
  * See terms of license at gnu.org.
  */
-package net.java.sip.communicator.impl.gui.main.chatroomslist;
+package net.java.sip.communicator.impl.gui.main.chat.conference;
 
 import java.awt.*;
 import java.awt.event.*;
