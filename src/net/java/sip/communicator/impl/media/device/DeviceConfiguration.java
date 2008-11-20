43104512daedfa7bbf4f601cbd4f28177d166bc2
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2008-11-20 09:22:49 +0000

    Drafts the publicly-visible modifications needed to DeviceConfiguration in order to make it suitable for backing a Video ConfigurationForm and allowing it to configure the default audio and video configuration devices.

--- src/net/java/sip/communicator/impl/media/device/DeviceConfiguration.java
+++ src/net/java/sip/communicator/impl/media/device/DeviceConfiguration.java
@@ -19,9 +19,22 @@ import net.java.sip.communicator.util.*;
  * 
  * @author Martin Andre
  * @author Emil Ivov
+ * @authod Lubomir Marinov
  */
 public class DeviceConfiguration
 {
+
+    /**
+     * The name of the <code>DeviceConfiguration</code> property which
+     * represents the capture device used by default by the
+     * <code>DeviceConfiguration</code> for video when it is not explicitly
+     * configured to use a specific video capture device.
+     */
+    public static final String DEFAULT_VIDEO_CAPTURE_DEVICE = "DEFAULT_VIDEO_CAPTURE_DEVICE";
+
+    private static final CaptureDeviceInfo[] NO_CAPTURE_DEVICES =
+        new CaptureDeviceInfo[0];
+
     private Logger logger = Logger.getLogger(DeviceConfiguration.class);
 
     /**
@@ -33,6 +46,13 @@ public class DeviceConfiguration
      * The device that we'll be using for video capture.
      */
     private CaptureDeviceInfo videoCaptureDevice = null;
+
+    /**
+     * The capture device used by default by this
+     * <code>DeviceConfiguration</code> for video when it is not explicitly
+     * configured to use a specific video capture device.
+     */
+    private CaptureDeviceInfo defaultVideoCaptureDevice;
 
     /**
      * Default constructor.
@@ -128,6 +148,54 @@ public class DeviceConfiguration
     }
 
     /**
+     * Gets the capture device which is to be used by default by this
+     * <code>DeviceConfiguration</code> for audio when it is not explicitly
+     * configured to use a specific audio capture device.
+     * 
+     * @return a <code>CaptureDeviceInfo</code> describing the default audio
+     *         capture device
+     */
+    public CaptureDeviceInfo getDefaultAudioCaptureDevice()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * Gets the list of audio capture devices which are available through this
+     * <code>DeviceConfiguration</code>, amongst which are
+     * {@link #getAudioCaptureDevice()} and
+     * {@link #getDefaultAudioCaptureDevice()} and represent acceptable values
+     * for {@link #setDefaultAudioCaptureDevice()}
+     * 
+     * @return an array of <code>CaptureDeviceInfo</code> describing the audio
+     *         capture devices available through this
+     *         <code>DeviceConfiguration</code>
+     */
+    public CaptureDeviceInfo[] getAvailableAudioCaptureDevices()
+    {
+        // TODO Auto-generated method stub
+        return NO_CAPTURE_DEVICES;
+    }
+
+    /**
+     * Gets the list of video capture devices which are available through this
+     * <code>DeviceConfiguration</code>, amongst which are
+     * {@link #getVideoCaptureDevice()} and
+     * {@link #getDefaultVideoCaptureDevice()} and represent acceptable values
+     * for {@link #setDefaultVideoCaptureDevice()}
+     * 
+     * @return an array of <code>CaptureDeviceInfo</code> describing the video
+     *         capture devices available through this
+     *         <code>DeviceConfiguration</code>
+     */
+    public CaptureDeviceInfo[] getAvailableVideoCaptureDevices()
+    {
+        // TODO Auto-generated method stub
+        return NO_CAPTURE_DEVICES;
+    }
+
+    /**
      * Returns a device that we could use for video capture.
      * 
      * @return the CaptureDeviceInfo of a device that we could use for video
@@ -136,6 +204,46 @@ public class DeviceConfiguration
     public CaptureDeviceInfo getVideoCaptureDevice()
     {
         return videoCaptureDevice;
+    }
+
+    /**
+     * Gets the capture device which is to be used by default by this
+     * <code>DeviceConfiguration</code> for video when it is not explicitly
+     * configured to use a specific video capture device.
+     * 
+     * @return a <code>CaptureDeviceInfo</code> describing the default video
+     *         capture device
+     */
+    public CaptureDeviceInfo getDefaultVideoCaptureDevice()
+    {
+        return defaultVideoCaptureDevice;
+    }
+
+    /**
+     * Sets the capture device which is to be used by default by this
+     * <code>DeviceConfiguration</code> for video when it is not explicitly
+     * configured to use a specific audio capture device.
+     * 
+     * @param device a <code>CaptureDeviceInfo</code> describing the video
+     *            capture device to be made default for this
+     *            <code>DeviceConfiguration</code>
+     */
+    public void setDefaultVideoCaptureDevice(CaptureDeviceInfo device)
+    {
+        if (defaultVideoCaptureDevice != device)
+        {
+            CaptureDeviceInfo oldDevice = defaultVideoCaptureDevice;
+
+            defaultVideoCaptureDevice = device;
+
+            firePropertyChange(DEFAULT_VIDEO_CAPTURE_DEVICE, oldDevice, device);
+        }
+    }
+
+    protected void firePropertyChange(String property, Object oldValue,
+        Object newValue)
+    {
+        // TODO Auto-generated method stub
     }
 
     /**
