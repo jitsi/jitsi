f337314dd81359ba6ee98f14847b8fbcb8f6a0f2
Werner Dittmann <wernerd@java.net>
2009-01-21 18:30:41 +0000

    SOme restructuring when sending events, add information to display to user in case something goes wrong or needs to be confirmed.

--- src/net/java/sip/communicator/impl/media/transform/zrtp/SCCallback.java
+++ src/net/java/sip/communicator/impl/media/transform/zrtp/SCCallback.java
@@ -9,6 +9,7 @@ package net.java.sip.communicator.impl.media.transform.zrtp;
 import java.util.*;
 
 import net.java.sip.communicator.impl.media.CallSessionImpl;
+import net.java.sip.communicator.impl.media.MediaActivator;
 import net.java.sip.communicator.service.media.*;
 import net.java.sip.communicator.service.protocol.*;
 import net.java.sip.communicator.util.Logger;
@@ -18,26 +19,38 @@ import net.java.sip.communicator.service.protocol.event.*;
 import gnu.java.zrtp.*;
 
 /**
- * The user callback class used by the ZRTP4J.
- * This class provides means to communicate events to the user through
- * GUI elements, and also allows the user to control the ZRTP activation
- * and deactivation.
+ * The user callback class for ZRTP4J.
+ * 
+ * This class constructs and sends events to the ZRTP GUI implementation. The
+ * <code>showMessage()<code> function implements a specific check to start 
+ * associated ZRTP multi-stream sessions.
+ * 
+ * Coordinate this callback class with the associated GUI implementation class
+ * net.java.sip.communicator.impl.gui.main.call.ZrtpPanel
+ *
+ * @see net.java.sip.communicator.impl.gui.main.call.ZrtpPanel
  *
  * @author Emanuel Onica
+ * @author Werner Dittmann
  *
  */
-public class SCCallback
-    extends ZrtpUserCallback
-{
-    private static final Logger logger
-    = Logger.getLogger(SCCallback.class);
+public class SCCallback extends ZrtpUserCallback {
+    private static final Logger logger = Logger.getLogger(SCCallback.class);
+
+    public static final String WARNING_NO_RS_MATCH = MediaActivator
+            .getResources().getI18NString(
+                    "impl.media.security.WARNING_NO_RS_MATCH");
+
+    public static final String WARNING_NO_EXPECTED_RS_MATCH = MediaActivator
+            .getResources().getI18NString(
+                    "impl.media.security.WARNING_NO_EXPECTED_RS_MATCH");
 
     private CallSession callSession = null;
 
     private SecurityGUIListener guiListener = null;
-    
+
     private CallParticipant participant;
-    
+
     /*
      * Is this a ZRTP DH (Master) session?
      */
@@ -48,16 +61,32 @@ public class SCCallback
      * supported.
      */
     private String sessionType = null;
+
+    /*
+     * Remember the cipher name
+     */
+    private String cipherName = null;
+
+    /*
+     * Remember the SAS
+     */
+    private String sasData = null;
+
+    /*
+     * Remember SAS verification state
+     */
+    private boolean sasVerification = false;
+
     /**
      * The class constructor.
      */
-    public SCCallback(CallSession callSession)
-    {
+    public SCCallback(CallSession callSession) {
 
         this.callSession = callSession;
-        
+
         guiListener = callSession.getCall().getSecurityGUIListener("zrtp");
-        Iterator<CallParticipant> participants = callSession.getCall().getCallParticipants();
+        Iterator<CallParticipant> participants = callSession.getCall()
+                .getCallParticipants();
         if (participants.hasNext())
             participant = participants.next();
 
@@ -65,11 +94,10 @@ public class SCCallback
 
     public void init() {
         SecurityGUIEvent evt = new SecurityGUIEvent(participant,
-                SecurityGUIEvent.NONE,
-                SecurityGUIEvent.SECURITY_ENABLED);
-        
+                SecurityGUIEvent.NONE, SecurityGUIEvent.SECURITY_ENABLED);
+
         if (logger.isInfoEnabled())
-            logger.info(sessionType +": initialize SCCallback");
+            logger.info(sessionType + ": initialize SCCallback");
 
         fireStateChangedEvent(evt);
     }
@@ -83,9 +111,9 @@ public class SCCallback
                 guiListener.securityStatusChanged(evt);
             }
         }
-        
+
     }
-    
+
     /**
      * Set the type of this session.
      * 
@@ -94,7 +122,7 @@ public class SCCallback
     public void setType(String type) {
         sessionType = type;
     }
-    
+
     /**
      * Set the DH session flag.
      * 
@@ -103,79 +131,147 @@ public class SCCallback
     public void setDHSession(boolean yesNo) {
         dhSession = yesNo;
     }
-    
+
     /*
      * The following methods implement the ZrtpUserCallback interface
      */
-    
+
     /*
      * (non-Javadoc)
+     * 
      * @see gnu.java.zrtp.ZrtpUserCallback#secureOn(java.lang.String)
      */
-    public void secureOn(String cipher)
-    {
+    public void secureOn(String cipher) {
         if (logger.isInfoEnabled())
             logger.info(sessionType + ": cipher enabled: " + cipher);
-        
-        HashMap<String, Object> state = new HashMap<String, Object>(3);
-        
-        state.put(SecurityGUIEventZrtp.SESSION_TYPE, sessionType);
-        state.put(SecurityGUIEventZrtp.SECURITY_CHANGE, Boolean.TRUE);
-        state.put(SecurityGUIEventZrtp.CIPHER, cipher);
 
-        SecurityGUIEventZrtp evt = new SecurityGUIEventZrtp(participant, state);
-        fireStateChangedEvent(evt);     
+        cipherName = cipher;
     }
 
     /*
      * (non-Javadoc)
+     * 
      * @see gnu.java.zrtp.ZrtpUserCallback#showSAS(java.lang.String, boolean)
      */
-    public void showSAS(String sas, boolean verified)
-    {
+    public void showSAS(String sas, boolean verified) {
         if (logger.isInfoEnabled())
             logger.info(sessionType + ": SAS is: " + sas);
-        
-        HashMap<String, Object> state = new HashMap<String, Object>(3);
-        
-        state.put(SecurityGUIEventZrtp.SESSION_TYPE, SecurityGUIEventZrtp.AUDIO);
-        state.put(SecurityGUIEventZrtp.SAS, sas);
-        if (verified) {
-            state.put(SecurityGUIEventZrtp.SAS_VERIFY, Boolean.TRUE);
-        }
-        else {
-            state.put(SecurityGUIEventZrtp.SAS_VERIFY, Boolean.FALSE);
-        }
 
-        SecurityGUIEventZrtp evt = new SecurityGUIEventZrtp(participant, state);
-        fireStateChangedEvent(evt);     
+        sasData = sas;
+        sasVerification = verified;
     }
 
     /*
      * (non-Javadoc)
+     * 
      * @see gnu.java.zrtp.ZrtpUserCallback#showMessage(
-     *                              gnu.java.zrtp.ZrtpCodes.MessageSeverity,
-     *                              java.util.EnumSet)
+     * gnu.java.zrtp.ZrtpCodes.MessageSeverity, java.util.EnumSet)
      */
     public void showMessage(ZrtpCodes.MessageSeverity sev, EnumSet<?> subCode) {
 
-        ZrtpCodes.InfoCodes inf;
         int multiStreams = 0;
+        HashMap<String, Object> state = new HashMap<String, Object>(5);
+        boolean fireEvent = false;
 
         Iterator<?> ii = subCode.iterator();
         Object msgCode = ii.next();
 
         if (sev == ZrtpCodes.MessageSeverity.Info) {
             if (msgCode instanceof ZrtpCodes.InfoCodes) {
-                inf = (ZrtpCodes.InfoCodes) msgCode;
+                ZrtpCodes.InfoCodes inf = (ZrtpCodes.InfoCodes) msgCode;
 
                 // If the ZRTP Master session (DH mode) signals "security on"
                 // then start multi-stream sessions.
-                if (dhSession && inf == ZrtpCodes.InfoCodes.InfoSecureStateOn) {
-                    multiStreams = ((CallSessionImpl) callSession)
-                            .startZrtpMultiStreams();
+                // Signal SAS to GUI only if this is a DH mode session.
+                // Multi-stream session don't have own SAS data
+                if (inf == ZrtpCodes.InfoCodes.InfoSecureStateOn) {
+                    if (dhSession) {
+                        multiStreams = ((CallSessionImpl) callSession)
+                                .startZrtpMultiStreams();
+                        state.put(SecurityGUIEventZrtp.SAS, sasData);
+                        if (sasVerification) {
+                            state.put(SecurityGUIEventZrtp.SAS_VERIFY,
+                                    Boolean.TRUE);
+                        } else {
+                            state.put(SecurityGUIEventZrtp.SAS_VERIFY,
+                                    Boolean.FALSE);
+                        }
+                    }
+                    state.put(SecurityGUIEventZrtp.SESSION_TYPE, sessionType);
+                    state.put(SecurityGUIEventZrtp.SECURITY_CHANGE,
+                            Boolean.TRUE);
+                    state.put(SecurityGUIEventZrtp.CIPHER, cipherName);
+                    fireEvent = true;
+                }
+
+                if (inf == ZrtpCodes.InfoCodes.InfoHelloReceived) {
+                    state.put(SecurityGUIEventZrtp.SESSION_TYPE,
+                            SecurityGUIEventZrtp.MSG_INFO);
+                    fireEvent = true;
                 }
             }
+        }
+
+        /*
+         * Warning codes usually do not affect encryption or security. Onl
+         * in few cases inform the user and ask to verify SAS
+         */
+        if (sev == ZrtpCodes.MessageSeverity.Warning) {
+            if (msgCode instanceof ZrtpCodes.WarningCodes) {
+                ZrtpCodes.WarningCodes warn = (ZrtpCodes.WarningCodes) msgCode;
+
+                if (warn == ZrtpCodes.WarningCodes.WarningNoRSMatch) {
+                    state.put(SecurityGUIEventZrtp.SESSION_TYPE,
+                            SecurityGUIEventZrtp.MSG_WARN);
+                    state.put(SecurityGUIEventZrtp.MSG_TEXT,
+                            WARNING_NO_RS_MATCH);
+                    fireEvent = true;
+                }
+                if (warn == ZrtpCodes.WarningCodes.WarningNoExpectedRSMatch) {
+                    state.put(SecurityGUIEventZrtp.SESSION_TYPE,
+                            SecurityGUIEventZrtp.MSG_WARN);
+                    state.put(SecurityGUIEventZrtp.MSG_TEXT,
+                            WARNING_NO_EXPECTED_RS_MATCH);
+                    fireEvent = true;
+                }
+            }
+        }
+
+        /*
+         * Severe or ZRTP error codes always signal that security cannot be established.
+         * Inform the user about this.
+         */
+        if (sev == ZrtpCodes.MessageSeverity.Severe) {
+            String[] param = new String[1];
+            param[0] = msgCode.toString();
+            String msg = MediaActivator
+            .getResources().getI18NString(
+                    "impl.media.security.SEVERE_GENERIC_MSG", param);
+            
+            state.put(SecurityGUIEventZrtp.SESSION_TYPE,
+                    SecurityGUIEventZrtp.MSG_SEVERE);
+            state.put(SecurityGUIEventZrtp.MSG_TEXT, msg);
+            fireEvent = true;
+        }
+        
+        
+        if (sev == ZrtpCodes.MessageSeverity.ZrtpError) {
+            String[] param = new String[1];
+            param[0] = msgCode.toString();
+            String msg = MediaActivator
+            .getResources().getI18NString(
+                    "impl.media.security.ZRTP_GENERIC_MSG", param);
+
+            state.put(SecurityGUIEventZrtp.SESSION_TYPE,
+                    SecurityGUIEventZrtp.MSG_ZRTP);
+            state.put(SecurityGUIEventZrtp.MSG_TEXT, msg);
+            fireEvent = true;
+        }
+
+        if (fireEvent) {
+            SecurityGUIEventZrtp evt = new SecurityGUIEventZrtp(participant,
+                    state);
+            fireStateChangedEvent(evt);
         }
 
         if (logger.isInfoEnabled()) {
@@ -183,37 +279,38 @@ public class SCCallback
                     + ", sub code: " + msgCode + ", DH session: " + dhSession
                     + ", multi: " + multiStreams);
         }
-
     }
 
     /*
      * (non-Javadoc)
+     * 
      * @see gnu.java.zrtp.ZrtpUserCallback#zrtpNegotiationFailed(
-     *          gnu.java.zrtp.ZrtpCodes.MessageSeverity,
-     *          java.util.EnumSet)
+     * gnu.java.zrtp.ZrtpCodes.MessageSeverity, java.util.EnumSet)
      */
     public void zrtpNegotiationFailed(ZrtpCodes.MessageSeverity severity,
-                EnumSet<?> subCode)
-    {
+            EnumSet<?> subCode) {
         Iterator<?> ii = subCode.iterator();
         Object msgCode = ii.next();
 
         if (logger.isInfoEnabled())
-            logger.warn(sessionType + ": ZRTP key negotiation failed, sub code: " + msgCode);
+            logger.warn(sessionType
+                    + ": ZRTP key negotiation failed, sub code: " + msgCode);
     }
 
     /*
      * (non-Javadoc)
+     * 
      * @see gnu.java.zrtp.ZrtpUserCallback#secureOff()
      */
-    public void secureOff()
-    {
+    public void secureOff() {
         if (logger.isInfoEnabled())
             logger.info(sessionType + ": Security off");
 
         HashMap<String, Object> state = new HashMap<String, Object>(2);
-        
-        state.put(SecurityGUIEventZrtp.SESSION_TYPE, SecurityGUIEventZrtp.AUDIO);
+
+        state
+                .put(SecurityGUIEventZrtp.SESSION_TYPE,
+                        SecurityGUIEventZrtp.AUDIO);
         state.put(SecurityGUIEventZrtp.SECURITY_CHANGE, Boolean.FALSE);
 
         SecurityGUIEventZrtp evt = new SecurityGUIEventZrtp(participant, state);
@@ -222,20 +319,22 @@ public class SCCallback
 
     /*
      * (non-Javadoc)
+     * 
      * @see gnu.java.zrtp.ZrtpUserCallback#zrtpNotSuppOther()
      */
-    public void zrtpNotSuppOther()
-    {
+    public void zrtpNotSuppOther() {
         if (logger.isInfoEnabled())
-            logger.info(sessionType + ": Other party does not support ZRTP key negotiation protocol, no secure calls possible");
+            logger
+                    .info(sessionType
+                            + ": Other party does not support ZRTP key negotiation protocol, no secure calls possible");
     }
 
     /*
      * (non-Javadoc)
+     * 
      * @see gnu.java.zrtp.ZrtpUserCallback#confirmGoClear()
      */
-    public void confirmGoClear()
-    {
+    public void confirmGoClear() {
         if (logger.isInfoEnabled())
             logger.info(sessionType + ": GoClear confirmation requested");
     }
