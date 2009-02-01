d117a3dd746741288a9664de36aad5b657c80b55
Sebastien Mazy <sebastien@mazy.eu>
2009-02-01 12:08:47 +0000

    minor branding fixes: year, ampersand

--- src/net/java/sip/communicator/plugin/branding/AboutWindow.java
+++ src/net/java/sip/communicator/plugin/branding/AboutWindow.java
@@ -70,13 +70,14 @@ public class AboutWindow
         int logoAreaFontSize
             = resources.getSettingsInt("plugin.branding.ABOUT_LOGO_FONT_SIZE");
 
+        // FIXME: the message exceeds the window length
         JTextArea logoArea =
             new JTextArea(resources.getI18NString("plugin.branding.LOGO_MESSAGE"));
         logoArea.setFont(
             logoArea.getFont().deriveFont(Font.BOLD, logoAreaFontSize));
         logoArea.setForeground(Constants.TITLE_COLOR);
         logoArea.setOpaque(false);
-        logoArea.setLineWrap(true);
+        logoArea.setLineWrap(false);
         logoArea.setWrapStyleWord(true);
         logoArea.setEditable(false);
         logoArea.setPreferredSize(new Dimension(100, 20));
