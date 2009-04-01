12092d02d22d5a816e10e84e29bdf4de068125e8
Yana Stamcheva <yana@jitsi.org>
2009-04-01 18:38:25 +0000

    In addition to previous commit.

---
+++ src/net/java/sip/communicator/util/swing/SIPCommMenuBar.java
@@ -0,0 +1,25 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.util.swing;
+
+import javax.swing.*;
+
+/**
+ * The SIPCommMenuBar is a <tt>JMenuBar</tt> without border decoration that can
+ * be used as a container for other components, like selector boxes that won't
+ * need a menu decoration.
+ * 
+ * @author Yana Stamcheva
+ */
+public class SIPCommMenuBar
+    extends JMenuBar
+{
+    public SIPCommMenuBar()
+    {
+        this.setBorder(null);
+    }
+}
