0ddf28ee15ccd735c691f0e1091ed854c91ebd8d
Damian Minkov <damencho@jitsi.org>
2009-05-11 14:30:28 +0000

    Implement new login v16 for yahoo.

--- src/net/java/sip/communicator/impl/protocol/yahoo/ProtocolProviderServiceYahooImpl.java
+++ src/net/java/sip/communicator/impl/protocol/yahoo/ProtocolProviderServiceYahooImpl.java
@@ -12,8 +12,6 @@ import net.java.sip.communicator.service.protocol.*;
 import net.java.sip.communicator.service.protocol.event.*;
 import net.java.sip.communicator.util.*;
 
-import org.jivesoftware.smack.*;
-
 import ymsg.network.*;
 import ymsg.network.event.*;
 
