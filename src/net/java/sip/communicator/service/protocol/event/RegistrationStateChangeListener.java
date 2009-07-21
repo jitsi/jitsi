0888a15b98a2bfdff015d928827add341e53b057
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2009-07-21 06:53:26 +0000

    Fixes spelling mistakes, formatting.

--- src/net/java/sip/communicator/service/protocol/event/RegistrationStateChangeListener.java
+++ src/net/java/sip/communicator/service/protocol/event/RegistrationStateChangeListener.java
@@ -4,22 +4,27 @@
  * Distributable under LGPL license.
  * See terms of license at gnu.org.
  */
-
 package net.java.sip.communicator.service.protocol.event;
+
+import java.util.*;
 
 /**
  * An event listener that should be implemented by parties interested in changes
- * that occur in the registration state of a ProtocolProvider.
+ * that occur in the registration state of a
+ * <code>ProtocolProviderService</code>.
+ * 
  * @author Emil Ivov
  */
-public interface RegistrationStateChangeListener extends java.util.EventListener
+public interface RegistrationStateChangeListener
+    extends EventListener
 {
     /**
-     * The method is called by a ProtocolProvider implementation whenver
-     * a change in the registration state of the corresponding provider had
-     * occurred.
-     * @param evt ProviderStatusChangeEvent the event describing the status
-     * change.
+     * The method is called by a <code>ProtocolProviderService</code>
+     * implementation whenever a change in the registration state of the
+     * corresponding provider had occurred.
+     * 
+     * @param evt
+     *            the event describing the status change.
      */
     public void registrationStateChanged(RegistrationStateChangeEvent evt);
 }
