a6497975d216a4d6caf13269be12d8b94368780c
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2009-07-27 21:40:42 +0000

    Adds Javadocs.

--- src/net/java/sip/communicator/util/PropertyChangeNotifier.java
+++ src/net/java/sip/communicator/util/PropertyChangeNotifier.java
@@ -9,53 +9,135 @@ package net.java.sip.communicator.util;
 import java.util.*;
 
 /**
+ * Represents a source of <code>PropertyChangeEvent</code>s which notifies
+ * <code>PropertyChangeListener</code>s about changes in the values of
+ * properties.
+ * 
  * @author Lubomir Marinov
  */
 public class PropertyChangeNotifier
 {
-    private final List<PropertyChangeListener> listeners
-            = new Vector<PropertyChangeListener>();
 
+    /**
+     * The list of <code>PropertyChangeListener</code>s interested in and
+     * notified about changes in the values of the properties of this
+     * <code>PropertyChangeNotifier</code>.
+     */
+    private final List<PropertyChangeListener> listeners
+        = new Vector<PropertyChangeListener>();
+
+    /**
+     * Adds a specific <code>PropertyChangeListener</code> to the list of
+     * listeners interested in and notified about changes in the values of the
+     * properties of this <code>PropertyChangeNotifier</code>.
+     * 
+     * @param listener
+     *            a <code>PropertyChangeListener</code> to be notified about
+     *            changes in the values of the properties of this
+     *            <code>PropertyChangeNotifier</code>. If the specified listener
+     *            is already in the list of interested listeners (i.e. it has
+     *            been previously added), it is not added again.
+     */
     public void addPropertyChangeListener(PropertyChangeListener listener)
     {
-        synchronized(listeners)
-        {
-            if (!listeners.contains(listener))
-                listeners.add(listener);
-        }
+        if (listener != null)
+            synchronized (listeners)
+            {
+                if (!listeners.contains(listener))
+                    listeners.add(listener);
+            }
     }
 
+    /**
+     * Removes a specific <code>PropertyChangeListener</code> from the list of
+     * listeners interested in and notified about changes in the values of the
+     * properties of this <code>PropertyChangeNotifer</code>.
+     * 
+     * @param listener
+     *            a <code>PropertyChangeListener</code> to no longer be notified
+     *            about changes in the values of the properties of this
+     *            <code>PropertyChangeNotifier</code>
+     */
     public void removePropertyChangeListener(PropertyChangeListener listener)
     {
-        synchronized(listeners)
-        {
-            listeners.remove(listener);
-        }
+        if (listener != null)
+            synchronized (listeners)
+            {
+                listeners.remove(listener);
+            }
     }
 
-    protected void firePropertyChange(String property, Object oldValue,
-            Object newValue)
+    /**
+     * Fires a new <code>PropertyChangeEvent</code> to the
+     * <code>PropertyChangeListener</code>s registered with this
+     * <code>PropertyChangeNotifier</code> in order to notify about a change in
+     * the value of a specific property which had its old value modified to a
+     * specific new value.
+     * 
+     * @param property
+     *            the name of the property of this
+     *            <code>PropertyChangeNotifier</code> which had its value
+     *            changed
+     * @param oldValue
+     *            the value of the property with the specified name before the
+     *            change
+     * @param newValue
+     *            the value of the property with the specified name after the
+     *            change
+     */
+    protected void firePropertyChange(
+        String property,
+        Object oldValue,
+        Object newValue)
     {
         PropertyChangeListener[] listeners;
         synchronized (this.listeners)
         {
             listeners
-                = this.listeners.toArray(
-                        new PropertyChangeListener[this.listeners.size()]);
+                = this.listeners
+                        .toArray(
+                            new PropertyChangeListener[this.listeners.size()]);
         }
 
-        PropertyChangeEvent event = new PropertyChangeEvent(
-            getPropertyChangeSource(property, oldValue, newValue),
-            property,
-            oldValue,
-            newValue);
+        PropertyChangeEvent event
+            = new PropertyChangeEvent(
+                    getPropertyChangeSource(property, oldValue, newValue),
+                    property,
+                    oldValue,
+                    newValue);
 
         for (PropertyChangeListener listener : listeners)
             listener.propertyChange(event);
     }
 
-    protected Object getPropertyChangeSource(String property, Object oldValue,
-            Object newValue)
+    /**
+     * Gets the <code>Object</code> to be reported as the source of a new
+     * <code>PropertyChangeEvent</code> which is to notify the
+     * <code>PropertyChangeListener</code>s registered with this
+     * <code>PropertyChangeNotifier</code> about the change in the value of a
+     * property with a specific name from a specific old value to a specific new
+     * value.
+     * 
+     * @param property
+     *            the name of the property which had its value changed from the
+     *            specified old value to the specified new value
+     * @param oldValue
+     *            the value of the property with the specified name before the
+     *            change
+     * @param newValue
+     *            the value of the property with the specified name after the
+     *            change
+     * @return the <code>Object</code> to be reported as the source of the new
+     *         <code>PropertyChangeEvent</code> which is to notify the
+     *         <code>PropertyChangeListener</code>s registered with this
+     *         <code>PropertyChangeNotifier</code> about the change in the value
+     *         of the property with the specified name from the specified old
+     *         value to the specified new value
+     */
+    protected Object getPropertyChangeSource(
+        String property,
+        Object oldValue,
+        Object newValue)
     {
         return this;
     }
