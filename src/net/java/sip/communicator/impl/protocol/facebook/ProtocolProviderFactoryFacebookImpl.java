74c02c60c82bc149274b2d2221779a7f1ded1683
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2009-07-28 17:51:15 +0000

    Fixes warnings about unused imports.

--- src/net/java/sip/communicator/impl/protocol/facebook/ProtocolProviderFactoryFacebookImpl.java
+++ src/net/java/sip/communicator/impl/protocol/facebook/ProtocolProviderFactoryFacebookImpl.java
@@ -8,7 +8,6 @@ package net.java.sip.communicator.impl.protocol.facebook;
 import java.util.*;
 
 import net.java.sip.communicator.service.protocol.*;
-import net.java.sip.communicator.util.*;
 
 import org.osgi.framework.*;
 
