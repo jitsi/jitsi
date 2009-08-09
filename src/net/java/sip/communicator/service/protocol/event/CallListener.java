26244e8c867a3b50d7ed307bc70f905fe3661ffb
Emil Ivov <emcho@jitsi.org>
2009-08-09 21:50:36 +0000

    Renames occurrences of callParticipant to callPeer so that it would better reflect our new Call architecture that also includes conferencing and ConferenceMembers

--- src/net/java/sip/communicator/service/protocol/event/CallListener.java
+++ src/net/java/sip/communicator/service/protocol/event/CallListener.java
@@ -12,7 +12,7 @@ import java.util.*;
  * Instances of this class are used for listening for notifications coming out
  * of a telephony Provider - such as an incoming Call for example. Whenever a
  * telephony Provider receives an invitation to a call from a particular
- * 
+ *
  * @author Emil Ivov
  */
 public interface CallListener
@@ -22,7 +22,7 @@ public interface CallListener
     /**
      * This method is called by a protocol provider whenever an incoming call is
      * received.
-     * 
+     *
      * @param event a CallEvent instance describing the new incoming call
      */
     public void incomingCallReceived(CallEvent event);
@@ -31,18 +31,18 @@ public interface CallListener
      * This method is called by a protocol provider upon initiation of an
      * outgoing call.
      * <p>
-     * 
+     *
      * @param event a CalldEvent instance describing the new incoming call.
      */
     public void outgoingCallCreated(CallEvent event);
 
     /**
-     * Indicates that all participants have left the source call and that it has
+     * Indicates that all peers have left the source call and that it has
      * been ended. The event may be considered redundant since there are already
-     * events issued upon termination of a single call participant but we've
+     * events issued upon termination of a single call peer but we've
      * decided to keep it for listeners that are only interested in call
      * duration and don't want to follow other call details.
-     * 
+     *
      * @param event the <tt>CallEvent</tt> containing the source call.
      */
     public void callEnded(CallEvent event);
