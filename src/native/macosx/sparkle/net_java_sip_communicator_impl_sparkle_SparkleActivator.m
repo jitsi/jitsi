916e9d1709842f6f0d7d3ca14a7465281a4a5bc6
Romain Kuntz <rkuntz@java.net>
2009-08-14 15:55:18 +0000

    Updated to the latest Sparkle version. It now should work on Java1.6. Updates are now signed. Work done by Egidijus Jankauska.

--- src/native/macosx/sparkle/net_java_sip_communicator_impl_sparkle_SparkleActivator.m
+++ src/native/macosx/sparkle/net_java_sip_communicator_impl_sparkle_SparkleActivator.m
@@ -16,11 +16,12 @@
  * installed in /Library/Frameworks/. This Framework is 
  * available at http://sparkle.andymatuschak.org/
  *
- * @author Romain Kuntz
+ * @author Romain Kuntz	
+ * @author Egidijus Jankauskas
  */
 
 #include <Cocoa/Cocoa.h>
-#include <Sparkle/SUUpdater.h>
+#include <Sparkle/Sparkle.h>
 #include "net_java_sip_communicator_impl_sparkle_SparkleActivator.h"
 
 /*
@@ -33,39 +34,45 @@ Java_net_java_sip_communicator_impl_sparkle_SparkleActivator_initSparkle
   (JNIEnv *env, jclass obj, jstring pathToSparkleFramework, 
    jboolean updateAtStartup, jint checkInterval)
 {
-    bool haveBundle = ([[NSBundle mainBundle] 
-                        objectForInfoDictionaryKey:@"CFBundleName"] != nil);
-    const char *path = (*env)->GetStringUTFChars(env, pathToSparkleFramework, 0);  
-
+    BOOL hasLaunchedBefore = [[NSUserDefaults standardUserDefaults] boolForKey:@"SCHasLaunchedBefore"];
+	
+    if(!hasLaunchedBefore)
+    {
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"SCHasLaunchedBefore"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+	
     // The below code was used to avoid to link the Sparkle framework
     // at comilation time. 
+    //const char *path = (*env)->GetStringUTFChars(env, pathToSparkleFramework, 0); 
     //NSBundle* bundle = [NSBundle bundleWithPath:[NSString stringWithCString: path]];
     //Class suUpdaterClass = [bundle classNamed:@"SUUpdater"];
     //id suUpdater = [[suUpdaterClass alloc] init];
-  
-    SUUpdater *suUpdater = [SUUpdater alloc];
-
-    (*env)->ReleaseStringUTFChars(env, pathToSparkleFramework, path);  
- 
+    //(*env)->ReleaseStringUTFChars(env, pathToSparkleFramework, path);
+	
+    SUUpdater *suUpdater = [SUUpdater updaterForBundle:[NSBundle mainBundle]];
+	
     NSMenu* menu = [[NSApplication sharedApplication] mainMenu];
     NSMenu* applicationMenu = [[menu itemAtIndex:0] submenu];
     NSMenuItem* checkForUpdatesMenuItem = [[NSMenuItem alloc]
-                                            initWithTitle:@"Check for Updates"
+                                            initWithTitle:@"Check for Updates..."
                                             action:@selector(checkForUpdates:)
                                             keyEquivalent:@""];
 
-    if (haveBundle) {
-        [checkForUpdatesMenuItem setEnabled:YES];
-        [checkForUpdatesMenuItem setTarget:suUpdater];
-    }
-
+    [checkForUpdatesMenuItem setEnabled:YES];
+    [checkForUpdatesMenuItem setTarget:suUpdater];
+	
     // 0 => top, 1 => after "About..."
     [applicationMenu insertItem:checkForUpdatesMenuItem atIndex:1];
 
-    // Check at Startup (SUCheckAtStartup and SUScheduledCheckInterval
-    // specified in the Info.plist does not seem to work)
-    if (updateAtStartup == JNI_TRUE)
-        [suUpdater checkForUpdatesInBackground];
-
-    [suUpdater scheduleCheckWithInterval:checkInterval];
+    // Update is launched only at the second startup
+    if (hasLaunchedBefore && updateAtStartup == JNI_TRUE)
+    {
+        // This method needs to be executed on the main thread because it may result
+        // in GUI showing up. Besides, Sparkle uses asynchronous URLConnection which
+        // requires to be called from a thread which runs in a default run loop mode.
+        [suUpdater performSelectorOnMainThread:@selector(checkForUpdatesInBackground)
+                                withObject:nil 
+                                waitUntilDone:NO];
+    }
 }
