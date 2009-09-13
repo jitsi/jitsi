64377809bd622e5ea9339bf9d42a99b0e42f8165
Emil Ivov <emcho@jitsi.org>
2009-09-13 15:54:27 +0000

    Adds new elements to the new MediaService (Work in Progress)

--- src/net/java/sip/communicator/service/neomedia/AudioMediaStream.java
+++ src/net/java/sip/communicator/service/neomedia/AudioMediaStream.java
@@ -6,6 +6,9 @@
  */
 package net.java.sip.communicator.service.neomedia;
 
+import net.java.sip.communicator.service.neomedia.event.*;
+import net.java.sip.communicator.service.protocol.*;
+
 /**
  * Extends the <tt>MediaStream</tt> interface and adds methods specific to
  * audio streaming.
@@ -16,13 +19,32 @@ public interface AudioMediaStream extends MediaStream
 {
     public void addSoundLevelListener();
 
-    public void startSendingDTMF();
+    public void startSendingDTMF(DTMFTone tone);
 
     public void stopSendingDTMF();
 
-    public void addDTMFListener(DTMFListener);
+    /**
+     * Registers a listener that would receive notification events if the
+     * remote party starts sending DTMF tones to us.
+     *
+     * @param listener the <tt>DTMFListener</tt> that we'd like to register.
+     */
+    public void addDTMFListener(DTMFListener listener);
 
-    public void removeDTMFListener();
+    /**
+     *
+     * @param listener
+     */
+    public void removeDTMFListener(DTMFListener listener);
 
-    public void setMute();
+    /**
+     * Causes this <tt>AudioMediaStream</tt> to stop transmitting the audio
+     * being fed from this stream's <tt>MediaDevice</tt> and transmit silence
+     * instead.
+     *
+     * @param on <tt>true</tt> if we are to start transmitting silence and
+     * <tt>false</tt> if we are to use media from this stream's
+     * <tt>MediaDevice</tt> again.
+     */
+    public void setMute(boolean on);
 }
