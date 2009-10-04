23c8c5b76b43179d7a77bfb353beff3d84fb22ee
Werner Dittmann <wernerd@java.net>
2009-10-04 12:37:25 +0000

    Change the license header to be inline with SC's license.

--- src/net/java/sip/communicator/impl/media/transform/zrtp/ZrtpRawPacket.java
+++ src/net/java/sip/communicator/impl/media/transform/zrtp/ZrtpRawPacket.java
@@ -1,20 +1,8 @@
-/**
- * Copyright (C) 2006-2008 Werner Dittmann
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Authors: Werner Dittmann <Werner.Dittmann@t-online.de>
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
  */
 package net.java.sip.communicator.impl.media.transform.zrtp;
 
