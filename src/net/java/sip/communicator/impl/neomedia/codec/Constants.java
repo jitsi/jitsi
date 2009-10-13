4feb1fba9e58e07d3ec7218d81d275a252bc89b4
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2009-10-13 08:59:55 +0000

    Commits files which were supposed to be in r6129 (Builds a neomedia bundle and provides the initial implementation of .service.neomedia).

---
+++ src/net/java/sip/communicator/impl/neomedia/codec/Constants.java
@@ -0,0 +1,20 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.impl.neomedia.codec;
+
+/**
+ * Allows start import of <tt>net.java.sip.communicator.impl.neomedia.codec</tt>
+ * in order to get the constants define in
+ * <tt>net.java.sip.communicator.impl.media.codec.Constants</tt> without star
+ * import of <tt>net.java.sip.communicator.impl.media.codec</tt>.
+ * 
+ * @author Lubomir Marinov
+ */
+public class Constants
+    extends net.java.sip.communicator.impl.media.codec.Constants
+{
+}
