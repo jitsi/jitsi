9790081d3ce91f222a37c606ebc1b94d955835fa
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2009-10-17 00:12:42 +0000

    Adds javadocs.

--- src/net/java/sip/communicator/service/browserlauncher/BrowserLauncherService.java
+++ src/net/java/sip/communicator/service/browserlauncher/BrowserLauncherService.java
@@ -7,10 +7,22 @@
 package net.java.sip.communicator.service.browserlauncher;
 
 /**
- * 
+ * Represents a service which is able to open an URL in a browser.
+ *
  * @author Yana Stamcheva
  */
 public interface BrowserLauncherService
 {
-    public void openURL(String urlString);
+
+    /**
+     * Tries to open the specified URL in a browser. The attempt is asynchronously
+     * executed and does not wait for possible errors related to the launching
+     * of the associated browser and the opening of the specified URL in it i.e.
+     * the method returns immediately and does not report the success or the
+     * failure of the opening.
+     *
+     * @param url a <tt>String</tt> value which represents the URL to be opened
+     * in a browser
+     */
+    public void openURL(String url);
 }
