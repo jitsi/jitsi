93c157e8d8a88b1a3d64cc217978c987806b0af6
Yana Stamcheva <yana@jitsi.org>
2009-10-24 11:29:47 +0000

    Updates extending renderers. (Fix of revision: 6177 Makes the contact count indicator in the contact list fit better the general contact list look)

--- src/net/java/sip/communicator/impl/gui/main/chat/conference/ChatContactCellRenderer.java
+++ src/net/java/sip/communicator/impl/gui/main/chat/conference/ChatContactCellRenderer.java
@@ -24,6 +24,18 @@ import net.java.sip.communicator.impl.gui.main.contactlist.*;
 public class ChatContactCellRenderer
     extends ContactListCellRenderer
 {
+    /**
+     * Implements the <tt>ListCellRenderer</tt> method. Returns this panel that
+     * has been configured to display a chat contact.
+     *
+     * @param list the source list
+     * @param value the value of the current cell
+     * @param index the index of the current cell in the source list
+     * @param isSelected indicates if this cell is selected
+     * @param cellHasFocus indicates if this cell is focused
+     * 
+     * @return this panel
+     */
     public Component getListCellRendererComponent(  JList list,
                                                     Object value,
                                                     int index,
@@ -32,7 +44,7 @@ public class ChatContactCellRenderer
     {
         this.index = index;
 
-        this.photoLabel.setIcon(null);
+        this.rightLabel.setIcon(null);
 
         ChatContact chatContact = (ChatContact) value;
 
@@ -60,7 +72,7 @@ public class ChatContactCellRenderer
         ImageIcon avatar = chatContact.getAvatar();
 
         if (avatar != null)
-            this.photoLabel.setIcon(avatar);
+            this.rightLabel.setIcon(avatar);
 
         // We should set the bounds of the cell explicitly in order to
         // make getComponentAt work properly.
@@ -69,7 +81,7 @@ public class ChatContactCellRenderer
         this.nameLabel.setBounds(
                     0, 0, list.getWidth() - 28, 17);
 
-        this.photoLabel.setBounds(
+        this.rightLabel.setBounds(
             list.getWidth() - 28, 0, 25, 30);
 
         this.isLeaf = true;
