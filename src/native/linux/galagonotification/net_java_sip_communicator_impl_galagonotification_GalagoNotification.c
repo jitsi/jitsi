e575bad1eb66badc30736436b0daa05d821ae24f
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2009-10-28 10:59:18 +0000

    For notification popups on Linux using freedesktop.org's Desktop Notifications, implements the display of icons.

--- src/native/linux/galagonotification/net_java_sip_communicator_impl_galagonotification_GalagoNotification.c
+++ src/native/linux/galagonotification/net_java_sip_communicator_impl_galagonotification_GalagoNotification.c
@@ -1,6 +1,228 @@
 #include "net_java_sip_communicator_impl_galagonotification_GalagoNotification.h"
 
+#include <stdlib.h>
 #include <dbus/dbus.h>
+
+typedef
+    struct _Image
+        {
+            dbus_int32_t width;
+            dbus_int32_t height;
+            dbus_int32_t rowstride;
+            dbus_bool_t hasAlpha;
+            dbus_int32_t bitsPerSample;
+            dbus_int32_t channels;
+            jint *data;
+            int dataLength;
+
+            jintArray jdata;
+        }
+    Image;
+
+static jint
+GalagoNotification_callIntGetter(
+    JNIEnv *env, jclass clazz, jobject obj, const char *getter)
+{
+    jmethodID getterID;
+
+    getterID = (*env)->GetMethodID(env, clazz, getter, "()I");
+    return getterID ? (*env)->CallIntMethod(env, obj, getterID) : 0;
+}
+
+static dbus_bool_t
+GalagoNotification_catchException(JNIEnv *env)
+{
+    if ((*env)->ExceptionOccurred(env))
+    {
+        (*env)->ExceptionDescribe(env);
+        (*env)->ExceptionClear(env);
+        return TRUE;
+    }
+    return FALSE;
+}
+
+static dbus_bool_t
+GalagoNotification_jobject2Image(JNIEnv *env, jobject image, Image *_image)
+{
+    jclass clazz;
+    jint x, y;
+    jmethodID getRGB;
+
+    clazz = (*env)->GetObjectClass(env, image);
+    if (!clazz)
+        return FALSE;
+
+    x = GalagoNotification_callIntGetter(env, clazz, image, "getMinX");
+    if (GalagoNotification_catchException(env))
+        return FALSE;
+    y = GalagoNotification_callIntGetter(env, clazz, image, "getMinY");
+    if (GalagoNotification_catchException(env))
+        return FALSE;
+    _image->width
+        = GalagoNotification_callIntGetter(env, clazz, image, "getWidth");
+    if (GalagoNotification_catchException(env))
+        return FALSE;
+    _image->height
+        = GalagoNotification_callIntGetter(env, clazz, image, "getHeight");
+    if (GalagoNotification_catchException(env))
+        return FALSE;
+
+    getRGB = (*env)->GetMethodID(env, clazz, "getRGB", "(IIII[III)[I");
+    if (!getRGB)
+    {
+        GalagoNotification_catchException(env);
+        return FALSE;
+    }
+    _image->jdata
+        = (jintArray)
+            (*env)
+                ->CallObjectMethod(
+                    env,
+                    image,
+                    getRGB,
+                    x,
+                    y,
+                    _image->width,
+                    _image->height,
+                    NULL,
+                    0,
+                    _image->width);
+    if (!(_image->jdata))
+    {
+        GalagoNotification_catchException(env);
+        return FALSE;
+    }
+    _image->data = (*env)->GetIntArrayElements(env, _image->jdata, NULL);
+    if (!(_image->data))
+    {
+        GalagoNotification_catchException(env);
+        return FALSE;
+    }
+
+    {
+        /*
+         * Java is big endian, we're likely little endian. Which means that when
+         * Java returns us the jint pixel values they are not the same series of
+         * bytes. So we have to bring back the order to the one in which the
+         * BufferedImage originally put them. Additionally, BufferedImage
+         * returns them as ARGB and we have to provide them to the server as
+         * RGBA. The combination of the two byte reorderings results in swapping
+         * the values of the bytes at indices 0 and 2.
+         */
+        jint jdataLength;
+        jint *data;
+        jint i;
+
+        jdataLength = (*env)->GetArrayLength(env, _image->jdata);
+        if (GalagoNotification_catchException(env))
+            return FALSE;
+        data = _image->data;
+        for (i = 0; i < jdataLength; i++)
+        {
+            jint *datai = data + i;
+            char *pixel = (char *) datai;
+            char swap;
+
+            swap = *pixel;
+            *pixel = *(pixel + 2);
+            *(pixel + 2) = swap;
+        }
+    }
+
+    _image->rowstride = 4 * _image->width;
+    _image->hasAlpha = TRUE;
+    _image->bitsPerSample = 8;
+    _image->channels = 4;
+
+    _image->dataLength
+        = ((_image->height - 1) * _image->rowstride)
+            + (_image->width
+                * ((_image->channels * _image->bitsPerSample + 7) / 8));
+    return TRUE;
+}
+
+static dbus_bool_t
+GalagoNotification_messageAppendIconHint(
+    DBusMessageIter *iter, const Image *image)
+{
+    DBusMessageIter entryIter;
+    const char *str = "icon_data";
+    DBusMessageIter varIter;
+    DBusMessageIter structIter;
+    DBusMessageIter arrayIter;
+
+    if (!dbus_message_iter_open_container(
+            iter,
+            DBUS_TYPE_DICT_ENTRY,
+            NULL,
+            &entryIter))
+        return FALSE;
+    if (!dbus_message_iter_append_basic(&entryIter, DBUS_TYPE_STRING, &str))
+        return FALSE;
+    if (!dbus_message_iter_open_container(
+            &entryIter,
+            DBUS_TYPE_VARIANT,
+            "(iiibiiay)",
+            &varIter))
+        return FALSE;
+    if (!dbus_message_iter_open_container(
+            &varIter,
+            DBUS_TYPE_STRUCT,
+            NULL,
+            &structIter))
+        return FALSE;
+    if (!dbus_message_iter_append_basic(
+            &structIter,
+            DBUS_TYPE_INT32,
+            &(image->width)))
+        return FALSE;
+    if (!dbus_message_iter_append_basic(
+            &structIter,
+            DBUS_TYPE_INT32,
+            &(image->height)))
+        return FALSE;
+    if (!dbus_message_iter_append_basic(
+            &structIter,
+            DBUS_TYPE_INT32,
+            &(image->rowstride)))
+        return FALSE;
+    if (!dbus_message_iter_append_basic(
+            &structIter,
+            DBUS_TYPE_BOOLEAN,
+            &(image->hasAlpha)))
+        return FALSE;
+    if (!dbus_message_iter_append_basic(
+            &structIter,
+            DBUS_TYPE_INT32,
+            &(image->bitsPerSample)))
+        return FALSE;
+    if (!dbus_message_iter_append_basic(
+            &structIter,
+            DBUS_TYPE_INT32,
+            &(image->channels)))
+        return FALSE;
+    if (!dbus_message_iter_open_container(
+            &structIter,
+            DBUS_TYPE_ARRAY,
+            DBUS_TYPE_BYTE_AS_STRING,
+            &arrayIter))
+        return FALSE;
+    if (!dbus_message_iter_append_fixed_array(
+            &arrayIter,
+            DBUS_TYPE_BYTE,
+            &(image->data),
+            image->dataLength))
+        return FALSE;
+    if (!dbus_message_iter_close_container(&structIter, &arrayIter))
+        return FALSE;
+    if (!dbus_message_iter_close_container(&varIter, &structIter))
+        return FALSE;
+    if (!dbus_message_iter_close_container(&entryIter, &varIter))
+        return FALSE;
+    if (!dbus_message_iter_close_container(iter, &entryIter))
+        return FALSE;
+    return TRUE;
+}
 
 static dbus_bool_t
 GalagoNotification_messageAppendString(
@@ -71,6 +293,29 @@ GalagoNotification_notifyAppendArgs(
             "{sv}",
             &subIter))
         return FALSE;
+    if (icon)
+    {
+        Image _icon;
+
+        _icon.jdata = NULL;
+        _icon.data = NULL;
+        if (GalagoNotification_jobject2Image(env, icon, &_icon))
+        {
+            dbus_bool_t success;
+
+            success
+                = GalagoNotification_messageAppendIconHint(&subIter, &_icon);
+            if (_icon.jdata && _icon.data)
+                (*env)
+                    ->ReleaseIntArrayElements(
+                        env,
+                        _icon.jdata,
+                        _icon.data,
+                        JNI_ABORT);
+            if (!success)
+                return FALSE;
+        }
+    }
     if (!dbus_message_iter_close_container(&iter, &subIter))
         return FALSE;
 
--- src/net/java/sip/communicator/impl/galagonotification/GalagoPopupMessageHandler.java
+++ src/net/java/sip/communicator/impl/galagonotification/GalagoPopupMessageHandler.java
@@ -7,6 +7,9 @@
 package net.java.sip.communicator.impl.galagonotification;
 
 import java.awt.image.*;
+import java.io.*;
+
+import javax.imageio.*;
 
 import net.java.sip.communicator.service.systray.*;
 import net.java.sip.communicator.util.*;
@@ -36,15 +39,59 @@ public class GalagoPopupMessageHandler
      */
     private final boolean iconStaticIsImplemented;
 
+    /**
+     * Initializes a new <tt>GalagoPopupMessageHandler</tt> instance with an
+     * indicator which determines whether the freedesktop.org Desktop
+     * Notifications server has reported that it implements the "icon-static"
+     * hint and the new instance has to send it the icons of the
+     * <tt>PopupMessage</tt>s for display.
+     *
+     * @param iconStaticIsImplemented <tt>true</tt> if the freedesktop.org
+     * Desktop Notifications server has reported that it implements the
+     * "icon-static" hint and the new instance has to send it the icons of the
+     * <tt>PopupMessage</tt>s for display; otherwise, <tt>false</tt> and the
+     * new instance will not send the icons of the <tt>PopupMessage</tt>s for
+     * display
+     */
     public GalagoPopupMessageHandler(boolean iconStaticIsImplemented)
     {
         this.iconStaticIsImplemented = iconStaticIsImplemented;
     }
 
-    private BufferedImage getIcon(PopupMessage popupMessge)
+    /**
+     * Gets the icon of the specified <tt>PopupMessage</tt> as a
+     * <tt>BufferedImage</tt> instance so that it can be sent to the
+     * freedesktop.org Desktop Notifications server for display.
+     *
+     * @param popupMessage the <tt>PopupMessage</tt> to get the icon of
+     * @return a <tt>BufferedIImage</tt> instance which represents the icon of
+     * <tt>popupMessage</tt> as reported by its {@link PopupMessage#getIcon()};
+     * <tt>null</tt> if it did not provide an icon or there was an error during
+     * the loading of the icon <tt>byte[]</tt> into a <tt>BufferedImage</tt>
+     * instance
+     */
+    private BufferedImage getIcon(PopupMessage popupMessage)
     {
-        // TODO Auto-generated method stub
-        return null;
+        byte[] iconBytes = popupMessage.getIcon();
+        BufferedImage icon = null;
+
+        if ((iconBytes != null) && (iconBytes.length > 0))
+            try
+            {
+                icon = ImageIO.read(new ByteArrayInputStream(iconBytes));
+            }
+            catch (IOException ioe)
+            {
+                logger
+                    .error("Failed to create BufferedImage from byte[].", ioe);
+            }
+
+        /*
+         * TODO If icon is null, we may want to provide another icon. For
+         * example, the Swing notifications show the application icon in the
+         * case.
+         */
+        return icon;
     }
 
     /**
@@ -57,7 +104,11 @@ public class GalagoPopupMessageHandler
      */
     public int getPreferenceIndex()
     {
-        return 1; // using a native popup mechanism
+        int preferenceIndex = 1; // using a native popup mechanism
+
+        if (iconStaticIsImplemented)
+            ++preferenceIndex; // showing images
+        return preferenceIndex;
     }
 
     /**
