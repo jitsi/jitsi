811d2d825c8e209ec27a3bc54415f6efe728b229
Emil Ivov <emcho@jitsi.org>
2009-11-20 16:00:00 +0000

    Disables the legacy media service

--- src/net/java/sip/communicator/impl/media/MediaActivator.java
+++ src/net/java/sip/communicator/impl/media/MediaActivator.java
@@ -53,7 +53,7 @@ public class MediaActivator
         throws Exception
     {
         logger.debug("Started.");
-
+/*
         MediaActivator.bundleContext = context;
 
         // MediaService
@@ -73,12 +73,13 @@ public class MediaActivator
                 "plugin.mediaconfig.PLUGIN_ICON",
                 "impl.media.configform.TITLE",
                 40),
-            null);
+              null);
 
         //we use the nist-sdp stack to make parse sdp and we need to set the
         //following property to make sure that it would accept java generated
         //IPv6 addresses that contain address scope zones.
         System.setProperty("gov.nist.core.STRIP_ADDR_SCOPES", "true");
+*/
     }
 
     /**
