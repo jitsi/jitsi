6d9fad92a1a867b053d7317e2c7853aea30b1ce2
Werner Dittmann <wernerd@java.net>
2009-12-28 11:40:57 +0000

    Return null if only malformed fmt lines were found.

--- src/net/java/sip/communicator/impl/protocol/sip/CallPeerSipImpl.java
+++ src/net/java/sip/communicator/impl/protocol/sip/CallPeerSipImpl.java
@@ -795,6 +795,12 @@ public class CallPeerSipImpl
      */
     public void processAck(ServerTransaction serverTransaction, Request ack)
     {
+//        CallPeerState peerState = getState();
+//        if (!CallPeerState.isOnHold(peerState))
+//        {
+//            setState(CallPeerState.CONNECTED);
+//        }
+
         ContentLengthHeader contentLength = ack.getContentLength();
         if ((contentLength != null) && (contentLength.getContentLength() > 0))
         {
@@ -895,6 +901,9 @@ public class CallPeerSipImpl
             return;
         }
 
+        if (!CallPeerState.isOnHold(getState()))
+            setState(CallPeerState.CONNECTED);
+        
         try
         {
              //Process SDP unless we've just had an answer in a 18X response
@@ -930,8 +939,8 @@ public class CallPeerSipImpl
         }
 
         // change status
-        if (!CallPeerState.isOnHold(getState()))
-            setState(CallPeerState.CONNECTED);
+//        if (!CallPeerState.isOnHold(getState()))
+//            setState(CallPeerState.CONNECTED);
 
         fireResponseProcessed(ok, null);
     }
