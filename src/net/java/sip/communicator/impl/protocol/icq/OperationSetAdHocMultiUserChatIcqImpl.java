79d5e6b1eb0a6ec1ef9a539a7382b37f1f73e97b
Valentin Martinet <vmartinet@java.net>
2010-01-18 20:25:14 +0000

    Fix ICQ ad-hoc chat room creation.

--- src/net/java/sip/communicator/impl/protocol/icq/OperationSetAdHocMultiUserChatIcqImpl.java
+++ src/net/java/sip/communicator/impl/protocol/icq/OperationSetAdHocMultiUserChatIcqImpl.java
@@ -192,7 +192,11 @@ public class OperationSetAdHocMultiUserChatIcqImpl
      * Creates an ad-hoc room with the named <tt>adHocRoomName</tt> and inviting
      * the specified list of <tt>contacts</tt>.
      *
-     * @param adHocRoomName the name of the ad-hoc room
+     * @param adHocRoomName the name of the ad-hoc room 
+     * (however, it won't/does not have to be used since the only way to create 
+     * an ICQ room is having a room name which begins by the "chat" prefix 
+     * followed by a random number (this is what is used in the ICQ 7 software).
+     * 
      * @param contacts the list of contacts ID
      * @param reason the reason to be sent with the invitation for contacts.
      *
@@ -205,8 +209,9 @@ public class OperationSetAdHocMultiUserChatIcqImpl
         throws OperationFailedException,
         OperationNotSupportedException
     {
+        // It's strongly recommended to don't change room's name below:
         AdHocChatRoom adHocChatRoom = createAdHocChatRoom(
-            adHocRoomName, new Hashtable<String, Object>());
+            "chat"+new Date().getTime(), new Hashtable<String, Object>());
 
         if (adHocChatRoom != null && contacts != null)
         {
