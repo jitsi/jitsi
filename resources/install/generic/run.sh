b3914e8d4282b5c5ef0906aa6ebb345dec44cb4b
Sebastien Vincent <seb@jitsi.org>
2010-03-11 12:45:27 +0000

    Add '-client -Xmx256m' to VM arguments for 32-bit OS in package scripts.

--- resources/install/generic/run.sh
+++ resources/install/generic/run.sh
@@ -1,4 +1,15 @@
 mkdir -p $HOME/.sip-communicator/log
 
+# Get architecture
+ARCH=`uname -m | sed -e s/x86_64/64/ -e s/i.86/32/`
+
+# Additionnal JVM arguments
+CLIENTARGS=""
+
+if [ $ARCH -eq 32 ]
+then
+    CLIENTARGS="-client -Xmx256m"
+fi
+
 export PATH=$PATH:native
-java -classpath "lib/jdic-all.jar:lib/jdic_stub.jar:lib/felix.jar:lib/bcprovider.jar:sc-bundles/sc-launcher.jar:sc-bundles/util.jar" -Djava.library.path=native -Dfelix.config.properties=file:./lib/felix.client.run.properties -Djava.util.logging.config.file=lib/logging.properties net.java.sip.communicator.launcher.SIPCommunicator
+java $CLIENTARGS -classpath "lib/jdic-all.jar:lib/jdic_stub.jar:lib/felix.jar:lib/bcprovider.jar:sc-bundles/sc-launcher.jar:sc-bundles/util.jar" -Djava.library.path=native -Dfelix.config.properties=file:./lib/felix.client.run.properties -Djava.util.logging.config.file=lib/logging.properties net.java.sip.communicator.launcher.SIPCommunicator
