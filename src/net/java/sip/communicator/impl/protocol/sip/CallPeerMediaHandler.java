4c11bba6bab26cef49bd1fc7bded352f93136c85
Matthieu Casanova <kpouer@java.net>
2010-04-16 13:20:38 +0000

    Use of List interface instead of Vector, and fixed a typo

--- src/net/java/sip/communicator/impl/protocol/sip/CallPeerMediaHandler.java
+++ src/net/java/sip/communicator/impl/protocol/sip/CallPeerMediaHandler.java
@@ -722,7 +722,7 @@ public class CallPeerMediaHandler
         }
 
         //fail if all devices were inactive
-        if(mediaDescs.size() == 0)
+        if(mediaDescs.isEmpty())
         {
             ProtocolProviderServiceSipImpl
                 .throwOperationFailedException(
@@ -1162,7 +1162,7 @@ public class CallPeerMediaHandler
         throws OperationFailedException,
                IllegalArgumentException
     {
-        Vector<MediaDescription> remoteDescriptions = SdpUtils
+        List<MediaDescription> remoteDescriptions = SdpUtils
                         .extractMediaDescriptions(offer);
 
         // prepare to generate answers to all the incoming descriptions
@@ -1421,7 +1421,7 @@ public class CallPeerMediaHandler
     {
         this.remoteSess = answer;
 
-        Vector<MediaDescription> remoteDescriptions
+        List<MediaDescription> remoteDescriptions
             = SdpUtils.extractMediaDescriptions(answer);
 
         this.setCallInfoURL(SdpUtils.getCallInfoURL(answer));
@@ -1454,7 +1454,7 @@ public class CallPeerMediaHandler
             {
                 //remote party must have messed up our SDP. throw an exception.
                 ProtocolProviderServiceSipImpl.throwOperationFailedException(
-                    "Remote party sent an invlid SDP answer.",
+                    "Remote party sent an invalid SDP answer.",
                      OperationFailedException.ILLEGAL_ARGUMENT, null, logger);
             }
 
