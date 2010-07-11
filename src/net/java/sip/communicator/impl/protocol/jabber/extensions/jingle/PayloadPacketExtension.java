d74728acc3734f23138c4664a23084ec3733b7d7
Emil Ivov <emcho@jitsi.org>
2010-07-11 15:09:29 +0000

    Adds an implementation of the payloat type element

--- src/net/java/sip/communicator/impl/protocol/jabber/extensions/jingle/PayloadPacketExtension.java
+++ src/net/java/sip/communicator/impl/protocol/jabber/extensions/jingle/PayloadPacketExtension.java
@@ -15,7 +15,7 @@ import org.jivesoftware.smack.packet.*;
  *
  * @author Emil Ivov
  */
-public class PayloadPacketExtension
+public class PayloadPacketExtension implements PacketExtension
 {
     /**
      * Payload types do not live in a namespace of their own so we have
@@ -98,6 +98,26 @@ public class PayloadPacketExtension
     private List<PacketExtension> parameters = new ArrayList<PacketExtension>();
 
     /**
+     * Returns the name of the <tt>payload-type</tt> element.
+     *
+     * @return the name of the <tt>payload-type</tt> element.
+     */
+    public String getElementName()
+    {
+        return ELEMENT_NAME;
+    }
+
+    /**
+     * Returns the namespace for the <tt>payload-type</tt> element.
+     *
+     * @return the namespace for the <tt>payload-type</tt> element.
+     */
+    public String getNamespace()
+    {
+        return NAMESPACE;
+    }
+
+    /**
      * Returns the XML representation of the <tt>payload-type</tt> element
      * including all child elements.
      *
@@ -108,5 +128,157 @@ public class PayloadPacketExtension
         StringBuilder bldr = new StringBuilder();
 
         return bldr.toString();
+    }
+
+    /**
+     * Sets the number of channels in this payload type. If omitted, it will be
+     * assumed to contain one channel.
+     *
+     * @param channels the number of channels in this payload type.
+     */
+    public void setChannels(int channels)
+    {
+        this.channels = channels;
+    }
+
+    /**
+     * Returns the number of channels in this payload type.
+     *
+     * @return the number of channels in this payload type.
+     */
+    public int getChannels()
+    {
+        return channels;
+    }
+
+    /**
+     * Specifies the sampling frequency in Hertz used by this encoding.
+     *
+     * @param clockrate the sampling frequency in Hertz used by this encoding.
+     */
+    public void setClockrate(int clockrate)
+    {
+        this.clockrate = clockrate;
+    }
+
+    /**
+     * Returns the sampling frequency in Hertz used by this encoding.
+     *
+     * @return the sampling frequency in Hertz used by this encoding.
+     */
+    public int getClockrate()
+    {
+        return clockrate;
+    }
+
+    /**
+     * Specifies the payload identifier for this encoding.
+     *
+     * @param id the payload type id
+     */
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    /**
+     * Returns the payload identifier for this encoding (as specified by RFC
+     * 3551 or a dynamic one).
+     *
+     * @return the payload identifier for this encoding (as specified by RFC
+     * 3551 or a dynamic one).
+     */
+    public int getId()
+    {
+        return id;
+    }
+
+    /**
+     * Sets the maximum packet time as specified in RFC 4566.
+     *
+     * @param maxptime the maximum packet time as specified in RFC 4566
+     */
+    public void setMaxptime(int maxptime)
+    {
+        this.maxptime = maxptime;
+    }
+
+    /**
+     * Returns maximum packet time as specified in RFC 4566.
+     *
+     * @return maximum packet time as specified in RFC 4566
+     */
+    public int getMaxptime()
+    {
+        return maxptime;
+    }
+
+    /**
+     * Sets the packet time as specified in RFC 4566.
+     *
+     * @param ptime the packet time as specified in RFC 4566
+     */
+    public void setPtime(int ptime)
+    {
+        this.ptime = ptime;
+    }
+
+    /**
+     * Returns packet time as specified in RFC 4566.
+     *
+     * @return packet time as specified in RFC 4566
+     */
+    public int getPtime()
+    {
+        return ptime;
+    }
+
+    /**
+     * Sets the name of the encoding, or as per the XEP: the appropriate subtype
+     * of the MIME type. Setting this field is RECOMMENDED for static payload
+     * types, REQUIRED for dynamic payload types.
+     *
+     * @param name the name of this encoding.
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * Returns the name of the encoding, or as per the XEP: the appropriate
+     * subtype of the MIME type. Setting this field is RECOMMENDED for static
+     * payload types, REQUIRED for dynamic payload types.
+     *
+     * @return the name of the encoding, or as per the XEP: the appropriate
+     * subtype of the MIME type. Setting this field is RECOMMENDED for static
+     * payload types, REQUIRED for dynamic payload types.
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * Adds an SDP parameter to the list that we already have registered for this
+     * payload type.
+     *
+     * @param parameter an SDP parameter for this encoding.
+     */
+    public void addParameter(PacketExtension parameter)
+    {
+        this.parameters.add(parameter);
+    }
+
+    /**
+     * Returns the list of parameters currently registered for this payload
+     * type.
+     *
+     * @return the list of parameters currently registered for this payload
+     * type.
+     */
+    public List<PacketExtension> getParameters()
+    {
+        return parameters;
     }
 }
