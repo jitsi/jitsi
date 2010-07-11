74a236526e0a35622bc363aab0c5c24ae3ab037e
Emil Ivov <emcho@jitsi.org>
2010-07-11 16:14:35 +0000

    Disables Jabber telephony until it's actually ready

--- src/net/java/sip/communicator/impl/protocol/jabber/ProtocolProviderServiceJabberImpl.java
+++ src/net/java/sip/communicator/impl/protocol/jabber/ProtocolProviderServiceJabberImpl.java
@@ -904,6 +904,7 @@ public class ProtocolProviderServiceJabberImpl
                                            new JingleIQProvider());
 
             //initialize the telephony operation set
+/* disable until ready
             addSupportedOperationSet(
                 OperationSetBasicTelephony.class,
                 new OperationSetBasicTelephonyJabberImpl(this));
@@ -915,6 +916,7 @@ public class ProtocolProviderServiceJabberImpl
             supportedFeatures.add(URN_XMPP_JINGLE_ICE_UDP_1);
             supportedFeatures.add(URN_XMPP_JINGLE_RTP_AUDIO);
             supportedFeatures.add(URN_XMPP_JINGLE_RTP_VIDEO);
+*/
 
             isInitialized = true;
         }
