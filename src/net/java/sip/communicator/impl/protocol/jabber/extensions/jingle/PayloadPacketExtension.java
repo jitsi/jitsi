84e709644e72a172689c9369fe6b1a591168c95c
Emil Ivov <emcho@jitsi.org>
2010-07-11 15:08:06 +0000

    Commits work on parsing the description jingle element

---
+++ src/net/java/sip/communicator/impl/protocol/jabber/extensions/jingle/PayloadPacketExtension.java
@@ -0,0 +1,99 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.impl.protocol.jabber.extensions.jingle;
+
+import java.util.*;
+
+import org.jivesoftware.smack.packet.*;
+
+/**
+ * Represents the <tt>payload-type</tt> elements described in XEP-0167.
+ *
+ * @author Emil Ivov
+ */
+public class PayloadPacketExtension
+{
+    /**
+     * Payload types do not live in a namespace of their own so we have
+     * <tt>null</tt> here.
+     */
+    public static final String NAMESPACE = null;
+
+    /**
+     * The name of the "payload-type" element.
+     */
+    public static final String ELEMENT_NAME = "payload-type";
+
+    /**
+     * The name of the <tt>channels</tt> <tt>payload-type</tt> argument.
+     */
+    public static final String CHANNELS_ARG_NAME = "channels";
+
+    /**
+     * The name of the <tt>clockrate</tt> SDP argument.
+     */
+    public static final String CLOCKRATE_ARG_NAME = "clockrate";
+
+    /**
+     * The name of the payload <tt>id</tt> SDP argument.
+     */
+    public static final String ID_ARG_NAME = "id";
+
+    /**
+     * The name of the <tt>maxptime</tt> SDP argument.
+     */
+    public static final String MAXPTIME_ARG_NAME = "maxptime";
+
+    /**
+     * The name of the <tt>name</tt> SDP argument.
+     */
+    public static final String NAME_ARG_NAME = "name";
+
+    /**
+     * The name of the <tt>ptime</tt> SDP argument.
+     */
+    public static final String PTIME_ARG_NAME = "ptime";
+
+    /**
+     * Number of channels in this payload. If omitted, it MUST be assumed to
+     * contain one channel which is why we set a default value of 1.
+     */
+    private int channels = 1;
+
+    /**
+     * The sampling frequency in Hertz used by this encoding.
+     */
+    private int clockrate = -1;
+
+    /**
+     * The payload identifier for this encoding.
+     */
+    private int id = -1;
+
+    /**
+     * The maximum packet time as specified in RFC 4566
+     */
+    private int maxptime = -1;
+
+    /**
+     * The name of the encoding, or as per the XEP. The appropriate subtype of
+     * the MIME type. Setting this field is RECOMMENDED for static payload
+     * types, REQUIRED for dynamic payload types.
+     */
+    private String name;
+
+    /**
+     * The packet time as specified in RFC 4566
+     */
+    private int ptime = -1;
+
+    /**
+     * An optional list of format parameters (like the one we get with the
+     * fmtp: SDP param).
+     */
+    private List<PacketExtension> parameters = new ArrayList<PacketExtension>();
+}
--- src/net/java/sip/communicator/impl/protocol/jabber/extensions/jingle/RtpDescriptionPacketExtension.java
+++ src/net/java/sip/communicator/impl/protocol/jabber/extensions/jingle/RtpDescriptionPacketExtension.java
@@ -6,9 +6,13 @@
  */
 package net.java.sip.communicator.impl.protocol.jabber.extensions.jingle;
 
+import java.util.*;
+
 import org.jivesoftware.smack.packet.*;
 
 /**
+ * Represents the content <tt>description</tt> elements described in XEP-0167.
+ *
  * @author Emil Ivov
  */
 public class RtpDescriptionPacketExtension
@@ -23,6 +27,93 @@ public class RtpDescriptionPacketExtension
      * The name of the "description" element.
      */
     public static final String ELEMENT_NAME = "description";
+
+    /**
+     * The name of the <tt>media</tt> description argument.
+     */
+    public static final String MEDIA_ARG_NAME = "media";
+
+    /**
+     * The name of the <tt>ssrc</tt> description argument.
+     */
+    public static final String SSRC_ARG_NAME = "ssrc";
+
+    /**
+     * An argument that specifies the media type, such as "audio" or "video".
+     */
+    private String media;
+
+    /**
+     * An attribute that specifies the 32-bit synchronization source for this
+     * media stream, as defined in RFC 3550
+     */
+    private String ssrc;
+
+    /**
+     * The list of payload types that this description element contains.
+     */
+    private final List<PayloadPacketExtension> payloadTypes
+                                    = new ArrayList<PayloadPacketExtension>();
+
+    /**
+     * An optional encryption element that contains encryption parameters for
+     * this session.
+     */
+    private PacketExtension encryptionElement;
+
+    /**
+     * An optional bandwidth element that specifies the allowable or preferred
+     * bandwidth for use by this application type.
+     */
+    private PacketExtension bandwidthElement;
+
+    /**
+     * Specifies the media type for the stream that this description element
+     * represents, such as "audio" or "video".
+     *
+     * @param media the media type for the stream that this element represents
+     * such as "audio" or "video".
+     */
+    public void setMedia(String media)
+    {
+        this.media = media;
+    }
+
+    /**
+     * Returns the media type for the stream that this description element
+     * represents, such as "audio" or "video".
+     *
+     * @return  the media type for the stream that this description element
+     * represents, such as "audio" or "video".
+     */
+    public String getMedia()
+    {
+        return media;
+    }
+
+    /**
+     * Sets the synchronization source ID (SSRC as per RFC 3550) that the stream
+     * represented by this description element will be using.
+     *
+     * @param ssrc the SSRC ID that the RTP stream represented here will be
+     * using.
+     */
+    public void setSsrc(String ssrc)
+    {
+        this.ssrc = ssrc;
+    }
+
+    /**
+     * Returns the synchronization source ID (SSRC as per RFC 3550) that the
+     * stream represented by this description element will be using.
+     *
+     * @return the synchronization source ID (SSRC as per RFC 3550) that the
+     * stream represented by this description element will be using.
+     */
+    public String getSsrc()
+    {
+        return ssrc;
+    }
 
     /**
      * Returns the name of the <tt>description</tt> element.
@@ -41,16 +132,58 @@ public class RtpDescriptionPacketExtension
      */
     public String getNamespace()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return NAMESPACE;
     }
 
-    /* (non-Javadoc)
-     * @see org.jivesoftware.smack.packet.PacketExtension#toXML()
+    /**
+     * Adds a new payload type to this description element.
+     *
+     * @param payload the new payload to add.
+     */
+    public void addPayload(PayloadPacketExtension payload)
+    {
+        this.payloadTypes.add(payload);
+    }
+
+    public List<PayloadPacketExtension> getPayloads()
+    {
+        return payloadTypes;
+    }
+
+    /**
+     * Returns the XML representation of the <tt>jingle</tt> content packet
+     * extension including all child elements.
+     *
+     * @return this packet extension as an XML <tt>String</tt>.
      */
     public String toXML()
     {
-        // TODO Auto-generated method stub
-        return null;
+        StringBuilder bldr = new StringBuilder(
+            "<" + ELEMENT_NAME + " xmlns='" + NAMESPACE + "' "
+                + MEDIA_ARG_NAME + "='" + getMedia() + "'");
+
+        if(getSsrc() != null)
+            bldr.append(SSRC_ARG_NAME + "='" + getSsrc() +"'");
+
+        bldr.append(">");
+
+        //payload types
+        for(PayloadPacketExtension payloadType : payloadTypes)
+        {
+            bldr.append(payloadType.toXML());
+        }
+
+        //encryption element
+        if (encryptionElement != null)
+            bldr.append(encryptionElement);
+
+        //bandwidth element
+        if (bandwidthElement != null)
+            bldr.append(bandwidthElement);
+
+        bldr.append("</" + ELEMENT_NAME + ">");
+
+        return bldr.toString();
     }
+
 }
