bb7fa3320ba0eec683ff3a1447b163e7cce10127
Emil Ivov <emcho@jitsi.org>
2010-07-12 17:08:26 +0000

    Makes ProtocolMediaActivator implement BundleActivator

--- src/net/java/sip/communicator/service/protocol/media/ProtocolMediaActivator.java
+++ src/net/java/sip/communicator/service/protocol/media/ProtocolMediaActivator.java
@@ -21,6 +21,7 @@ import org.osgi.framework.*;
  * @author Emil Ivov
  */
 public class ProtocolMediaActivator
+    implements BundleActivator
 {
     /**
      * The <tt>Logger</tt> used by the <tt>ProtocolMediaActivator</tt>
@@ -65,6 +66,20 @@ public class ProtocolMediaActivator
             logger.debug("Started.");
 
         ProtocolMediaActivator.bundleContext = context;
+    }
+
+    /**
+     * Called when this bundle is stopped so the Framework can perform the
+     * bundle-specific activities necessary to stop the bundle.
+     *
+     * @param context The execution context of the bundle being stopped.
+     * @throws Exception If this method throws an exception, the bundle is
+     *   still marked as stopped, and the Framework will remove the bundle's
+     *   listeners, unregister all services registered by the bundle, and
+     *   release all services used by the bundle.
+     */
+    public void stop(BundleContext context) throws Exception
+    {
     }
 
     /**
