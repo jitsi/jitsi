9b4beb3a9286dbacb48ba5681295013393148ae8
Werner Dittmann <wernerd@java.net>
2010-08-16 19:04:19 +0000

    Bring forward fix from standard GUI to touch screen GUI

--- src/net/java/sip/communicator/impl/gui/main/MainFrameTouch.java
+++ src/net/java/sip/communicator/impl/gui/main/MainFrameTouch.java
@@ -231,10 +231,10 @@ public class MainFrameTouch
      */
     private void init()
     {
-        if (GuiActivator.getUIService().getExitOnMainWindowClose())
-            this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        else
-            this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        setDefaultCloseOperation(
+                GuiActivator.getUIService().getExitOnMainWindowClose()
+                    ? JFrame.DISPOSE_ON_CLOSE
+                    : JFrame.HIDE_ON_CLOSE);
 
         this.setKeybindingInput(KeybindingSet.Category.MAIN);
         this.addKeybindingAction("main-rename",
