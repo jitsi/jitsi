9abbc72777f98e815c5399677b0bf3b95a08b77e
Sebastien Vincent <seb@jitsi.org>
2010-10-05 16:25:52 +0000

    Rollback part of commit 7779 because Sparkle is not loaded properly.

--- src/native/macosx/sparkle/net_java_sip_communicator_impl_sparkle_SparkleActivator.h
+++ src/native/macosx/sparkle/net_java_sip_communicator_impl_sparkle_SparkleActivator.h
@@ -10,10 +10,10 @@ extern "C" {
 /*
  * Class:     net_java_sip_communicator_impl_sparkle_SparkleActivator
  * Method:    initSparkle
- * Signature: (Ljava/lang/String;ZILjava/lang/String;)V
+ * Signature: (Ljava/lang/String;ZI)V
  */
 JNIEXPORT void JNICALL Java_net_java_sip_communicator_impl_sparkle_SparkleActivator_initSparkle
-  (JNIEnv *, jclass, jstring, jboolean, jint, jstring);
+  (JNIEnv *, jclass, jstring, jboolean, jint);
 
 #ifdef __cplusplus
 }
