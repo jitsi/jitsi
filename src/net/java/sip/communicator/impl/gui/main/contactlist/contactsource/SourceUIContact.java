77575882a03e0b5753f0b2de9557e8788767cd93
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2011-01-08 20:12:07 +0000

    Fixes the following issue reported by Emil Ivov on the dev mailing list in the thread _A few issues with Apple's Address Book support_: I don't get the images [...] in the contact list and only see them in the tool tip.

--- src/net/java/sip/communicator/impl/gui/main/contactlist/contactsource/SourceUIContact.java
+++ src/net/java/sip/communicator/impl/gui/main/contactlist/contactsource/SourceUIContact.java
@@ -121,9 +121,9 @@ public class SourceUIContact
     {
         byte[] image = sourceContact.getImage();
 
-        if (image != null)
+        if ((image != null) && (image.length > 0))
         {
-            ImageIcon icon = new ImageIcon();
+            ImageIcon icon = new ImageIcon(image);
 
             if (icon.getIconWidth() > width || icon.getIconHeight() > height)
             {
