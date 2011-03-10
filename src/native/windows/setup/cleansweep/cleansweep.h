0b49e0b090b7f3ed561185b82dad1705de0d34df
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2011-03-10 02:06:06 +0000

    Makes it possible to choose in the Windows setup to remove the personal SIP Communicator configuration while removing the product.

---
+++ src/native/windows/setup/cleansweep/cleansweep.h
@@ -0,0 +1,17 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+
+#ifndef _NET_JAVA_SIP_COMMUNICATOR_WINDOWS_SETUP_CLEANSWEEP_H_
+#define _NET_JAVA_SIP_COMMUNICATOR_WINDOWS_SETUP_CLEANSWEEP_H_
+
+#include <windows.h>
+
+#ifndef PRODUCTNAME
+#define PRODUCTNAME "SIP Communicator"
+#endif /* #ifndef PRODUCTNAME */
+
+#endif /* #ifndef _NET_JAVA_SIP_COMMUNICATOR_WINDOWS_SETUP_CLEANSWEEP_H_ */
