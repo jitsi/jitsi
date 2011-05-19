e05adb157803a82d5bfa1e2b498799110f48d451
Yana Stamcheva <yana@jitsi.org>
2011-05-19 11:10:07 +0000

    Check for create form for all account registration wizards before sending the user to the web registration page.

---
+++ src/net/java/sip/communicator/service/gui/CreateAccountWindow.java
@@ -0,0 +1,29 @@
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package net.java.sip.communicator.service.gui;
+
+/**
+ * 
+ * @author Yana Stamcheva
+ */
+public interface CreateAccountWindow
+{
+    /**
+     * Shows or hides this create account window.
+     *
+     * @param visible <tt>true</tt> to show this window, <tt>false</tt> -
+     * otherwise
+     */
+    public void setVisible(boolean visible);
+
+    /**
+     * Sets the selected wizard.
+     *
+     * @param wizard the wizard to select
+     */
+    public void setSelectedWizard(AccountRegistrationWizard wizard);
+}
