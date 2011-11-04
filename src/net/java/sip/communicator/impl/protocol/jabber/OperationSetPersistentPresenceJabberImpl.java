4054f6b85be3ca8482bf2f63de7cc82fdfab1ba9
Damian Minkov <damencho@jitsi.org>
2011-11-04 15:20:41 +0000

    Fixes some issues with jabber statuses and priority of resources.

--- src/net/java/sip/communicator/impl/protocol/jabber/OperationSetPersistentPresenceJabberImpl.java
+++ src/net/java/sip/communicator/impl/protocol/jabber/OperationSetPersistentPresenceJabberImpl.java
@@ -946,7 +946,9 @@ public class OperationSetPersistentPresenceJabberImpl
                     logger.debug("Received a status update for buddy=" + userID);
 
                 // all contact statuses that are received from all its resources
-                // ordered by priority
+                // ordered by priority(higher first) and those with equal
+                // priorities order with the one that is most connected as
+                // first
                 TreeSet<Presence> userStats = statuses.get(userID);
                 if(userStats == null)
                 {
@@ -954,22 +956,17 @@ public class OperationSetPersistentPresenceJabberImpl
                      {
                         public int compare(Presence o1, Presence o2)
                         {
-                            int res = o1.getPriority() - o2.getPriority();
+                            int res = o2.getPriority() - o1.getPriority();
 
                             // if statuses are with same priorities
                             // return which one is more available
                             // counts the JabberStatusEnum order
                             if(res == 0)
                             {
-                                res =
-                                    jabberStatusToPresenceStatus(
-                                                o1,
-                                                parentProvider)
-                                            .getStatus()
-                                        - jabberStatusToPresenceStatus(
-                                                    o2,
-                                                    parentProvider)
-                                                .getStatus();
+                                res = jabberStatusToPresenceStatus(
+                                        o2, parentProvider).getStatus()
+                                      - jabberStatusToPresenceStatus(
+                                            o1, parentProvider).getStatus();
                             }
 
                             return res;
