5bf6c7295a7f87d458a486b6bfc638a3f5d39414
Yana Stamcheva <yana@jitsi.org>
2012-04-04 16:13:55 +0000

    Fixes security details layout.

--- src/net/java/sip/communicator/impl/gui/main/call/ZrtpSecurityPanel.java
+++ src/net/java/sip/communicator/impl/gui/main/call/ZrtpSecurityPanel.java
@@ -182,7 +182,7 @@ public class ZrtpSecurityPanel
         constraints.gridx = 0;
         constraints.gridy = 0;
         constraints.gridheight = 1;
-        constraints.weightx = 0f;
+        constraints.weightx = 0.5f;
         constraints.weighty = 0f;
         this.add(audioSecurityLabel, constraints);
 
@@ -191,7 +191,7 @@ public class ZrtpSecurityPanel
         constraints.gridx = 0;
         constraints.gridy = 1;
         constraints.gridheight = 1;
-        constraints.weightx = 0f;
+        constraints.weightx = 0.5f;
         constraints.weighty = 0f;
         constraints.insets = new Insets(5, 0, 0, 0);
         this.add(videoSecurityLabel, constraints);
@@ -210,7 +210,7 @@ public class ZrtpSecurityPanel
         constraints.gridx = 0;
         constraints.gridy = 2;
         constraints.gridheight = 1;
-        constraints.weightx = 0f;
+        constraints.weightx = 0.5f;
         constraints.weighty = 0f;
         constraints.insets = new Insets(5, 0, 0, 0);
         this.add(cipherLabel, constraints);
@@ -220,7 +220,7 @@ public class ZrtpSecurityPanel
         constraints.gridx = 1;
         constraints.gridy = 0;
         constraints.gridheight = 3;
-        constraints.weightx = 1f;
+        constraints.weightx = 0.5f;
         constraints.weighty = 0f;
         constraints.insets = new Insets(0, 5, 0, 5);
         this.add(createSasPanel(), constraints);
