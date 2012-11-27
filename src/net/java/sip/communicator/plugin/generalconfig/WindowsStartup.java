2c6d57dcc2f0d157071c73929a9fb849bda30675
Damian Minkov <damencho@jitsi.org>
2012-11-27 17:14:09 +0000

    Renames windows launcher name to one using the application name.

--- src/net/java/sip/communicator/plugin/generalconfig/WindowsStartup.java
+++ src/net/java/sip/communicator/plugin/generalconfig/WindowsStartup.java
@@ -8,9 +8,11 @@ package net.java.sip.communicator.plugin.generalconfig;
 
 import java.io.*;
 
-import net.java.sip.communicator.util.*;
-
+import com.sun.jna.*;
 import com.sun.jna.platform.win32.*;
+import com.sun.jna.win32.*;
+
+import net.java.sip.communicator.util.*;
 
 /**
  * Take care of application auto startup. Reading and writing the registry.
@@ -32,6 +34,19 @@ public class WindowsStartup
             "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
 
     /**
+     * Process Status API. Used to obtain current running executable name.
+     */
+    public interface PSAPI extends StdCallLibrary
+    {
+        PSAPI INSTANCE = (PSAPI)Native.loadLibrary("psapi", PSAPI.class);
+        int GetModuleFileNameExA (
+            WinNT.HANDLE process,
+            Pointer hModule,
+            byte[] lpString,
+            int nMaxCount);
+    };
+
+    /**
      * Checks whether startup is enabled.
      * @param appName the application name.
      * @return is startup enabled.
@@ -42,6 +57,21 @@ public class WindowsStartup
                 WinReg.HKEY_CURRENT_USER,
                 REGISTRY_STARTUP_KEY,
                 appName);
+    }
+
+    /**
+     * Returns the currently running process executable name with path.
+     * @return the currently running process executable name with path.
+     */
+    public static String getModuleFilename()
+    {
+        byte[] exePathName = new byte[512];
+
+        WinNT.HANDLE process = Kernel32.INSTANCE.GetCurrentProcess();
+
+        int result = PSAPI.INSTANCE.GetModuleFileNameExA(
+            process, new Pointer(0), exePathName, exePathName.length);
+        return Native.toString(exePathName).substring(0, result);
     }
 
     /**
@@ -59,11 +89,32 @@ public class WindowsStartup
     {
           if(isAutoStart)
           {
+              String executableFileName = null;
+              String filePath = getModuleFilename();
+
+              if(filePath != null && filePath.length() > 0)
+              {
+                  int ix = filePath.lastIndexOf(File.separatorChar);
+
+                  if(ix > 0)
+                      executableFileName = filePath.substring(ix + 1);
+              }
+
+              if(executableFileName == null)
+              {
+                  logger.warn("Missing information for process, " +
+                      "shortcut will be created any way using defaults.");
+                  // if missing we will use application name
+                  // removing spaces,_,-
+                  executableFileName = appName.replaceAll(" ", "")
+                    .replaceAll("_", "").replaceAll("-","") + ".exe";
+              }
+
               Advapi32Util.registrySetStringValue(
                       WinReg.HKEY_CURRENT_USER,
                       REGISTRY_STARTUP_KEY,
                       appName,
-                      workingDirectory + File.separator + "run.exe");
+                      workingDirectory + File.separator + executableFileName);
           }
           else
           {
