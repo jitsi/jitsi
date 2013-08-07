d3afc1df8c8b9760eea94813d9a5e568a22d9462
paweldomas <pawel.domas@jitsi.org>
2013-08-07 11:58:06 +0200

    Clears reference to certificate dialog service on bundle stop.

--- src/net/java/sip/communicator/impl/certificate/CertificateVerificationActivator.java
+++ src/net/java/sip/communicator/impl/certificate/CertificateVerificationActivator.java
@@ -79,6 +79,7 @@ public class CertificateVerificationActivator
         configService = null;
         resourcesService = null;
         credService = null;
+        certificateDialogService = null;
     }
 
     /**
