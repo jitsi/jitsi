6b39f6cec22114ddc61eb6d26e55bc9c2a7b4da7
hristoterezov <hristo@jitsi.org>
2013-09-24 16:19:27 +0300

    Fixes StringIndexOutOfBoundsException exception when a file with the same filename as the transfered file exists and the filename doesn't contains ".".

--- src/net/java/sip/communicator/impl/gui/main/chat/filetransfer/ReceiveFileConversationComponent.java
+++ src/net/java/sip/communicator/impl/gui/main/chat/filetransfer/ReceiveFileConversationComponent.java
@@ -181,12 +181,17 @@ public class ReceiveFileConversationComponent
         // If a file with the given name already exists, add an index to the
         // file name.
         int index = 0;
+        int filenameLenght = incomingFileName.lastIndexOf(".");
+        if(filenameLenght == -1)
+        {
+            filenameLenght = incomingFileName.length();
+        }
         while (downloadFile.exists())
         {
             String newFileName
-             = incomingFileName.substring(0, incomingFileName.lastIndexOf("."))
+             = incomingFileName.substring(0, filenameLenght)
                  + "-" + ++index
-                 + incomingFileName.substring(incomingFileName.lastIndexOf("."));
+                 + incomingFileName.substring(filenameLenght);
 
             downloadFile = new File(downloadDir, newFileName);
         }
