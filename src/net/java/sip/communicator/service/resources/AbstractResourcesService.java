608faa413bed225cbc0c76d9c328562b689d2d85
Ingo Bauersachs <ingo@jitsi.org>
2014-01-12 20:13:34 +0100

    Fix apostrophe fix from 3729fb5

--- src/net/java/sip/communicator/service/resources/AbstractResourcesService.java
+++ src/net/java/sip/communicator/service/resources/AbstractResourcesService.java
@@ -429,7 +429,7 @@ public abstract class AbstractResourcesService
      */
     public String getI18NString(String key)
     {
-        return getI18NString(key, Locale.getDefault());
+        return getI18NString(key, null, Locale.getDefault());
     }
 
     /**
@@ -454,8 +454,7 @@ public abstract class AbstractResourcesService
      */
     public String getI18NString(String key, Locale locale)
     {
-        return processI18NString(
-                doGetI18String(key, locale));
+        return getI18NString(key, null, locale);
     }
 
     /**
@@ -506,8 +505,7 @@ public abstract class AbstractResourcesService
      */
     public String getI18NString(String key, String[] params, Locale locale)
     {
-        String resourceString = getI18NString(key, locale);
-
+        String resourceString = doGetI18String(key, locale);
         if (resourceString == null)
         {
             logger.warn("Missing resource for key: " + key);
@@ -520,7 +518,7 @@ public abstract class AbstractResourcesService
                     = MessageFormat.format(resourceString, (Object[]) params);
         }
 
-        return resourceString;
+        return processI18NString(resourceString);
     }
 
     /**
