85c52a2753e5023c36c0addc35c725ac30d7adbd
Boris Grozev <boris@jitsi.org>
2014-01-15 09:41:23 +0100

    Adds the "net.java.sip.communicator.impl.browserlauncher.LINUX_BROWSERS" property which holds the list of browsers on linux. It should contain a colon-separated list of browsers to try. Entries are tested using which(1), so either names of executables in PATH or absolute paths can be used. The first one that works is used. Example: net.java.sip.communicator.impl.browserlauncher.LINUX_BROWSERS="chromium-browser:/opt/custom-firefox-install/bin/firefox"

--- src/net/java/sip/communicator/impl/browserlauncher/BrowserLauncherActivator.java
+++ src/net/java/sip/communicator/impl/browserlauncher/BrowserLauncherActivator.java
@@ -8,6 +8,8 @@ package net.java.sip.communicator.impl.browserlauncher;
 
 import net.java.sip.communicator.service.browserlauncher.*;
 import net.java.sip.communicator.util.*;
+import org.jitsi.service.configuration.*;
+import org.osgi.framework.*;
 
 /**
  * Implements <tt>BundleActivator</tt> for the browserlauncher bundle.
@@ -19,6 +21,15 @@ import net.java.sip.communicator.util.*;
 public class BrowserLauncherActivator
     extends SimpleServiceActivator<BrowserLauncherImpl>
 {
+    /**
+     * The <tt>BundleContext</tt>
+     */
+    private static BundleContext bundleContext = null;
+
+    /**
+     * The <tt>ServiceConfiguration</tt> to be used by this service.
+     */
+    private static ConfigurationService configService = null;
 
     /**
      * Creates new instance of <tt>BrowserLauncherActivator</tt>.
@@ -35,5 +46,40 @@ public class BrowserLauncherActivator
     protected BrowserLauncherImpl createServiceImpl()
     {
         return new BrowserLauncherImpl();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Saves <tt>bundleContext</tt> locally.
+     */
+    @Override
+    public void start(BundleContext bundleContext)
+            throws Exception
+    {
+        BrowserLauncherActivator.bundleContext = bundleContext;
+
+        super.start(bundleContext);
+    }
+
+    /**
+     * Returns the <tt>ConfigurationService</tt> obtained from the
+     * the <tt>BundleContext</tt>
+     *
+     * @return the <tt>ConfigurationService</tt> obtained from the
+     * the <tt>BundleContext</tt>
+     */
+    public static ConfigurationService getConfigurationService()
+    {
+        if (configService == null && bundleContext != null)
+        {
+            ServiceReference serviceReference = bundleContext
+                    .getServiceReference(ConfigurationService.class.getName());
+
+            configService = (ConfigurationService)bundleContext
+                    .getService(serviceReference);
+        }
+
+        return configService;
     }
 }
