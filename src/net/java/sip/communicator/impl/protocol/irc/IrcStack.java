5b657da8b64e62e2068c214fe7672d78d6a3daee
Danny van Heumen <danny@dannyvanheumen.nl>
2014-03-07 23:06:23 +0100

    Fix issue with case sentivity in channel names.

--- src/net/java/sip/communicator/impl/protocol/irc/IrcStack.java
+++ src/net/java/sip/communicator/impl/protocol/irc/IrcStack.java
@@ -488,7 +488,7 @@ public class IrcStack
                         {
                             try
                             {
-                                if (!channel.getName().equals(
+                                if (!channel.getName().equalsIgnoreCase(
                                     actualChatRoom.getIdentifier()))
                                 {
                                     // If the channel name is not the
@@ -1334,7 +1334,7 @@ public class IrcStack
          */
         private boolean isThisChatRoom(String chatRoomName)
         {
-            return this.chatroom.getIdentifier().equals(chatRoomName);
+            return this.chatroom.getIdentifier().equalsIgnoreCase(chatRoomName);
         }
 
         /**
