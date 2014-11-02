8ce2fb8ca5e956d73f7773e0f7b5dece804c53ad
Danny van Heumen <danny@dannyvanheumen.nl>
2014-11-02 20:10:55 +0100

    Fixed bad equality check between source chat room contact instance and chat room instance.
    This fails specifically when having, for example, 2 chat rooms with the
    same name but for different accounts. Matching was done solely on chat
    room name and as a consequence it changed/deleted the wrong contact list
    entry.

--- src/net/java/sip/communicator/impl/muc/ChatRoomQuery.java
+++ src/net/java/sip/communicator/impl/muc/ChatRoomQuery.java
@@ -8,12 +8,12 @@ package net.java.sip.communicator.impl.muc;
 import java.util.*;
 import java.util.regex.*;
 
-import org.osgi.framework.*;
-
 import net.java.sip.communicator.service.contactsource.*;
 import net.java.sip.communicator.service.muc.*;
 import net.java.sip.communicator.service.protocol.*;
 import net.java.sip.communicator.service.protocol.event.*;
+
+import org.osgi.framework.*;
 
 /**
  * The <tt>ChatRoomQuery</tt> is a query over the
@@ -165,16 +165,16 @@ public class ChatRoomQuery
         LocalUserChatRoomPresenceChangeEvent evt)
     {
         ChatRoom sourceChatRoom = evt.getChatRoom();
-    
+
         String eventType = evt.getEventType();
-        
+
         boolean existingContact = false;
         ChatRoomSourceContact foundContact = null;
         synchronized (contactResults)
         {
-            for(ChatRoomSourceContact contact : contactResults)
+            for (ChatRoomSourceContact contact : contactResults)
             {
-                if(contact.getContactAddress().equals(sourceChatRoom.getName()))
+                if (contactEqualsChatRoom(contact, sourceChatRoom))
                 {
                     existingContact = true;
                     foundContact = contact;
@@ -306,27 +306,26 @@ public class ChatRoomQuery
      * @param evt the event that describes the change.
      */
     @Override
-    public void contentChanged(ChatRoomListChangeEvent evt)
+    public void contentChanged(final ChatRoomListChangeEvent evt)
     {
         ChatRoomWrapper chatRoom = evt.getSourceChatRoom();
-        switch(evt.getEventID())
+        switch (evt.getEventID())
         {
             case ChatRoomListChangeEvent.CHAT_ROOM_ADDED:
-                addChatRoom(chatRoom.getChatRoom(), false, 
+                addChatRoom(chatRoom.getChatRoom(), false,
                     chatRoom.isAutojoin());
                 break;
             case ChatRoomListChangeEvent.CHAT_ROOM_REMOVED:
                 LinkedList<ChatRoomSourceContact> tmpContactResults;
                 synchronized (contactResults)
                 {
-                    tmpContactResults 
+                    tmpContactResults
                         = new LinkedList<ChatRoomSourceContact>(contactResults);
-                
-                
-                    for(ChatRoomSourceContact contact : tmpContactResults)
+
+                    for (ChatRoomSourceContact contact : tmpContactResults)
                     {
-                        if(contact.getContactAddress().equals(
-                            chatRoom.getChatRoomName()))
+                        if (contactEqualsChatRoom(contact,
+                            chatRoom.getChatRoom()))
                         {
                             contactResults.remove(contact);
                             fireContactRemoved(contact);
@@ -338,12 +337,12 @@ public class ChatRoomQuery
             case ChatRoomListChangeEvent.CHAT_ROOM_CHANGED:
                 synchronized (contactResults)
                 {
-                    for(ChatRoomSourceContact contact : contactResults)
+                    for (ChatRoomSourceContact contact : contactResults)
                     {
-                        if(contact.getContactAddress().equals(
-                            chatRoom.getChatRoomName()))
+                        if (contactEqualsChatRoom(contact,
+                            chatRoom.getChatRoom()))
                         {
-                            if(chatRoom.isAutojoin() != contact.isAutoJoin())
+                            if (chatRoom.isAutojoin() != contact.isAutoJoin())
                             {
                                 contact.setAutoJoin(chatRoom.isAutojoin());
                                 fireContactChanged(contact);
@@ -382,6 +381,22 @@ public class ChatRoomQuery
                 }
             }
         }
+    }
+
+    /**
+     * Test equality of contact to chat room. This test recognizes that chat
+     * rooms may have equal names but connected to different accounts.
+     *
+     * @param contact the contact
+     * @param chatRoom the chat room
+     * @return returns <tt>true</tt> if they are equal, or <tt>false</tt> if
+     *         they are different
+     */
+    private boolean contactEqualsChatRoom(final ChatRoomSourceContact contact,
+        final ChatRoom chatRoom)
+    {
+        return contact.getProvider() == chatRoom.getParentProvider()
+            && contact.getContactAddress().equals(chatRoom.getIdentifier());
     }
 
     /**
