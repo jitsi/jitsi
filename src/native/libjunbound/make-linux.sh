8bb711e005cb10adacdbef18d9124dd452a7e5cc
Ingo Bauersachs <ingo@jitsi.org>
2015-02-09 12:10:02 +0100

    Update build for libjbunbound to use version 1.5.1 and include libssl

--- src/native/libjunbound/make-linux.sh
+++ src/native/libjunbound/make-linux.sh
@@ -10,16 +10,13 @@ mkdir -p $prefix
 
 cd $out
 
-expat=expat-2.0.1
-ldns=ldns-1.6.11
-unbound=unbound-1.4.14
+expat=expat-2.1.0
+unbound=unbound-1.5.1
 
-wget -nc http://downloads.sourceforge.net/project/expat/expat/2.0.1/$expat.tar.gz
-wget -nc http://nlnetlabs.nl/downloads/ldns/$ldns.tar.gz
+wget -nc http://downloads.sourceforge.net/project/expat/expat/2.1.0/$expat.tar.gz
 wget -nc http://unbound.net/downloads/$unbound.tar.gz
 
 tar -xzvf $expat.tar.gz
-tar -xzvf $ldns.tar.gz
 tar -xzvf $unbound.tar.gz
 
 cd $out/$expat
@@ -27,18 +24,12 @@ cd $out/$expat
 make
 make install
 
-cd $out/$ldns
-./configure --with-pic --prefix=$prefix
-make
-make install
-
 cd $out/$unbound
 patch -p 1 -i $out/../../unbound.patch
-./configure --with-pic --prefix=$prefix --with-libexpat=$prefix --with-ldns=$prefix
+./configure --with-pic --prefix=$prefix --with-libexpat=$prefix
 make
 make install
 
 cd $out
-gcc $out/../../src/net_java_sip_communicator_impl_dns_UnboundApi.cpp -fpic -shared -o libjunbound.so -I$JAVA_HOME/include -Wl,-Bstatic -L$prefix/lib -lunbound -lldns -I$prefix/include -Wl,-Bdynamic -lcrypto
+gcc $out/../../src/net_java_sip_communicator_impl_dns_UnboundApi.cpp -fpic -shared -o libjunbound.so -I$JAVA_HOME/include -Wl,-Bstatic -L$prefix/lib -lunbound -I$prefix/include -Wl,-Bdynamic -lcrypto -lssl
 strip libjunbound.so
-
