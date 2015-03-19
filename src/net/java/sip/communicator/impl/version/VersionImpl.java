f8d82ec2ddc226e3c60c20589fcb979cbe28f09a
Damian Minkov <damencho@jitsi.org>
2015-03-19 07:56:59 +0200

    Changes version to 2.8.

--- src/net/java/sip/communicator/impl/version/VersionImpl.java
+++ src/net/java/sip/communicator/impl/version/VersionImpl.java
@@ -31,7 +31,7 @@ public class VersionImpl
      * number changes when a relatively extensive set of new features and
      * possibly rearchitecturing have been applied to the Jitsi.
      */
-    public static final int VERSION_MINOR = 7;
+    public static final int VERSION_MINOR = 8;
 
     /**
      * Indicates whether this version represents a prerelease (i.e. a
