e0a28662b01960fa51d78629322a2f3322e34de5
George Politis <gp@jitsi.org>
2015-10-16 11:41:15 -0500

    Uses an EvictingQueue instead of an ArrayList in the PacketLoggingServiceImpl.

--- src/net/java/sip/communicator/impl/packetlogging/PacketLoggingServiceImpl.java
+++ src/net/java/sip/communicator/impl/packetlogging/PacketLoggingServiceImpl.java
@@ -20,6 +20,7 @@ package net.java.sip.communicator.impl.packetlogging;
 import java.io.*;
 import java.util.*;
 
+import com.google.common.collect.*;
 import net.java.sip.communicator.util.*;
 
 import org.jitsi.service.fileaccess.*;
@@ -769,7 +770,7 @@ public class PacketLoggingServiceImpl
         /**
          * List of packets queued to be written in the file.
          */
-        private final List<Packet> pktsToSave = new ArrayList<Packet>();
+        private final Queue<Packet> pktsToSave = EvictingQueue.create(10);
 
         /**
          * Initializes a new <tt>SaverThread</tt>.
@@ -805,7 +806,7 @@ public class PacketLoggingServiceImpl
                         continue;
                     }
 
-                    pktToSave = pktsToSave.remove(0);
+                    pktToSave = pktsToSave.poll();
                 }
 
                 if(pktToSave != null)
