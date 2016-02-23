4dcdaf941846f0105b41d9f535617d729a8742a8
Boris Grozev <boris@jitsi.org>
2016-02-23 16:45:20 -0600

    Fixes a typo, reported by @kamenik.

--- src/net/java/sip/communicator/impl/protocol/jabber/extensions/colibri/ColibriConferenceIQ.java
+++ src/net/java/sip/communicator/impl/protocol/jabber/extensions/colibri/ColibriConferenceIQ.java
@@ -161,7 +161,7 @@ public class ColibriConferenceIQ
             throw new NullPointerException("channelBundle");
 
         return
-            channelBundles.contains(channelBundles)
+            channelBundles.contains(channelBundle)
                ? false
                : channelBundles.add(channelBundle);
     }
