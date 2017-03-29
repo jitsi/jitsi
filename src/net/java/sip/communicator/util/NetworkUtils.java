ab35a45828de8c1896e772bd512c98a6b95b529b
Boris Grozev <boris@jitsi.org>
2017-03-29 14:09:45 -0500

    fix: Fixes an inexact port range validation.

--- src/net/java/sip/communicator/util/NetworkUtils.java
+++ src/net/java/sip/communicator/util/NetworkUtils.java
@@ -1204,11 +1204,11 @@ public class NetworkUtils
      * @param port the port number that we'd like verified.
      *
      * @return <tt>true</tt> if port is a valid and bindable port number and
-     * <tt>alse</tt> otherwise.
+     * <tt>false</tt> otherwise.
      */
     public static boolean isValidPortNumber(int port)
     {
-        return MIN_PORT_NUMBER < port && port < MAX_PORT_NUMBER;
+        return MIN_PORT_NUMBER <= port && port <= MAX_PORT_NUMBER;
     }
 
     /**
