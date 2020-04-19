520ec96585b619e524b62cc04d502aefd93a49fe
Ingo Bauersachs <ingo@jitsi.org>
2020-04-19 12:59:48 +0200

    Mac paths

--- resources/install/mac/build-dmg.sh
+++ resources/install/mac/build-dmg.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -x
 
-SRC_DIR="dmg"
+SRC_DIR=$1
 TEMP_MOUNT=$(mktemp -d "${TMPDIR:-/tmp}jitsi-dmg.XXXXXXXXX")
 TEMP_DMG="Jitsi-tmp.dmg"
 DMG="jitsi.dmg"
 
-hdiutil create -srcfolder ${SRC_DIR} -volname "Jitsi" -fs HFS+ -ov "${TEMP_DMG}" -format UDRW
+hdiutil create -srcfolder "${SRC_DIR}" -volname "Jitsi" -fs HFS+ -ov "${TEMP_DMG}" -format UDRW
 hdiutil attach "${TEMP_DMG}" -mountroot "${TEMP_MOUNT}/"
 SetFile -a C "${TEMP_MOUNT}/Jitsi"
 ln -s /Applications "${TEMP_MOUNT}/Jitsi"
