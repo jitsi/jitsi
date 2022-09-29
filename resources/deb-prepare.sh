4c118ac4011c88243472218ea7129de3666e2d86
Af0x <remasch@gmx.de>
2022-09-29 23:51:03 +0200

    make deb-prepare.sh compatible with Debian
    Created a variable to optionally adjust the package names for Ubuntu.

--- resources/deb-prepare.sh
+++ resources/deb-prepare.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 set -e
 set -x
+if grep -qi 'Ubuntu' $(cat /etc/os-release); then
+  ubuntu-os="/$(lsb_release -c -s)-backports"
+fi
+
 sudo apt-get update
 # dev-tools from backports because https://bugs.launchpad.net/ubuntu/+source/ubuntu-dev-tools/+bug/1916633
 sudo apt-get install -y \
   debhelper \
   aptitude \
   sbuild \
-  schroot \
-  ubuntu-dev-tools/$(lsb_release -c -s)-backports \
-  python3-ubuntutools/$(lsb_release -c -s)-backports \
+  schroot  \
+  ubuntu-dev-tools$(ubuntu-os) \
+  python3-ubuntutools$(ubuntu-os) \
   debian-archive-keyring \
   git-buildpackage \
   rename \
