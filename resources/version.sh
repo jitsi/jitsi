ad0ad88f69c4c950774e6ae8e0fd12211f320373
TAKAHASHI Shuuji <shuuji3@gmail.com>
2022-12-28 10:30:05 +0000

    ci: fix GitHub Actions set-output deprecation

--- resources/version.sh
+++ resources/version.sh
@@ -8,9 +8,9 @@ if [[ $MVNVER =~ $MVNVER_MATCHER ]]; then
   echo "MAJOR=${MAJOR}"
   echo "MINOR=${MINOR}"
   echo "SUFFIX=${SUFFIX}"
-  echo "::set-output name=jitsi_version_major::${MAJOR}"
-  echo "::set-output name=jitsi_version_minor::${MINOR}"
-  echo "::set-output name=jitsi_version_suffix::${SUFFIX}"
+  echo "jitsi_version_major=${MAJOR}" >> $GITHUB_OUTPUT
+  echo "jitsi_version_minor=${MINOR}" >> $GITHUB_OUTPUT
+  echo "jitsi_version_suffix=${SUFFIX}" >> $GITHUB_OUTPUT
 else
   echo "$MVNVER did not match $MVNVER_MATCHER"
   exit 1
@@ -18,15 +18,15 @@ fi;
 
 GITVER=`git describe --match Jitsi-[0-9.]* --long --dirty --always`
 echo "GITVER=${GITVER}"
-echo "::set-output name=jitsi_version_git::${GITVER}"
+echo "jitsi_version_git=${GITVER}" >> $GITHUB_OUTPUT
 GITVER_MATCHER="^((Jitsi-[0-9.]+)-([0-9]+)-)?([A-Za-z0-9-]+)$"
 if [[ $GITVER =~ $GITVER_MATCHER ]]; then
   NCOMMITS=${BASH_REMATCH[3]}
   HASH=${BASH_REMATCH[4]}
   echo "NCOMMITS=${NCOMMITS}"
   echo "HASH=${HASH}"
-  echo "::set-output name=jitsi_version_ncommits::${NCOMMITS}"
-  echo "::set-output name=jitsi_version_hash::${HASH}"
+  echo "jitsi_version_ncommits=${NCOMMITS}" >> $GITHUB_OUTPUT
+  echo "jitsi_version_hash=${HASH}" >> $GITHUB_OUTPUT
 else
   echo "$GITVER did not match $GITVER_MATCHER"
   exit 1
@@ -45,6 +45,6 @@ fi;
 echo "VERSION_SHORT=${VERSION_SHORT}"
 echo "VERSION_FULL=${VERSION_FULL}"
 echo "VERSION_DEB=${VERSION_DEB}"
-echo "::set-output name=jitsi_version_short::${VERSION_SHORT}"
-echo "::set-output name=jitsi_version_full::${VERSION_FULL}"
-echo "::set-output name=jitsi_version_deb::${VERSION_DEB}"
+echo "jitsi_version_short=${VERSION_SHORT}" >> $GITHUB_OUTPUT
+echo "jitsi_version_full=${VERSION_FULL}" >> $GITHUB_OUTPUT
+echo "jitsi_version_deb=${VERSION_DEB}" >> $GITHUB_OUTPUT
